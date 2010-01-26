5e17e518ee3ae8f2e4b2ef1d5e7f10d06e18ca15
U-CAMBRIA\experimenters <experimenters@cambria>
2010-01-26 12:09:23 -0500

    fixed a bunch of bugs in Test

--- lib/drivers/QImaging/QImagingTest/main.c
+++ lib/drivers/QImaging/QImagingTest/main.c
@@ -2,11 +2,12 @@
 #include <QCamApi.h>
 
 int main () {
-
+        printf("Starting..\n");
 	//Load driver and open the camera
 	QCam_LoadDriver();
-	Qcam_CamListItem	list[10];
-	unsigned long		listLen = sizeof(list)/sizeof(list[Qcam_CamListItem]);
+        printf("Loaded driver.\n");
+	QCam_CamListItem	list[10];
+	unsigned long		listLen = 10;
 	QCam_ListCameras(list, &listLen);
 	QCam_Handle			myHandle;
 	QCam_OpenCamera(list[0].cameraId, &myHandle);
@@ -17,19 +18,19 @@ int main () {
 	QCam_ReadSettingsFromCam(myHandle, &mySettings);
 	
 	//Test Binning...this should work
-	int a = QCam_IsParamSupported(myHandle, QCam_Param	QCam_qprmBinning);
+	int a = QCam_IsParamSupported(myHandle, qprmBinning);
 	printf("IsBinningSupported=", a);
-	int b = QCam_IsRangeTable(&mySettings, QCam_Param QCam_qprmBinning);
+	int b = QCam_IsRangeTable(&mySettings, qprmBinning);
 	printf("IsBinningRangeTable=", b);
-	int c = QCam_IsSparseTable(&mySettings, QCam_Param QCam_qprmBinning);
+	int c = QCam_IsSparseTable(&mySettings, qprmBinning);
 	printf("IsBinningSparseTable=", c);
 	
 	//Test Readout Speed...in python readout speed is reported to be both a range and sparse table
-	int d = QCam_IsParamSupported(myHandle, QCam_Param QCam_qprmReadoutSpeed);
+	int d = QCam_IsParamSupported(myHandle, qprmReadoutSpeed);
 	printf("IsReadoutSpeedSupported=", d);
-	int e = QCam_IsRangeTable(&mySettings, QCam_Param QCam_qprmReadoutSpeed);
+	int e = QCam_IsRangeTable(&mySettings, qprmReadoutSpeed);
 	printf("IsReadoutSpeedRangeTable=", e);
-	int f = QCam_IsSparseTable(&mySettings, QCam_Param QCam_qprmReadoutSpeed);
+	int f = QCam_IsSparseTable(&mySettings, qprmReadoutSpeed);
 	printf("IsReadoutSpeedSparseTable=", f);	
 	
     return 0;
