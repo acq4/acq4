efcc8b5125508f7bac2390fd613ad27156ec6125
Luke Campagnola <luke.campagnola@gmail.com>
2010-05-22 20:47:52 -0400

    minor ui update

--- lib/analysis/UncagingControlTemplate.py
+++ lib/analysis/UncagingControlTemplate.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'UncagingControlTemplate.ui'
 #
-# Created: Sat May 22 20:19:46 2010
+# Created: Sat May 22 20:47:44 2010
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
