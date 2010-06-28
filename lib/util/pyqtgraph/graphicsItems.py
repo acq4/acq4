8b5a050eead95d2b0dc9a9f179a9fe57290c1bc2
Luke Campagnola <luke.campagnola@gmail.com>
2010-06-28 11:49:38 -0400

    More QGraphicsObject rollbacks

--- lib/util/pyqtgraph/graphicsItems.py
+++ lib/util/pyqtgraph/graphicsItems.py
@@ -9,7 +9,7 @@ Provides ImageItem, PlotCurveItem, and ViewBox, amongst others.
 
 
 from PyQt4 import QtGui, QtCore
-#from ObjectWorkaround import *
+from ObjectWorkaround import *
 #tryWorkaround(QtCore, QtGui)
 from numpy import *
 import scipy.weave as weave
@@ -41,23 +41,23 @@ class ItemGroup(QtGui.QGraphicsItem):
         item.setParentItem(self)
 
 
-if hasattr(QtGui, "QGraphicsObject"):
-    QGraphicsObject = QtGui.QGraphicsObject
-else:
-    class QObjectWorkaround:
-        def __init__(self):
-            self._qObj_ = QtCore.QObject()
-        def connect(self, *args):
-            return QtCore.QObject.connect(self._qObj_, *args)
-        def disconnect(self, *args):
-            return QtCore.QObject.disconnect(self._qObj_, *args)
-        def emit(self, *args):
-            return QtCore.QObject.emit(self._qObj_, *args)
+#if hasattr(QtGui, "QGraphicsObject"):
+    #QGraphicsObject = QtGui.QGraphicsObject
+#else:
+    #class QObjectWorkaround:
+        #def __init__(self):
+            #self._qObj_ = QtCore.QObject()
+        #def connect(self, *args):
+            #return QtCore.QObject.connect(self._qObj_, *args)
+        #def disconnect(self, *args):
+            #return QtCore.QObject.disconnect(self._qObj_, *args)
+        #def emit(self, *args):
+            #return QtCore.QObject.emit(self._qObj_, *args)
             
-    class QGraphicsObject(QtGui.QGraphicsItem, QObjectWorkaround):
-        def __init__(self, *args):
-            QtGui.QGraphicsItem.__init__(self, *args)
-            QObjectWorkaround.__init__(self)
+    #class QGraphicsObject(QtGui.QGraphicsItem, QObjectWorkaround):
+        #def __init__(self, *args):
+            #QtGui.QGraphicsItem.__init__(self, *args)
+            #QObjectWorkaround.__init__(self)
 
     
     
