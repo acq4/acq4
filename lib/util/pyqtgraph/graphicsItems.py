ab5363f2c4193e5c894f87232877fe3e97a75d11
megan kratz <meganbkratz@gmail.com>
2011-05-09 22:40:31 -0400

    small adjustments to AuditoryCortex atlas position

--- lib/util/pyqtgraph/graphicsItems.py
+++ lib/util/pyqtgraph/graphicsItems.py
@@ -2429,7 +2429,7 @@ class GridItem(UIGraphicsItem):
             #print "no pic, draw.."
             self.generatePicture()
         p.drawPicture(0, 0, self.picture)
-        print "drawing Grid."
+        #print "drawing Grid."
         
         
     def generatePicture(self):
