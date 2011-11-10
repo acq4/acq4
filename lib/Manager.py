914c2c30137dc8fede7f493df9426cfa2e770bcc
meganbkratz@gmail.com <>
2011-11-10 01:19:52 -0500

    bugfixes

--- lib/Manager.py
+++ lib/Manager.py
@@ -610,7 +610,6 @@ Valid options are:
         
     ## These functions just wrap the functionality of an InterfaceDirectory
     def declareInterface(self, *args, **kargs):  ## args should be name, [types..], object  
-        print "declare:", args, kargs
         return self.interfaceDir.declareInterface(*args, **kargs)
         
     def removeInterface(self, *args, **kargs):
