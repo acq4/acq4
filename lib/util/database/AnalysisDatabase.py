1117436cbee31e8229c6ed297cdc6b980037c128
Luke Campagnola <lcampagn@email.unc.edu>
2012-01-09 16:28:12 -0500

    merged DB fixes from kratz

--- lib/util/database/AnalysisDatabase.py
+++ lib/util/database/AnalysisDatabase.py
@@ -1,4 +1,5 @@
 from database import *
+from pyqtgraph.widgets.ProgressDialog import ProgressDialog
 
 
 class AnalysisDatabase(SqliteDatabase):
@@ -70,38 +71,69 @@ class AnalysisDatabase(SqliteDatabase):
             
     def _convertDB(self, dbFile, version):
         ## Convert datbase dbFile from version to the latest version
+        
         newFileName = dbFile+"version_upgrade"
+        if os.path.exists(newFileName):
+            raise Exception("A .version_upgrade for %s already exists. Please delete or rename it" %dbFile)
         if version is None:
+            prog = ProgressDialog("Converting database...")
             from AnalysisDatabase_ver0 import AnalysisDatabase as AnalysisDatabaseOld
             oldDb = AnalysisDatabaseOld(dbFile)
-            newDb = AnalysisDatabase(newFileName, self.dataModel(), self.baseDir())
+            newDb = AnalysisDatabase(newFileName, self.dataModel(), oldDb.baseDir())
             
             dirTypes = ['Day', 'Experiment', 'Slice', 'Cell', 'Site', 'Protocol', 'ProtocolSequence']
-            
+            print oldDb.listTables()
             for table in dirTypes:
+                if not oldDb.hasTable(table):
+                    continue
                 for rec in oldDb.select(table):
                     dh = oldDb.baseDir()[rec['Dir']]
                     newDb.addDir(dh)
-            for table in ['photostim_events', 'photostim_sites', 'photostim_maps']:
+                    
+            total = len(oldDb.select('Photostim_events')) + len(oldDb.select('Photostim_sites'))
+            n=0
+            for table in ['Photostim_events', 'Photostim_sites']:
+                if prog.wasCanceled():
+                    break
                 schema = oldDb.tableSchema(table)
                 ## SourceDir -> ProtocolSequenceDir     type='directory:ProtocolSequence'
+                del schema['SourceDir']
+                schema['ProtocolSequenceDir'] = 'directory:ProtocolSequence'
+                
                 ## add column ProtocolDir
+                schema['ProtocolDir'] = 'directory:Protocol'
+                
                 ## SourceFile -> ?        type='file'
+                if 'SourceFile' in schema:
+                    schema['SourceFile'] = 'file'
+                
                 owner = oldDb.tableOwner(table)
                 newDb.createTable(table, schema, owner=owner)
                 
                 
                 
-                records = self.select(table)
+                records = oldDb.select(table)
+                for r in records:
+                    if prog.wasCanceled():
+                        break
                 ##  SourceFile -> convert to filehandle
+                    r['SourceFile']= oldDb.getDir('ProtocolSequence', r['SourceDir'])[r['SourceFile']]
+                    del r['SourceDir']
                 ##  ProtocolDir, ProtocolSequenceDir -> dirHandles
-                self.insert(table, records)
+                    #r['ProtocolSequenceDir'] = oldDb.getDir('ProtocolSequence', r['SourceDir'])
+                    r['ProtocolDir'] = r['SourceFile'].parent()
+                    r['ProtocolSequenceDir'] = self.dataModel().getParent(r['ProtocolDir'], 'ProtocolSequence')
+                    n+=1
+                    prog.setValue(n/total)
+                
+                newDb.insert(table, records)
             
             
             oldDb.close()
             newDb.close()
-            os.rename(dbFile, dbFile+'version_upgrade_backup')
-            os.rename(newFileName, dbFile)
+            if not prog.wasCanceled():
+                os.rename(dbFile, dbFile+'version_upgrade_backup')
+                os.rename(newFileName, dbFile)
         else:
             raise Exception("Don't know how to convert from version %s" % str(version))
         
