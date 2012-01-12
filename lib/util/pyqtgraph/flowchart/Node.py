fc233d57a4bb6e3f3ad5f1c90d5a3240d2cff2c1
Luke Campagnola <>
2012-01-12 00:14:40 -0500

    give terminals the same Z value as their parent

--- lib/util/pyqtgraph/flowchart/Node.py
+++ lib/util/pyqtgraph/flowchart/Node.py
@@ -398,6 +398,7 @@ class NodeGraphicsItem(GraphicsObject):
         for i, t in inp.iteritems():
             item = t.graphicsItem()
             item.setParentItem(self)
+            item.setZValue(self.zValue())
             br = self.bounds
             item.setAnchor(0, y)
             self.terminals[i] = (t, item)
@@ -409,6 +410,7 @@ class NodeGraphicsItem(GraphicsObject):
         for i, t in out.iteritems():
             item = t.graphicsItem()
             item.setParentItem(self)
+            item.setZValue(self.zValue())
             br = self.bounds
             item.setAnchor(bounds.width(), y)
             self.terminals[i] = (t, item)
@@ -483,7 +485,7 @@ class NodeGraphicsItem(GraphicsObject):
     def keyPressEvent(self, ev):
         if ev.key() == QtCore.Qt.Key_Delete:
             ev.accept()
-            if not self._allowRemove:
+            if not self.node._allowRemove:
                 return
             self.node.close()
         else:
