ac410f2278a5c1558376f73789384e6318e14900
Luke Campagnola <>
2012-01-22 18:44:09 -0500

    viewbox menu fix

--- lib/util/pyqtgraph/graphicsItems/ViewBox/ViewBoxMenu.py
+++ lib/util/pyqtgraph/graphicsItems/ViewBox/ViewBoxMenu.py
@@ -135,7 +135,7 @@ class ViewBoxMenu(QtGui.QMenu):
         self.view.setMouseEnabled(x=b)
 
     def xManualClicked(self):
-        self.view.enableAutoRange('x', False)
+        self.view.enableAutoRange(VewBox.XAxis, False)
         
     def xMinTextChanged(self):
         self.ctrl[0].manualRadio.setChecked(True)
@@ -146,11 +146,11 @@ class ViewBoxMenu(QtGui.QMenu):
         self.view.setXRange(float(self.ctrl[0].minText.text()), float(self.ctrl[0].maxText.text()), padding=0)
         
     def xAutoClicked(self):
-        self.view.enableAutoRange('x', True)
+        self.view.enableAutoRange(VewBox.XAxis, True)
         
     def xAutoSpinChanged(self, val):
         self.ctrl[0].autoRadio.setChecked(True)
-        self.view.setAutoRange('x', val)
+        self.view.enableAutoRange(VewBox.XAxis, val)
 
     def xLinkComboChanged(self, ind):
         self.view.setXLink(str(self.ctrl[0].linkCombo.currentText()))
@@ -161,7 +161,7 @@ class ViewBoxMenu(QtGui.QMenu):
         self.view.setMouseEnabled(y=b)
 
     def yManualClicked(self):
-        self.view.enableAutoRange('y', False)
+        self.view.enableAutoRange(VewBox.YAxis, False)
         
     def yMinTextChanged(self):
         self.ctrl[1].manualRadio.setChecked(True)
@@ -172,11 +172,11 @@ class ViewBoxMenu(QtGui.QMenu):
         self.view.setYRange(float(self.ctrl[1].minText.text()), float(self.ctrl[1].maxText.text()), padding=0)
         
     def yAutoClicked(self):
-        self.view.enableAutoRange('y', True)
+        self.view.enableAutoRange(VewBox.YAxis, True)
         
     def yAutoSpinChanged(self, val):
         self.ctrl[1].autoRadio.setChecked(True)
-        self.view.setAutoRange('y', val)
+        self.view.enableAutoRange(VewBox.YAxis, val)
 
     def yLinkComboChanged(self, ind):
         self.view.setYLink(str(self.ctrl[1].linkCombo.currentText()))
