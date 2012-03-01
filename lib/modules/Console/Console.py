7a40ddd1b4da222c281767083a32c581661a7c6e
Luke Campagnola <>
2012-02-29 23:33:07 -0500

    removed print

--- lib/modules/Console/Console.py
+++ lib/modules/Console/Console.py
@@ -158,7 +158,7 @@ class Console(Module):
                 self.output.textCursor().insertHtml("</div><br><div style='font-weight: normal; background-color: #FFF;'>")
                 #self.stdout.write("</div><br><div style='font-weight: normal; background-color: #FFF;'>")
             self.output.insertPlainText(strn)
-        self.stdout.write(strn)
+        #self.stdout.write(strn)
             
     def displayException(self):
         tb = traceback.format_exc()
