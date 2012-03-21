1f2b7c0cfa6f5edd9be5ee23256a9eb9bdfd652b
Megan Kratz <meganbkratz@gmail.com>
2012-03-21 10:10:27 -0400

    added isocurve/contour plotting

--- lib/util/pyqtgraph/functions.py
+++ lib/util/pyqtgraph/functions.py
@@ -735,7 +735,85 @@ def rescaleData(data, scale, offset):
     #return facets
     
 
+def isocurve(data, level):
+    """
+        Generate isocurve from 2D data using marching squares algorithm.
+        
+        *data*   2D numpy array of scalar values
+        *level*  The level at which to generate an isosurface
+        
+        This function is SLOW; plenty of room for optimization here.
+        """    
     
+    sideTable = [
+    [],
+    [0,1],
+    [1,2],
+    [0,2],
+    [0,3],
+    [1,3],
+    [0,1,2,3],
+    [2,3],
+    [2,3],
+    [0,1,2,3],
+    [1,3],
+    [0,3],
+    [0,2],
+    [1,2],
+    [0,1],
+    []
+    ]
+    
+    edgeKey=[
+    [(0,1),(0,0)],
+    [(0,0), (1,0)],
+    [(1,0), (1,1)],
+    [(1,1), (0,1)]
+    ]
+    
+    
+    lines = []
+    
+    ## mark everything below the isosurface level
+    mask = data < level
+    
+    ### make four sub-fields and compute indexes for grid cells
+    index = np.zeros([x-1 for x in data.shape], dtype=np.ubyte)
+    fields = np.empty((2,2), dtype=object)
+    slices = [slice(0,-1), slice(1,None)]
+    for i in [0,1]:
+        for j in [0,1]:
+            fields[i,j] = mask[slices[i], slices[j]]
+            #vertIndex = i - 2*j*i + 3*j + 4*k  ## this is just to match Bourk's vertex numbering scheme
+            vertIndex = i+2*j
+            #print i,j,k," : ", fields[i,j,k], 2**vertIndex
+            index += fields[i,j] * 2**vertIndex
+            #print index
+    #print index
+    
+    ## add lines
+    for i in xrange(index.shape[0]):                 # data x-axis
+        for j in xrange(index.shape[1]):             # data y-axis     
+            sides = sideTable[index[i,j]]
+            for l in range(0, len(sides), 2):     ## faces for this grid cell
+                edges = sides[l:l+2]
+                pts = []
+                for m in [0,1]:      # points in this face
+                    p1 = edgeKey[edges[m]][0] # p1, p2 are points at either side of an edge
+                    p2 = edgeKey[edges[m]][1]
+                    v1 = data[i+p1[0], j+p1[1]] # v1 and v2 are the values at p1 and p2
+                    v2 = data[i+p2[0], j+p2[1]]
+                    f = (level-v1) / (v2-v1)
+                    fi = 1.0 - f
+                    p = (    ## interpolate between corners
+                        p1[0]*fi + p2[0]*f + i + 0.5, 
+                        p1[1]*fi + p2[1]*f + j + 0.5
+                        )
+                    pts.append(p)
+                lines.append(pts)
+
+    return lines ## a list of pairs of points
+
     
 def isosurface(data, level):
     """
