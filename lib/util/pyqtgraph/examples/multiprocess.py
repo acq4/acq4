74685eee10a17197e407fc7c68e6aaaf4709dc2f
Luke Campagnola <>
2012-06-17 00:10:52 -0400

    multiprocess updates

--- lib/util/pyqtgraph/examples/multiprocess.py
+++ lib/util/pyqtgraph/examples/multiprocess.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 import initExample ## Add path to library (just for examples; you do not need this)
 
-import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
 import pyqtgraph.multiprocess as mp
 
-app = pg.QtGui.QApplication([])
 
 print "Start process"
 proc = mp.Process()
@@ -15,32 +12,37 @@ rnp = proc._import('numpy')
 arr = rnp.array([1,2,3,4])
 print repr(arr)
 print str(arr)
-print repr(arr.mean(returnValue=True))
-print repr(arr.mean(returnValue=False))
-print repr(arr.mean(returnValue='auto'))
-#proc.join()
-#print "process finished"
+print repr(arr.mean(_returnValue=True))
+print repr(arr.mean(_returnValue=False))
+print repr(arr.mean(_returnValue='auto'))
+proc.join()
+print "process finished"
 
-#print "Start forked process"
-#try:
-    #proc = mp.ForkedProcess()
-#except SystemExit:
-    #print "forked process exit"
-    #raise
-#print "started"
-#rnp = proc._import('numpy')
-#arr = rnp.array([1,2,3,4])
-#print repr(arr)
-#print str(arr)
-#print repr(arr.mean())
-#proc.join()
-#print "process finished"
+print "Start forked process"
+try:
+    proc = mp.ForkedProcess()
+except SystemExit:
+    print "forked process exit"
+    raise
+print "started"
+rnp = proc._import('numpy')
+arr = rnp.array([1,2,3,4])
+print repr(arr)
+print str(arr)
+print repr(arr.mean())
+proc.join()
+print "process finished"
 
-#print "Start Qt Process"
-#proc = mp.QtProcess()
-#rpg = proc._import('pyqtgraph')
-#rpg.plot([1,5,2,3,4,2])
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+app = pg.QtGui.QApplication([])
 
+print "Start Qt Process"
+proc = mp.QtProcess()
+d1 = proc.transfer(np.random.normal(size=1000))
+d2 = proc.transfer(np.random.normal(size=1000))
+rpg = proc._import('pyqtgraph')
+plt = rpg.plot(d1+d2)
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 #import sys
