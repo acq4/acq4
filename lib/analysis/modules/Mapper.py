3ceffaf7bb48491b908453d070089c16a548fefc
Luke Campagnola <>
2012-06-25 18:05:13 -0400

    updated notes for mapper changes

--- lib/analysis/modules/Mapper.py
+++ lib/analysis/modules/Mapper.py
@@ -1,38 +1,130 @@
-##  Description:
-##      
-##  Input: event / site data previously analyzed by photostim
-##  Output: 
-##      - per-event probability of being direct / evoked / spont
-##      - per-site probability of having evoked / direct input
-##      - per-cell measurements of direct and presynaptic area
-##  
-##  Whereas photostim largely operates on a single stimulation (or a single stimulation site)
-##  at a time, mapper operates on complete mapping datasets--multiple scans within a cell
-##  
-##  Ideally, this module should replace the 'stats' and 'map' functionality in photostim
-##  as well as integrate megan's map analysis, but I would really like it 
-##  to be an independent module (and if it's not too difficult, it should _also_ be possible
-##  to integrate it with photostim)
-##  
-##  
-##  Features:
-##      - tracks spontaneous event rate over the timecourse of a cell as well as the prevalence
-##        of specific event features -- amplitude, shape, etc. This data is used to 
-##        determine:
-##           - For each event, the probability that it is evoked / spontaneous / direct
-##           - For each site, the probability that it contains evoked and/or direct events
-##        This should have no notion of 'episodes' -- events at the beginning of one trace
-##        may have been evoked by the previous stim.
-##      - can report total number of evoked presynaptic sites per atlas region, total area of direct activation
-##      
-##      - display colored maps in 3d atlas
-##      
-##      - event-explorer functionality:    (perhaps this should stay separate)
-##          - display scatter plots of events based on various filtering criteria
-##          - mark regions of events within scatter plot as being invalid
-##          - filter generator: filter down events one criteria at a time, use lines / rois to control limits
-##              eg: plot by amplitude, tau; select a population of events that are known to be too large / fast
-##                  replot by relative error and length/tau ratio; select another subset
-##                  once a group is selected / deselected, tag the set (new column in events table)
-##                  
-##         
+"""
+Description:
+    
+Input: event / site data previously analyzed by photostim
+Output: 
+    - per-event probability of being direct / evoked / spont
+    - per-site probability of having evoked / direct input
+    - per-cell measurements of direct and presynaptic area
+
+Whereas photostim largely operates on a single stimulation (or a single stimulation site)
+at a time, mapper operates on complete mapping datasets--multiple scans within a cell
+
+Ideally, this module should replace the 'stats' and 'map' functionality in photostim
+as well as integrate megan's map analysis, but I would really like it 
+to be an independent module (and if it's not too difficult, it should _also_ be possible
+to integrate it with photostim)
+
+
+Features:
+
+    - tracks spontaneous event rate over the timecourse of a cell as well as the prevalence
+    of specific event features -- amplitude, shape, etc. This data is used to 
+    determine:
+        - For each event, the probability that it is evoked / spontaneous / direct
+        - For each site, the probability that it contains evoked and/or direct events
+    This should have no notion of 'episodes' -- events at the beginning of one trace
+    may have been evoked by the previous stim.
+    - can report total number of evoked presynaptic sites per atlas region, total area of direct activation
+    
+    - display colored maps in 3d atlas
+    
+    - event-explorer functionality:    (perhaps this should stay separate)
+        - display scatter plots of events based on various filtering criteria
+        - mark regions of events within scatter plot as being invalid
+        - filter generator: filter down events one criteria at a time, use lines / rois to control limits
+            eg: plot by amplitude, tau; select a population of events that are known to be too large / fast
+                replot by relative error and length/tau ratio; select another subset
+                once a group is selected / deselected, tag the set (new column in events table)
+                
+
+
+Changes to event detector:
+    - Ability to manually adjust PSP fits, particularly for direct responses (this goes into event detector?)
+    - Ability to decrease sensitivity after detecting a direct event
+    - Move region selection out of event detector entirely; should be part of mapper
+    (the mapper can add columns to the event table if we want..)
+    
+
+    
+Notes on probability computation:
+
+    We need to be able to detect several different situations:
+    
+    (1) Obvious, immediate rate change
+    
+    |___||____|_|_______|____|___|___|_|||||||_|_||__|_||___|____|__|_|___|____|___
+                                      ^
+    (2) Obvious, delayed rate change
+    
+    |___||____|_|_______|____|___|___|_|____|__|_|___|___|_||_|_||_|_|__|_|_||_____
+                                      ^
+    (3) Non-obvious rate change, but responses have good precision   
+    
+    |______|______|_________|_______|____|______|________|________|_________|______
+    _____|___________|_______|___|_______|__________|____|______|______|___________
+    ___|________|_________|___|______|___|__|_________|____|_______|___________|___
+                                      ^
+    (4) Very low spont rate (cannot measure intervals between events)
+        with good response precision
+        
+    ______________________________________|________________________________________
+    ________|___________________________________|__________________________________
+    _________________________________________|________________________|____________
+                                      ^
+    (5) Non-obvious rate change, but response amplitudes are very different
+    
+    __,______.___,_______.___,_______,_____|___,_____._________,_,______.______,___
+                                      ^
+
+    
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+## Code for playing with poisson distributions
+
+import numpy as np
+import scipy.stats as stats
+
+def poissonProcess(mu, tmax):
+    """Simulate a poisson process; return a list of event times"""
+    events = []
+    t = 0
+    while True:
+        t += np.random.exponential(mu)
+        if t > tmax:
+            break
+        events.append(t)
+    return events
+
+def poissonProb(events, xvals):
+    y = []
+    for x in xvals:
+        y.append(stats.poisson(10 * x).pmf(len(events<=x)))
+    return np.array(y)
+
+
+rate = 10.
+
+d1 = np.random.poisson(rate, size=100000)
+h1 = np.histogram(d1, bins=range(d1.max()+1))
+
+d2 = np.array([len(poissonProcess(1.0/rate, 1)) for i in xrange(100000)])
+h2 = np.histogram(d2, bins=range(d2.max()+1))
+
+plt = pg.plot(h2[1][1:], h2[0], pen='g', symbolSize=3)
+plt.plot(h1[1][1:], h1[0], pen='r', symbolSize=3)
+
+
+
