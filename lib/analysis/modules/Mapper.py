mapper updates fix for axisitem log scaling

lib/analysis/modules/Mapper.py
@@ -105,22 +105,35 @@ def poissonProcess(rate, tmax):
     events = []
     t = 0
     while True:
-        t += np.random.exponential(1/rate)
+        t += np.random.exponential(1./rate)
         if t > tmax:
             break
         events.append(t)
     return np.array(events)
 
 def poissonProb(events, xvals, rate):
+    ## Given a list of event times,
     ## evaluate poisson cdf of events for multiple windows (0 to x for x in xvals)
+    ## for each value x in xvals, returns the probability that events from 0 to x
+    ## would be produced by a poisson process with the given rate.
     y = []
     for x in xvals:
         y.append(stats.poisson(rate * x).cdf(np.sum(events<=x)))
-    return 1./(1.-np.array(y))
+    return 1.0-np.array(y)
 
 def poissonScore(events, rate):
-    ## maximum of poisson probabilities windowed at each event
-    return poissonProb(events, events, rate).max()
+    ## 1) For each event, measure the probability that the event and those preceding
+    ##    it could be produced by a poisson process
+    ## 2) Of the probabilities computed in 1), select the minimum value
+    ## 3) X = 1 / min to convert from probability to improbability
+    ## 4) apply some magic: Y = sqrt(X) / 2  -- don't know why this works, but
+    ##    it scales the value such that 1 in Y random trials will produce a score >= Y
+    
+    pp = poissonProb(events, events, rate)
+    if len(pp) == 0:
+        return 1.0
+    else:
+        return 0.5 * ((1.0 / pp.min())**0.5)
     
 def poissonBlame(ev, rate):
     ## estimate how much each event contributes to the poisson-score of a list of events.
@@ -129,8 +142,8 @@ def poissonBlame(ev, rate):
         ev2 = list(ev)
         ev2.pop(i)
         #pp.append(poissonScore(ev, rate) / poissonScore(ev2, rate))
-        pp1 = poissonProb(ev, ev, rate)
-        pp2 = poissonProb(ev2, ev2, rate)
+        pp1 = 1. / (1.-poissonProb(ev, ev, rate))
+        pp2 = 1. / (1.-poissonProb(ev2, ev2, rate))
         pp2l = list(pp2)
         print pp2[max(0,i-1):i]
         pp2l.insert(i, pp2[i-1:i].mean())
@@ -224,10 +237,6 @@ def poissonBlame(ev, rate):
         
     #return correctedScores
     
-#app = pg.mkQApp()
-#con = pyqtgraph.console.ConsoleWidget()
-#con.show()
-#con.catchAllExceptions()
     
 ## Attempt to assign a post-probability to each event
 #rate = 3.
@@ -264,11 +273,44 @@ def poissonBlame(ev, rate):
 #recursiveBlame(ev, list(range(len(ev))), rate)
 
 
+app = pg.mkQApp()
+con = pyqtgraph.console.ConsoleWidget()
+con.show()
+con.catchAllExceptions()
+
+## Test ability of poissonScore to predict proability of seeing false positives
+
+
+for rate in [2, 5, 10, 20]:
+    print "spont rate:", rate
+    #rate = 5.
+    tMax = 1.0
+    totals = [0,0,0,0,0,0]
+    pptotals = [0,0,0,0,0,0]
+    trials = 10000
+    for i in xrange(trials):
+        events = poissonProcess(rate, tMax)
+        #prob = 1.0 / (1.-poissonProb(events, [tMax], rate)[0])
+        score = poissonScore(events, rate)
+        for i in range(1,6):
+            if score > 10**i:
+                totals[i] += 1
+            #if prob > 10**i:
+                #pptotals[i] += 1
+    print "False negative scores:"
+    for i in range(1,6):
+        print "   > %d: %d (%0.2f%%)" % (10**i, totals[i], 100*totals[i]/float(trials))
+    #print "False negative probs:"
+    #for i in range(1,6):
+        #print "   > %d: %d (%0.2f%%)" % (10**i, pptotals[i], 100*pptotals[i]/float(trials))
+
+
+raise SystemExit(0)
 
 ## Create a set of test cases:
 
 reps = 30
-spontRate = 10.
+spontRate = 3.
 miniAmp = 1.0
 tMax = 0.5
 
@@ -334,45 +376,57 @@ for i in range(reps):
 
 ## Analyze and plot all:
 
-win = pg.GraphicsWindow()
-for i in range(len(tests)):
-    first = (i == 0)
-    last = (i == len(tests)-1)
-    
-    if first:
-        evLabel = win.addLabel('Event amplitude', angle=-90, rowspan=len(tests))
-    evplt = win.addPlot()
-    
-    if first:
-        scoreLabel = win.addLabel('Poisson Score', angle=-90, rowspan=len(tests))
-    scoreplt = win.addPlot()
-    scoreplt.setLogMode(True, True)
-    diag = pg.InfiniteLine(angle=45)
-    scoreplt.addItem(diag)
-    scoreplt.hideAxis('left')
-    scoreplt.hideAxis('bottom')
-    
-    for j in range(reps):
-        ev = tests[i][j]
-        colors = [(0,255,0,50) if source=='spont' else (255,255,255,50) for source in ev['source']]
-        evplt.plot(x=ev['time'], y=ev['amp'], pen=None, symbolBrush=colors, symbol='d', symbolSize=8, symbolPen=None)
+win = pg.GraphicsWindow(border=0.3)
+with pg.ProgressDialog('processing..', maximum=len(tests)) as dlg:
+    for i in range(len(tests)):
+        first = (i == 0)
+        last = (i == len(tests)-1)
         
-        print ev['time']
-        score1 = poissonScore(ev['time'], spontRate)
-        score2 = poissonScore((ev[ev['source'] == 'spont'])['time'], spontRate)
-        scoreplt.plot(x=[score2], y=[score1], pen=None, symbol='o')
+        if first:
+            evLabel = win.addLabel('Event amplitude', angle=-90, rowspan=len(tests))
+        evplt = win.addPlot()
         
-        evplt.hideAxis('bottom')
-        #scoreplt.hideAxis('bottom')
-        if last:
-            evplt.showAxis('bottom')
-            evplt.setLabel('bottom', 'Event time', 's')
-            #scoreplt.showAxis('bottom')
-            #scoreplt.setLabel('bottom', 'Spontaneous Score')
+        if first:
+            scoreLabel = win.addLabel('Poisson Score', angle=-90, rowspan=len(tests))
+        scoreplt = win.addPlot()
         
+        if first:
+            evplt.register('EventPlot1')
+            scoreplt.register('ScorePlot1')
+        else:
+            evplt.setXLink('EventPlot1')
+            scoreplt.setXLink('ScorePlot1')
+            
+        scoreplt.setLogMode(False, True)
+        #diag = pg.InfiniteLine(angle=45)
+        #scoreplt.addItem(diag)
+        #scoreplt.hideAxis('left')
+        scoreplt.hideAxis('bottom')
         
-        
-    win.nextRow()
+        for j in range(reps):
+            ev = tests[i][j]
+            colors = [(0,255,0,50) if source=='spont' else (255,255,255,50) for source in ev['source']]
+            evplt.plot(x=ev['time'], y=ev['amp'], pen=None, symbolBrush=colors, symbol='d', symbolSize=8, symbolPen=None)
+            
+            #print ev['time']
+            score1 = poissonScore(ev['time'], spontRate)
+            score2 = poissonScore((ev[ev['source'] == 'spont'])['time'], spontRate)
+            scoreplt.plot(x=[j], y=[score1], pen=None, symbol='o', symbolBrush=(255,255,255,100))
+            scoreplt.plot(x=[j], y=[score2], pen=None, symbol='o', symbolBrush=(0,255,0,100))
+            
+            evplt.hideAxis('bottom')
+            #scoreplt.hideAxis('bottom')
+            if last:
+                evplt.showAxis('bottom')
+                evplt.setLabel('bottom', 'Event time', 's')
+                #scoreplt.showAxis('bottom')
+                #scoreplt.setLabel('bottom', 'Spontaneous Score')
+            
+        dlg += 1
+        if dlg.wasCanceled():
+            break
+            
+        win.nextRow()
     
     
     

lib/util/pyqtgraph/graphicsItems/AxisItem.py
@@ -370,8 +370,6 @@ class AxisItem(GraphicsWidget):
         """
         minVal, maxVal = sorted((minVal, maxVal))
         
-        if self.logMode:
-            return self.logTickValues(minVal, maxVal, size)
             
         ticks = []
         tickLevels = self.tickSpacing(minVal, maxVal, size)
@@ -391,18 +389,33 @@ class AxisItem(GraphicsWidget):
             values = filter(lambda x: all(np.abs(allValues-x) > spacing*0.01), values) 
             allValues = np.concatenate([allValues, values])
             ticks.append((spacing, values))
+            
+        if self.logMode:
+            return self.logTickValues(minVal, maxVal, size, ticks)
+            
         return ticks
     
-    def logTickValues(self, minVal, maxVal, size):
-        v1 = int(np.floor(minVal))
-        v2 = int(np.ceil(maxVal))
-        major = list(range(v1+1, v2))
+    def logTickValues(self, minVal, maxVal, size, stdTicks):
         
-        minor = []
-        for v in range(v1, v2):
-            minor.extend(v + np.log10(np.arange(1, 10)))
-        minor = [x for x in minor if x>minVal and x<maxVal]
-        return [(1.0, major), (None, minor)]
+        ## start with the tick spacing given by tickValues().
+        ## Any level whose spacing is < 1 needs to be converted to log scale
+        
+        ticks = []
+        for (spacing, t) in stdTicks:
+            if spacing >= 1.0:
+                ticks.append((spacing, t))
+        
+        if len(ticks) < 3:
+            v1 = int(np.floor(minVal))
+            v2 = int(np.ceil(maxVal))
+            #major = list(range(v1+1, v2))
+            
+            minor = []
+            for v in range(v1, v2):
+                minor.extend(v + np.log10(np.arange(1, 10)))
+            minor = [x for x in minor if x>minVal and x<maxVal]
+            ticks.append((None, minor))
+        return ticks
 
     def tickStrings(self, values, scale, spacing):
         """Return the strings that should be placed next to ticks. This method is called 