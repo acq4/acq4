improved precision in poissonscore computation fixed map analyzer bugs

lib/analysis/modules/MapAnalyzer/MapAnalyzer.py
@@ -111,7 +111,7 @@ class MapAnalyzer(AnalysisModule):
         for stage in self.stages:
             params.append(stage.parameters())
         
-        self.params = ptree.Parameter(name='options', type='group', children=params)
+        self.params = ptree.Parameter.create(name='options', type='group', children=params)
         self.ctrl.setParameters(self.params, showTop=False)
         
         self.params.sigTreeStateChanged.connect(self.update)
@@ -224,7 +224,7 @@ class MapAnalyzer(AnalysisModule):
 
 class EventFilter:
     def __init__(self):
-        self.params = ptree.types.GroupParameter(name='Event Selection', children=[
+        self.params = ptree.Parameter.create(name='Event Selection', type='group', children=[
                 dict(name='Amplitude Sign', type='list', values=['+', '-'], value='+'),
             ])
     
@@ -245,7 +245,7 @@ class SpontRateAnalyzer:
         self.timeMarker = TimelineMarker()
         plot.addItem(self.timeMarker)
         
-        self.params = ptree.types.GroupParameter(name='Spontaneous Rate', type='group', children=[
+        self.params = ptree.Parameter.create(name='Spontaneous Rate', type='group', children=[
                 dict(name='Stimulus Time', type='float', value=0.495, suffix='s', siPrefix=True, step=0.005),
                 dict(name='Method', type='list', values=['Constant', 'Constant (Mean)', 'Constant (Median)', 'Mean Window', 'Median Window', 'Gaussian Window'], value='Gaussian Window'),
                 dict(name='Constant Rate', type='float', value=0, suffix='Hz', siPrefix=True),
@@ -335,9 +335,10 @@ class SpontRateAnalyzer:
 
 class EventStatisticsAnalyzer:
     def __init__(self):
-        self.params = ptree.types.GroupParameter(name='Analysis Methods', type='group', children=[
-                dict(name='Start Time', type='float', value=0.505, suffix='s', siPrefix=True, step=0.001),
-                dict(name='Stop Time', type='float', value=0.7, suffix='s', siPrefix=True, step=0.001),
+        self.params = ptree.Parameter.create(name='Analysis Methods', type='group', children=[
+                dict(name='Stimulus Time', type='float', value=0.5, suffix='s', siPrefix=True, step=0.001),
+                dict(name='Post Start', type='float', value=0.505, suffix='s', siPrefix=True, step=0.001),
+                dict(name='Post Stop', type='float', value=0.7, suffix='s', siPrefix=True, step=0.001),
                 dict(name='Z-Score', type='bool', value=False),
                 dict(name='Poisson', type='bool', value=False),
                 dict(name='Poisson Multi', type='bool', value=True, children=[
@@ -352,8 +353,9 @@ class EventStatisticsAnalyzer:
         return self.params
 
     def process(self, map, spontRateTable, events, ampMean, ampStdev):
-        postStart = self.params['Start Time']
-        postStop = self.params['Stop Time']
+        postStart = self.params['Post Start']
+        postStop = self.params['Post Stop']
+        stimTime = self.params['Stimulus Time']
         postDt = postStop - postStart
         
         ## generate dict of spont. rates for each site
@@ -374,7 +376,7 @@ class EventStatisticsAnalyzer:
             for scan,dh in site['data']['sites']:
                 ev = postEvents[postEvents['ProtocolDir'] == dh]
                 ev2 = np.empty(len(ev), dtype=[('time', float), ('amp', float)])
-                ev2['time'] = ev['fitTime']
+                ev2['time'] = ev['fitTime'] - stimTime
                 ev2['amp'] = ev['fitAmplitude']
                 events.append(ev2)
                 rates.append(spontRate[dh]['filteredSpontRate'])

lib/analysis/tools/poissonScore/poissonScore.py
@@ -125,7 +125,7 @@ def poissonProb(n, t, l, clip=False):
     For a poisson process, return the probability of seeing at least *n* events in *t* seconds given
     that the process has a mean rate *l*.
     """
-    p = stats.poisson(l*t).cdf(n)   
+    p = stats.poisson(l*t).sf(n)   
     if clip:
         p = np.clip(p, 0, 1.0-1e-25)
     return p
@@ -137,7 +137,7 @@ def gaussProb(amps, mean, stdev):
     """
     if len(amps) == 0:
         return 1.0
-    p = 1.0 - stats.norm(mean, stdev).cdf(amps)
+    p = stats.norm(mean, stdev).sf(amps)
     return 1.0 / (p.prod() ** (1./len(amps)))
 
 class PoissonScore:
@@ -181,7 +181,7 @@ class PoissonScore:
             
             nVals = np.array([(ev<=t).sum()-1 for t in ev]) ## looks like arange, but consider what happens if two events occur at the same time.
             pi = poissonProb(nVals, ev, rate*nSets)  ## note that by using n=0 to len(ev)-1, we correct for the fact that the time window always ends at the last event
-            pi = 1.0 / (1.0 - pi)
+            pi = 1.0 / pi
             
             ## apply extra score for uncommonly large amplitudes
             ## (note: by default this has no effect; see amplitudeScore)