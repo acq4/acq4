8a65300160976af06907ec916b78deec96086df3
Luke Campagnola <lcampagn@email.unc.edu>
2012-10-25 18:40:06 -0400

    gl scatterplot update

--- lib/util/pyqtgraph/examples/GLScatterPlotItem.py
+++ lib/util/pyqtgraph/examples/GLScatterPlotItem.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.opengl as gl
+import numpy as np
 
 app = QtGui.QApplication([])
 w = gl.GLViewWidget()
@@ -14,18 +15,22 @@ w.show()
 g = gl.GLGridItem()
 w.addItem(g)
 
-pts = [
-    {'pos': (1,0,0), 'size':0.5, 'color':(1.0, 0.0, 0.0, 0.5)}, 
-    {'pos': (0,1,0), 'size':0.2, 'color':(0.0, 0.0, 1.0, 0.5)}, 
-    {'pos': (0,0,1), 'size':2./3., 'color':(0.0, 1.0, 0.0, 0.5)},
-]
+pos = np.empty((53, 3))
+size = np.empty((53))
+color = np.empty((53, 4))
+pos[0] = (1,0,0); size[0] = 0.5;   color[0] = (1.0, 0.0, 0.0, 0.5)
+pos[1] = (0,1,0); size[1] = 0.2;   color[1] = (0.0, 0.0, 1.0, 0.5)
+pos[2] = (0,0,1); size[2] = 2./3.; color[2] = (0.0, 1.0, 0.0, 0.5)
+
 z = 0.5
 d = 6.0
-for i in range(50):
-    pts.append({'pos': (0,0,z), 'size':2./d, 'color':(0.0, 1.0, 0.0, 0.5)})
+for i in range(3,53):
+    pos[i] = (0,0,z)
+    size[i] = 2./d
+    color[i] = (0.0, 1.0, 0.0, 0.5)
     z *= 0.5
     d *= 2.0
-sp = gl.GLScatterPlotItem(pts)
+sp = gl.GLScatterPlotItem(pos=pos, sizes=size, colors=color, pxMode=True)
 w.addItem(sp)
 
 ## Start Qt event loop unless running in interactive mode.
--- lib/util/pyqtgraph/opengl/items/GLScatterPlotItem.py
+++ lib/util/pyqtgraph/opengl/items/GLScatterPlotItem.py
@@ -8,31 +8,51 @@ __all__ = ['GLScatterPlotItem']
 class GLScatterPlotItem(GLGraphicsItem):
     """Draws points at a list of 3D positions."""
     
-    def __init__(self, data=None):
+    def __init__(self, **kwds):
         GLGraphicsItem.__init__(self)
-        self.data = []
-        if data is not None:
-            self.setData(data)
+        self.pos = []
+        self.colors = None
+        self.sizes = None
+        self.size = 10
+        self.color = [1.0,1.0,1.0,0.5]
+        self.pxMode = True
+        self.setData(**kwds)
     
-    def setData(self, data):
+    def setData(self, **kwds):
         """
         Data may be either a list of dicts (one dict per point) or a numpy record array.
         
         ====================  ==================================================
-        Allowed fields are:
+        Arguments
+        data                  data to be plotted, including position, color, and
+                              size (see below).
+        pxMode                If True, size is interpreted as pixels. Otherwise,
+                              size is expressed in the item's coordinate system.
+        ====================  ==================================================
+        
+        
+        ====================  ==================================================
+        Allowed data fields:
         ------------------------------------------------------------------------
         pos                   (x,y,z) tuple of coordinate values or QVector3D
         color                 (r,g,b,a) tuple of floats (0.0-1.0) or QColor
         size                  (float) diameter of spot
         ====================  ==================================================
+        
+        
         """
-        
-        
-        self.data = data
+        self.pos = kwds.get('pos', self.pos)
+        self.color = kwds.get('color', self.color)
+        self.size = kwds.get('size', self.size)
+        self.colors = kwds.get('colors', self.colors)
+        self.sizes = kwds.get('sizes', self.sizes)
+        self.pxMode = kwds.get('pxMode', self.pxMode)
         self.update()
 
         
     def initializeGL(self):
+        
+        ## Generate texture for rendering points
         w = 64
         def fn(x,y):
             r = ((x-w/2.)**2 + (y-w/2.)**2) ** 0.5
@@ -60,41 +80,55 @@ class GLScatterPlotItem(GLGraphicsItem):
         #glPointParameterfv(GL_POINT_SIZE_MAX, (65500,))
         #glPointParameterfv(GL_POINT_SIZE_MIN, (0,))
         
-        glEnable(GL_POINT_SPRITE)
-        glActiveTexture(GL_TEXTURE0)
-        glEnable( GL_TEXTURE_2D )
-        glBindTexture(GL_TEXTURE_2D, self.pointTexture)
-    
-        glTexEnvi(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_TRUE)
-        #glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)    ## use texture color exactly
-        glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE )  ## texture modulates current color
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
         
-        for pt in self.data:
-            pos = pt['pos']
-            try:
-                color = pt['color']
-            except KeyError:
-                color = (1,1,1,1)
-            try:
-                size = pt['size']
-            except KeyError:
-                size = 10
-                
-            if isinstance(color, QtGui.QColor):
-                color = fn.glColor(color)
-                
-            pxSize = self.view().pixelSize(QtGui.QVector3D(*pos))
+        if self.pxMode:     
+            glVertexPointerf(self.pos)
+            if self.colors is None:
+                glColor4f(*self.color)
+            else:
+                glColorPointerf(self.colors)
+            glPointSize(self.size)
+            glEnableClientState(GL_VERTEX_ARRAY)
+            glDrawArrays(GL_POINTS, 0, len(self.pos))
+        else:
+            glEnable(GL_POINT_SPRITE)
+            glActiveTexture(GL_TEXTURE0)
+            glEnable( GL_TEXTURE_2D )
+            glBindTexture(GL_TEXTURE_2D, self.pointTexture)
+        
+            glTexEnvi(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_TRUE)
+            #glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)    ## use texture color exactly
+            glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE )  ## texture modulates current color
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
             
-            glPointSize(size / pxSize)
-            glBegin( GL_POINTS )
-            glColor4f(*color)  # x is blue
-            #glNormal3f(size, 0, 0)
-            glVertex3f(*pos)
-            glEnd()
+            
+            for i in range(len(self.pos)):
+                pos = self.pos[i]
+                
+                if self.colors is None:
+                    color = self.color
+                else:
+                    color = self.colors[i]
+                if isinstance(color, QtGui.QColor):
+                    color = fn.glColor(color)
+                    
+                if self.sizes is None:
+                    size = self.size
+                else:
+                    size = self.sizes[i]
+                    
+                    
+                pxSize = self.view().pixelSize(QtGui.QVector3D(*pos))
+                
+                glPointSize(size / pxSize)
+                glBegin( GL_POINTS )
+                glColor4f(*color)  # x is blue
+                #glNormal3f(size, 0, 0)
+                glVertex3f(*pos)
+                glEnd()
 
         
         
