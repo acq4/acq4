ca63ac05b7951819d2491e2766deb666d311a4f3
Paul B. Manis <pmanis@med.unc.edu>
2013-12-05 21:31:53 -0500

    Added functionality to trace analysis and reorganized plots. Module now computes spike counts, latency, rising slope, for both pulse duration sweeps and for repeated trains at a given frequency.

--- lib/analysis/modules/TraceAnalyzer/TraceAnalyzer.py
+++ lib/analysis/modules/TraceAnalyzer/TraceAnalyzer.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
+__author__ = 'pbmanis'
+
 """
+TraceAnalysis provides a platform for the analysis of voltage or current traces.
+The module provides standard tools to access data and databases, and flowcharts.
+Two "trace" windows (PSP Plot and Data Plot) are provided to facilitate interactive
+adjustment of analysis parameters.
+Three "scatter plots" are provided on the right for summary analysis.
+The output data is placed into a table.
+The specifics of the analysis depends on the choice of the flowchart.
+12/5/2013 pbmanis
 """
 
 from PyQt4 import QtGui, QtCore
 from lib.analysis.AnalysisModule import AnalysisModule
+import lib.analysis.modules.EventDetector as EventDetector
 from collections import OrderedDict
 import pyqtgraph as pg
 from metaarray import MetaArray
+#from DBCtrl import DBCtrl
 import numpy as np
 from DirTreeWidget import DirTreeLoader
 from FileLoader import FileLoader
 import pyqtgraph.flowchart as fc
 import pyqtgraph.debug as debug
 import os
+import lib.analysis.scripts.chr2analysis as ChR2
 
 class TraceAnalyzer(AnalysisModule):
     def __init__(self, host):
@@ -20,7 +33,8 @@ class TraceAnalyzer(AnalysisModule):
         
         fcpath = os.path.join(os.path.abspath(os.path.split(__file__)[0]), "flowcharts")
         confpath = os.path.join(os.path.abspath(os.path.split(__file__)[0]), "configs")
-        
+        self.dbIdentity = "TraceAnalysis"  ## how we identify to the database; this determines which tables we own
+
         self.confWidget = QtGui.QWidget()
         self.confLoader = ConfLoader(self, confpath)
         self.fileLoader = DataLoader(self, host.dataManager())
@@ -42,17 +56,33 @@ class TraceAnalyzer(AnalysisModule):
         
         self.params = None
         self.data = None
-        
+
+        ## setup map DB ctrl
+        #self.dbCtrl = DBCtrl(self, self.dbIdentity)
+
         self.flowchart = fc.Flowchart(filePath=fcpath)
         self.flowchart.addInput('Input')
         self.flowchart.addOutput('Output')
         #self.flowchart.sigChartLoaded.connect(self.connectPlots)
-        
+        ## create event detector
+        fcDir = os.path.join(os.path.abspath(os.path.split(__file__)[0]), "detector_fc")
+       # self.detector = EventDetector.EventDetector(host, flowchartDir=fcDir, dbIdentity=self.dbIdentity+'.events')
+
+        self.flowchart.sigChartLoaded.connect(self.connectPlots)
+
+        #elems = self.detector.listElements()
+        #print elems
+
         # Setup basic GUI
         self._elements_ = OrderedDict([
             ('Configuration', {'type': 'ctrl', 'object': self.confWidget, 'size': (200,200)}),
-            ('File Loader', {'type': 'ctrl', 'object': self.fileLoader, 'size': (200, 300), 'pos': ('bottom', 'Configuration')}),
-            ('Flowchart', {'type': 'ctrl', 'object': self.flowchart.widget(), 'size': (300,500), 'pos': ('right',)}),
+            ('File Loader', {'type': 'ctrl', 'object': self.fileLoader, 'size': (200, 300), 'pos': ('above', 'Configuration')}),
+            ('Flowchart', {'type': 'ctrl', 'object': self.flowchart.widget(), 'size': (400,500), 'pos': ('right', 'Configuration')}),
+            ('Data Plot', {'type': 'plot', 'pos': ('bottom', 'Flowchart'), 'size': (400,300)}),
+            ('PSP Plot', {'type': 'plot', 'pos': ('bottom', 'Data Plot'), 'size': (400,300)}),
+            ('Scatter Plot1', {'type': 'plot',  'pos': ('right',), 'size': (300,300)}),
+            ('Scatter Plot2', {'type': 'plot',  'pos': ('bottom', 'Scatter Plot1'), 'size': (300,300)}),
+            ('Scatter Plot3', {'type': 'plot',  'pos': ('bottom', 'Scatter Plot2'), 'size': (300,300)}),
             ('Results', {'type': 'table', 'size': (500,200), 'pos': 'bottom'}),
         ])
         self.initializeElements()
@@ -68,11 +98,18 @@ class TraceAnalyzer(AnalysisModule):
         self.flowchart.setInput(Input=files[0])
         table = self.getElement('Results')
         table.setData(None)
+        ChR2.clearSummary()
         
     def fileSelected(self, dh):
         self.flowchart.setInput(Input=dh)
-        
-        
+
+    def connectPlots(self):
+        plots = ['Data Plot', 'PSP Plot']
+        for plotName in plots:
+            dp = self.getElement(plotName, create=False)
+            if dp is not None and plotName in self.flowchart.nodes().keys():
+                self.flowchart.nodes()[plotName].setPlot(dp)
+
     def addPlotClicked(self):
         plot = pg.PlotWidget()
         self.plots.append(plot)
@@ -86,16 +123,24 @@ class TraceAnalyzer(AnalysisModule):
         #dock.setTitle(name)
         
     def processClicked(self):
+       # print ChR2.getSummary()
+        ChR2.clearSummary()
+
         output = []
-        
         table = self.getElement('Results')
         for fh in self.fileLoader.loadedFiles():
             try:
-                output.append(self.flowchart.process(Input=fh))
+                res = self.flowchart.process(Input=fh)
+                output.append(res) # [res[k] for k in res.keys()])
             except:
                 debug.printExc('Error processing %s' % fh)
         table.setData(output)
-    
+        pl = []
+        for i in ['1', '2', '3']:
+            name = 'Scatter Plot%s' % i
+            pl.append(self.getElement(name, create=False))
+            ChR2.printSummary(plotWidget = pl)
+
     def outputChanged(self):
         if self.processCheck.isChecked():
             self.processClicked()
--- lib/analysis/scripts/chr2analysis.py
+++ lib/analysis/scripts/chr2analysis.py
@@ -1,6 +1,9 @@
 __author__ = 'pbmanis'
 
 from collections import OrderedDict
+import re
+import numpy as np
+import scipy.stats
 
 initialized = False
 
@@ -33,21 +36,48 @@ class Params(object):
             print '   ' + x + ' = ',
             print eval('self.' + x)
 
-def protocolInfo(fh, inputs):
+def protocolInfo(fh, inputs, derivative):
+    """
+    protocolInfo is called through "Process" for every directory (epoch run) stored below the protocol directory.
+    fh is the file handle for the current file we are processing
+    inputs is the result of the analysis, which is the result of the threshold detection of spikes
+    inputs contains information about the spike latency, width, peak, etc.
+    The routine returns the "result", which is an ordered dictionary, for each call.
+    However, it also updates the global list "summary", thus concatenating the results into a single
+    array.
+    """
     global summary
     nspikes = len(inputs)
-    reps = fh.info()[('protocol', 'repetitions')]
+    #print inputs
+    reps = fh.parent().info()['protocol']['conf']['repetitions'] # fh.info()[('protocol', 'repetitions')]
     pulseDurIndex = fh.info()[('LED-Blue', 'Command.PulseTrain_length')]
-    #if reps == 0 and pulseDurIndex == 0:
-    #    print 'File\tRep\tPulseDur\tStartTime\tIPI\tNspikes\tEPSPSlopes'
+
     fn = fh.shortName()
+    # find date string in the path, and return path to current data set
+    # allows us to identify the data set by date, slice, cell, protocol, etc.
+    dm = re.compile(r'(\d{4,4})\.(\d{2,2})\.(\d{2,2})*')
+    dsearch = dm.search(fh.name())
+    expname = fh.name()[dsearch.start():] # pull full path for experiment here, but leave out everything above the date
     pulseDur = fh.parent().info()['sequenceParams'][('LED-Blue','Command.PulseTrain_length')][pulseDurIndex]
     pulseTrainInfo = fh.parent().info()['devices']['LED-Blue']['channels']['Command']['waveGeneratorWidget']['stimuli']['PulseTrain']
-    startTime = pulseTrainInfo['start']['value']
-    ipi = pulseTrainInfo['interpulse_length']['value']
-#    print '%12s\t%d\t%8.3f\t%8.3f\t%8.3f\t%d\t' % (fn, reps, pulseDur*1000., startTime*1000., ipi*1000., nspikes)
-    res = OrderedDict([('File: ', fn), ('startTime', startTime*1000), ('IPI', ipi*1000),
-                       ('PulseDur', pulseDur*1000), ('NSpikes', nspikes)])
+    startTime = pulseTrainInfo['start']['value'] # retrieve start time
+    rep = fh.info()[('protocol', 'repetitions')]
+    ipi = pulseTrainInfo['interpulse_length']['value'] # retrieve interpulse interval
+    npulses = pulseTrainInfo['pulse_number']['value'] # retrieve number of pulses in train
+    spikeTimes = [t['time'] for t in inputs]
+    # figure max of derivative of the data after each stimulus pulse. 5 msec window.
+    t=derivative.xvals("Time")
+    slopes = np.zeros(npulses)
+    for n in range(npulses):
+        t0 = startTime + n * ipi
+        t1 = t0 + 3e-3
+        x = np.where((t > t0) & (t <= t1))
+        slopes[n] = np.max(derivative[x])
+
+
+    res = OrderedDict([('Experiment: ', expname), ('File: ', fn), ('startTime', startTime),
+                       ('NPulses', npulses), ('IPI', ipi), ('PulseDur', pulseDur), ('Reps', reps), ('thisRep', rep),
+                       ('NSpikes', nspikes), ('SpikeTimes', spikeTimes), ('Slopes', slopes)])
     summary.append(res)
     return res
 
@@ -59,21 +89,114 @@ def clearSummary():
     global summary
     summary = []
 
-def printSummary():
+def printSummary(plotWidget = None):
     global summary
     if len(summary) == 0:
         return
     title = ''
     kl = []
+    excludeKeys =  ['Experiment: ', 'SpikeTimes', 'Reps']
+    print '----------------------------------'
+    if excludeKeys[0] in summary[0].keys():
+        print 'Experiment: %s  reps: %d' % (summary[0][excludeKeys[0]], summary[0]['Reps'])
     for s in summary[0].keys():
+        if s in excludeKeys:
+            continue
         title = title + s + '\t'
         kl.append(s)
     print title
     for i in range(len(summary)):
         for k in kl: # keeps order
+            if k in excludeKeys:
+                continue
             print summary[i][k], '\t',
         print ''
+    print '----------------------------------'
     print '\n'
-#    print '%12s\t%d\t%8.3f\t%8.3f\t%8.3f\t%d\t' % (fn, reps, pulseDur*1000., startTime*1000., ipi*1000., nspikes)
+    # generate a summary that ranks data by pulse duration
+    # analysis:
+    # mean # spikes per stimulus (count spikes from stimulus onset to the ipi following
+    # mean latency of spikes vs stimulus number
+    # mean std of spikes vs stimulus number
+    # assumption: what varies is the pulse Duration, so we create a dictionary to organize the values
+    # and sequence over that.
+    pdurs = [x['PulseDur'] for x in summary]
+    npulses = [x['NPulses'] for x in summary]
+    reps = summary[0]['Reps'] # wont change in protocol
+    uniqDurs, uniqDursIndx = np.unique(pdurs, return_inverse=True)
+    ndur = len(uniqDurs)
+    npul = npulses[0] # assumption - the same number of pulses in each run
+    nspk = np.zeros((ndur, npul, reps))
+    lat = np.zeros((ndur, npul, reps))
+    durs = np.zeros((ndur, npul, reps))
+    slopes = np.zeros((ndur, npul, reps))
+    rep = [[0]*npul] * ndur
+    for du in range(len(summary)):
+        s = summary[du] # get summary for this duration
+        duration = s['PulseDur']
+        st = np.array(s['SpikeTimes'])
+        # now loop through and fill the arrays to make calculations
+        repc = s['thisRep']
+        for n in range(s['NPulses']):
+            t0 = s['startTime'] + n * s['IPI'] # start time for this pulse window
+            t1 = t0 + s['IPI'] # end time for this pulse window
+            x = np.intersect1d(np.where(st > t0)[0].tolist(), np.where(st <= t1)[0].tolist())
+            if len(x) > 0:
+                lat[uniqDursIndx[du], n, repc] = st[x[0]]-t0
+            else:
+                lat[uniqDursIndx[du], n, repc] = np.nan
+            durs[uniqDursIndx[du], n, repc] = duration # save the associated pulse duration
+            nspk[uniqDursIndx[du], n, repc] = len(x)
+            rep[uniqDursIndx[du]][n] = repc
+            slopes[uniqDursIndx[du], n, repc] = s['Slopes'][n]
 
+    meanlat = scipy.stats.nanmean(lat, axis=2)
+    meannspk = scipy.stats.nanmean(nspk, axis=2)
+    stdlat = scipy.stats.nanstd(lat, axis = 2)
+    meanslope = scipy.stats.nanmean(slopes, axis=2)
+    #print meanlat[0]
+    #print stdlat[0]
+    #print meannspk[0]
+    #print meanslope[0]
+    #print durs[:,:,0]
+
+    xmax = 0.
+    for i, plw in enumerate(plotWidget):
+        plw.plotItem.clear()
+#        plotWidget.plotItem.scatterPlot().clear()
+        if i == 0:
+            if npul > 2:
+                plw.plotItem.scatterPlot().setData(x=np.arange(npul), y=meanslope[0], symbol='o')
+                plw.plotItem.setLabel('left', 'Slope (V/s)')
+                plw.plotItem.setLabel('bottom', 'Pulse #')
+            else:
+                plw.plotItem.scatterPlot().setData(x = uniqDurs, y=[x[0] for x in meanslope], symbol='s')
+                plw.plotItem.setLabel('left', 'Slope (V/s)')
+                plw.plotItem.setLabel('bottom', 'Pulse Dur', 's')
+        elif i == 1:
+            if npul > 2:
+                plw.plotItem.scatterPlot().setData(x=np.arange(npul), y=meannspk[0], symbol='o')
+                plw.plotItem.setLabel('left', 'Spike Count')
+                plw.plotItem.setLabel('bottom', 'Pulse #')
+            else:
+                plw.plotItem.scatterPlot().setData(x = uniqDurs, y=[x[0] for x in meannspk], symbol='s')
+                plw.plotItem.setLabel('left', 'Spike Count')
+                plw.plotItem.setLabel('bottom', 'Pulse Dur', 's')
+        elif i == 2:
+            if npul > 2:
+                plw.plotItem.scatterPlot().setData(x=np.arange(npul), y=meanlat[0], symbol='o')
+                plw.plotItem.setLabel('left', 'Latency', 's')
+                plw.plotItem.setLabel('bottom', 'Pulse #')
+            else:
+                plw.plotItem.scatterPlot().setData(x = uniqDurs, y=[x[0] for x in meanlat], symbol='s')
+                plw.plotItem.setLabel('left', 'Latency', 's')
+                plw.plotItem.setLabel('bottom', 'Pulse Dur', 's')
+        plw.plotItem.autoRange()
+        view = plw.plotItem.viewRange()
+        if view[0][1] > xmax:
+            xmax = view[0][1]
+        plw.plotItem.setYRange(0., view[1][1])
+
+    for plw in plotWidget:
+        plw.plotItem.setXRange(0., xmax)
 
