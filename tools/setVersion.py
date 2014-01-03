c74b5d45bcf08d5f93e4a231b110fdfc2a7dd191
Luke Campagnola <luke.campagnola@gmail.com>
2014-01-03 05:11:08 -0500

    added innosetup to setversion script

--- tools/setVersion.py
+++ tools/setVersion.py
@@ -5,9 +5,10 @@ version = sys.argv[1]
 replace = [
     ("acq4/__init__.py", r"__version__ = .*", "__version__ = '%s'" % version),
     #("setup.py", r"    version=.*,", "    version='%s'," % version),  # setup.py automatically detects version
-    ("doc/source/conf.py", r"version = .*", "version = '%s'" % version),
-    ("doc/source/conf.py", r"release = .*", "release = '%s'" % version),
-    #("tools/debian/control", r"^Version: .*", "Version: %s" % version)
+    ("documentation/source/conf.py", r"version = .*", "version = '%s'" % version),
+    ("documentation/source/conf.py", r"release = .*", "release = '%s'" % version),
+    #("tools/debian/control", r"^Version: .*", "Version: %s" % version),
+    ("tools/innosetup.iss", r'#define AppVersion .*', '#define AppVersion "%s"' % version),
     ]
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
