900c14fc388d4184b19df5b642fa490dda0c11bb
Antonin Blot <antonin.blot@gmail.com>
2014-05-07 18:00:55 +0200

    Parameters of Patch modules can be changed in the config file
    Fixes https://github.com/acq4/acq4/issues/5
    
    Parameters of PatchWindow.params can now be changed by user
    configuration. Unknown parameters or parameters that cannot
    be converted to the proper data types are ignored
    
    TODO: - replace plane 'print' by showMessage?
          - (re)move the type checking to make it more generic and
            usable in every module?

--- acq4/modules/Patch/Patch.py
+++ acq4/modules/Patch/Patch.py
@@ -4,13 +4,52 @@ from PatchWindow import *
 import os
 from PyQt4 import QtGui
 
+
+
 class Patch(Module):
+    valid_params = {
+            'mode': str,
+            'rate': float,
+            'downsample': int,
+            'cycleTime': float,
+            'recordTime': float,
+            'delayTime': float,
+            'pulseTime': float,
+            'icPulse': float,
+            'vcPulse': float,
+            'icHolding': float,
+            'vcHolding': float,
+            'icHoldingEnabled': bool,
+            'icPulseEnabled': bool,
+            'vcHoldingEnabled': bool,
+            'vcPulseEnabled': bool,
+            'drawFit': bool,
+            'average': int,
+    }
+
     def __init__(self, manager, name, config):
         Module.__init__(self, manager, name, config)
         self.ui = PatchWindow(manager, config['clampDev'])
         self.ui.sigWindowClosed.connect(self.quit)
         mp = os.path.dirname(__file__)
         self.ui.setWindowIcon(QtGui.QIcon(os.path.join(mp, 'icon.png')))
+        
+        # Changing defaults using config file
+        for param, val in config.items():
+            if param == 'clampDev':
+                continue
+            if not self.valid_params.has_key(param):
+                # should probably use showMessage instead of print
+                print 'Ignoring unknown parameter in config file: "%s".'%param
+                continue
+            try:
+                self.ui.params[param]=self.valid_params[param](val)
+            except:
+                print 'Error while setting "%s". Expect a %s, got: "%s" (%s).'%(param, 
+                                                                              self.valid_params[param].__name__,
+                                                                              val, 
+                                                                              type(val).__name__)
+                print 'Default value will be used.'
     
     def window(self):
         return self.ui
