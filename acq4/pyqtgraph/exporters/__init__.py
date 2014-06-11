5c0624e0e75dd2afa79ea8a7f1a8d28800faf1db
Paul manis <pmanis@med.unc.edu>
2014-06-11 13:23:50 -0400

    pyqtgraph: exporters. Added two HDF5 exporters (one x axis, multiple y, and one x for each y). Also changed __init__.py to recognize the exporters.

--- acq4/pyqtgraph/exporters/__init__.py
+++ acq4/pyqtgraph/exporters/__init__.py
@@ -4,6 +4,8 @@ from .SVGExporter import *
 from .Matplotlib import *
 from .CSVExporter import *
 from .PrintExporter import *
+from .HDF5Exporter import *
+from .HDF5Exporter2 import *
 
 
 def listExporters():
