455614fd8b8418359f3bbe959230288ba9817829
Luke Campagnola <luke.campagnola@gmail.com>
2014-12-03 17:35:30 -0500

    Added spiral scan control (no protocol generation yet)

--- acq4/devices/Scanner/ScanProgram/spiral.py
+++ acq4/devices/Scanner/ScanProgram/spiral.py
@@ -46,6 +46,7 @@ class SpiralScanComponent(ScanProgramComponent):
         Generate mirror voltages for this scan component and store inside
         *array*. Returns the start and stop indexes used by this component.
         """
+        # NOTE: point should have constant speed regardless of radius.
         raise NotImplementedError()
         
     def generatePosCmd(self, array):
@@ -57,61 +58,46 @@ class SpiralScanComponent(ScanProgramComponent):
         raise NotImplementedError()
 
 
-#class SpiralScanROI(pg.ROI):
-    #def __init__(self, pos=None, radius=None, **args):
-        #pg.ROI.__init__(self, pos, size=(radius, radius), **args)
-        ##self.translateSnap = False
-        #self.addFreeHandle([0.25,0], name='a')
-        #self.addRotateFreeHandle([1,0], [0,0], name='r')
+class SpiralScanROI(pg.ROI):
+    def __init__(self, pos=None, radius=None, **args):
+        self.angles = [0, 8 * np.pi]
+        self.radii = [radius * 0.75, radius]
+        self._path = None
+        size = (radius*2,) * 2
+        pg.ROI.__init__(self, pos, size, **args)
+        self.aspectLocked = True
+        self.addScaleHandle([0.5*2.**-0.5 + 0.5, 0.5*2.**-0.5 + 0.5], 
+                            [0.5, 0.5], name='outer')        
         
-    #def getRadius(self):
-        #radius = pg.Point(self.handles[1]['item'].pos()).length()
-        #return radius
-    
-    #def boundingRect(self):
-        #r = self.getRadius()
-        #return QtCore.QRectF(-r*1.1, -r*1.1, 2.2*r, 2.2*r)
-            
-    #def stateChanged(self, finish=True):
-        #pg.ROI.stateChanged(self, finish=finish)
-        #if len(self.handles) > 1:
-            #self.path = QtGui.QPainterPath()
-            #h0 = pg.Point(self.handles[0]['item'].pos()).length()
-            #a = h0/(2.0*np.pi)
-            #theta = 30.0*(2.0*np.pi)/360.0
-            #self.path.moveTo(QtCore.QPointF(a*theta*np.cos(theta), a*theta*np.sin(theta)))
-            #x0 = a*theta*np.cos(theta)
-            #y0 = a*theta*np.sin(theta)
-            #radius = self.getRadius()
-            #theta += 20.0*(2.0*np.pi)/360.0
-            #i = 0
-            #while pg.Point(x0, y0).length() < radius and i < 1000:
-                #x1 = a*theta*np.cos(theta)
-                #y1 = a*theta*np.sin(theta)
-                #self.path.lineTo(QtCore.QPointF(x1,y1))
-                #theta += 20.0*(2.0*np.pi)/360.0
-                #x0 = x1
-                #y0 = y1
-                #i += 1
-            #return self.path
+    def setAngles(self, start, stop):
+        self.angles = [start, stop]
+        self._path = None
+        self.update()
         
-    #def shape(self):
-        #p = QtGui.QPainterPath()
-        #p.addEllipse(self.boundingRect())
-        #return p
+    def setRadii(self, start, stop):
+        self.radii = [start, stop]
+        self.setSize([stop*2, stop*2])
+        self._path = None
     
-    #def paint(self, p, *args):
-        #p.setRenderHint(QtGui.QPainter.Antialiasing)
-        #p.setPen(self.currentPen)
-        #p.drawPath(self.path)
-        #p.setPen(QtGui.QPen(QtGui.QColor(255,0,0)))
-        #p.drawPath(self.shape())
-        #p.setPen(QtGui.QPen(QtGui.QColor(0,0,255)))
-        #p.drawRect(self.boundingRect())
-
-class SpiralScanROI(pg.CircleROI):
-    def __init__(self, pos, radius):
-        pg.CircleROI.__init__(self, pos, (radius, radius))
+    def stateChanged(self, finish=True):
+        self._path = None
+        pg.ROI.stateChanged(self, finish=finish)
+    
+    def paint(self, p, *args):
+        if self._path is None:
+            radii = self.radii
+            angles = self.angles
+            # draw 30 pts per turn
+            npts = min(5000, int(30 * abs(angles[1] - angles[0]) / np.pi))
+            theta = np.linspace(angles[0], angles[1], npts)
+            r = np.linspace(radii[0], radii[1], npts)
+            x = r * np.cos(theta) + radii[1]
+            y = r * np.sin(theta) + radii[1]
+            self._path = pg.arrayToQPath(x, y)
+        p.setRenderHint(QtGui.QPainter.Antialiasing)
+        p.setPen(self.currentPen)
+        p.drawPath(self._path)
+        
 
 
 class SpiralScanControl(QtCore.QObject):
@@ -128,10 +114,10 @@ class SpiralScanControl(QtCore.QObject):
 
         self.params.component = self.component
         
-        self.roi = SpiralScanROI(pos=[0.0, 0.0], radius=self.params['outer radius'])
+        self.roi = SpiralScanROI(pos=[0.0, 0.0], radius=self.params['radius'])
 
         self.params.sigTreeStateChanged.connect(self.paramsChanged)
-        self.roi.sigRegionChangeFinished.connect(self.roiChanged)
+        self.roi.sigRegionChanged.connect(self.roiChanged)
         self.paramsChanged()
         
     def getGraphicsItems(self):
@@ -154,28 +140,38 @@ class SpiralScanControl(QtCore.QObject):
         return self.params
 
     def paramsChanged(self, param=None, changes=None):
+        outer = self.params['radius']
+        inner = outer - (outer * self.params['thickness'] / 100.)
+        self.roi.setRadii(inner, outer)
+        
+        spacing = self.params['spacing']
+        turns = (outer - inner) / spacing
+        a0 = 0
+        a1 = 2 * np.pi * turns
+        self.roi.setAngles(a0, a1)
+        
+        # compute length of spiral:
+        # This is L = (spacing / 2pi) * Integral(sqrt(Theta^2 + 1) dTheta)
+        # = sp/2pi * (a1/2 sqrt(a1^2+1) + 1/2 ln(a1 + sqrt(a1^2+1)) - 
+        #             a0/2 sqrt(a0^2+1) - 1/2 ln(a0 + sqrt(a0^2+1)))
+        s0 = (a0**2 + 1)**0.5
+        s1 = (a1**2 + 1)**0.5
+        slen = ((spacing / 2*np.pi) * 
+                ((a1 / 2.) * s1 + 0.5 * np.log(a1 + s1)) - 
+                ((a0 / 2.) * s0 + 0.5 * np.log(a0 + s0)))
+        self.params['speed'] = 1e-3 * slen / self.params['duration']
         self.update()
         
     def update(self):
         pass
     
     def roiChanged(self):
-        """ read the ROI rectangle width and height and repost
-        in the parameter tree """
-        #state = self.roi.getState()
-        #w, h = state['size']
-        #self.params.system.p0 = pg.Point(self.roi.mapToView(pg.Point(0,0)))
-        #self.params.system.p1 = pg.Point(self.roi.mapToView(pg.Point(w,0)))
-        #self.params.system.p2 = pg.Point(self.roi.mapToView(pg.Point(0,h)))
+        """ read the ROI size and repost in the parameter tree """
+        state = self.roi.getState()
+        w, h = state['size']
+        self.params['radius'] = w / 2.
         
     def generateTask(self):
-        #state = self.roi.getState()
-        #w, h = state['size']
-        #p0 = pg.Point(0,0)
-        #p1 = pg.Point(w,0)
-        #p2 = pg.Point(0, h)
-        #points = [p0, p1, p2]
-        #points = [pg.Point(self.roi.mapToView(p)) for p in points] # convert to view points (as needed for scanner)
         task = {'type': self.name, 'active': self.isActive(), 'scanInfo': {'pos': (0,0)}}
         return task
 
@@ -187,11 +183,12 @@ class SpiralScanParameter(pTypes.SimpleParameter):
     def __init__(self):
         fixed = [{'name': 'fixed', 'type': 'bool', 'value': True}] # child of parameters that may be determined by the user
         params = [
-            dict(name='outer radius', readonly=True, type='float', value=2e-5, suffix='m', siPrefix=True, bounds=[1e-6, None], step=1e-6),
-            dict(name='inner radius', readonly=True, type='float', value=1e-5, suffix='m', siPrefix=True, bounds=[1e-6, None], step=1e-6),
-            dict(name='turns', type='float', value=10, step=1, bounds=[0, None]),
+            dict(name='radius', type='float', value=2e-5, suffix='m', siPrefix=True, bounds=[1e-6, None], step=1e-6),
+            dict(name='thickness', type='float', value=25, suffix='%', bounds=[0, 100], step=1),
+            dict(name='spacing', type='float', value=2e-6, suffix='m', siPrefix=True, step=0.5e-6, bounds=[1e-7, None]),
             dict(name='duration', type='float', value=0.002, suffix='s', bounds=[1e-6, None], siPrefix=True, step=1e-3),
+            dict(name='speed', type='float', readonly=True, value=0, suffix='m/ms', siPrefix=True),
         ]
-        pTypes.SimpleParameter.__init__(self, name='rect_scan', type='bool', value=True, removable=True, renamable=True, children=params)
+        pTypes.SimpleParameter.__init__(self, name='spiral_scan', type='bool', value=True, removable=True, renamable=True, children=params)
 
         #self.sigTreeStateChanged.connect(self.updateSystem)
