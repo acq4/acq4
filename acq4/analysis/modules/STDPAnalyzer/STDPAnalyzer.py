47a341e1be751fe38760c840542273296c22874d
Megan Kratz <meganbkratz@gmail.com>
2015-05-01 12:30:07 -0400

    Started implementing averaging

--- acq4/analysis/modules/STDPAnalyzer/STDPAnalyzer.py
+++ acq4/analysis/modules/STDPAnalyzer/STDPAnalyzer.py
@@ -38,7 +38,7 @@ class STDPAnalyzer(AnalysisModule):
         self.initializeElements()
            
         
-        ## Set labels/titles on plots
+        ## Set labels/titles on plots 
         self.plots.exptPlot.setTitle('Experiment Timecourse')
         self.plots.tracesPlot.setLabel('left', "Voltage") ### TODO: check whether traces are in VC or IC
         self.plots.tracesPlot.setTitle("Data")
@@ -58,10 +58,17 @@ class STDPAnalyzer(AnalysisModule):
         self.traceSelectRgn.sigRegionChanged.connect(self.updateTracesPlot)
 
         self.traces = np.array([], dtype=[('timestamp', float), ('data', object)]) 
+        self.averagedTraces = None
+        self.lastAverageState = {}
+        self.resetAveragedTraces()
         self.files = []
-        #self.timeTicks = pg.VTickGroup(yrange=[0,1])
 
-        #self.plots.exptPlot.addItem(self.timeTicks)
+
+        ### Connect control panel
+        self.averageCtrl = pg.WidgetGroup(self.ctrl.traceDisplayGroup) ##TODO: save state when we save data
+        self.ctrl.averageTimeSpin.setOpts(suffix='s', siPrefix=True, dec=True, value=60, step=1)
+        self.averageCtrl.sigChanged.connect(self.averageCtrlChanged)
+
 
 
     def loadFileRequested(self, files):
@@ -87,6 +94,7 @@ class STDPAnalyzer(AnalysisModule):
                     if dlg.wasCanceled():
                         return
                 self.traces = np.concatenate((self.traces, arr))
+                self.lastAverageState = {}
 
         self.updateExptPlot()
 
@@ -94,16 +102,122 @@ class STDPAnalyzer(AnalysisModule):
         self.expStart = self.traces['timestamp'].min()
         # self.timeTicks.setXVals(list(self.traces['timestamp']-expStart))
         self.plots.exptPlot.plot(x=self.traces['timestamp']-self.expStart, y=[1]*len(self.traces), pen=None, symbol='o')
+        if self.ctrl.averageCheck.isChecked():
+            self.plots.exptPlot.plot(x=self.averagedTraces['avgTimeStamp']-self.expStart, y=y[2]*len(self.averagedTraces), pen=None, symbol='o', symbolBrush=(255,0,0))
 
     def updateTracesPlot(self):
-        print "updateTracesPlot"
         rgn = self.traceSelectRgn.getRegion()
-        print rgn
-        data = self.traces[(self.traces['timestamp'] > rgn[0]+self.expStart)*(self.traces['timestamp'] < rgn[1]+self.expStart)]['data']
-        print data.shape
         self.plots.tracesPlot.clear()
-        for d in data:
-            self.plots.tracesPlot.plot(d['primary'])
+        if not self.ctrl.averageCheck.isChecked():
+            data = self.traces[(self.traces['timestamp'] > rgn[0]+self.expStart)*(self.traces['timestamp'] < rgn[1]+self.expStart)]['data']
+            for i, d in enumerate(data):
+                self.plots.tracesPlot.plot(d['primary'], pen=pg.intColor(i, len(data)))
+
+        if self.ctrl.averageCheck.isChecked():
+            data = self.averagedTraces[(self.averagedTraces['avgTimeStamp'] > rgn[0]+self.expStart)*(self.averagedTraces['avgTimeStamp'] < rgn[1]+self.expStart)]
+            displayOrig = self.ctrl.displayTracesCheck.isChecked()
+            for i, d in enumerate(data['avgData']):
+                self.plots.tracesPlot.plot(d['primary'], pen=pg.intColor(i, len(data)))
+                if displayOrig:
+                    for t in data['origTimes']:
+                        orig = self.traces[self.traces['timestamp']==t]['data']
+                        self.plots.tracesPlot.plot(orig['primary'], pen=pg.intcolor(i, len(data), alpha=100))
+
+
+    def resetAveragedTraces(self, n=0):
+        ## only define the array in one place (here)
+        self.averagedTraces = np.zeros(n, dtype=[('avgTimeStamp', float), ('avgData', object), ('origTimes', object)])
+
+    def averageCtrlChanged(self):
+        if not self.ctrl.averageCheck.isChecked(): ## if we're not averaging anyway, we don't need to do anything
+            return
+        if not self.needNewAverage(): ## if the parameters for averaging didn't change, we don't need to do anything
+            return
+
+        self.getNewAverages()
+        self.updateExptPlot()
+
+    def needNewAverage(self):
+        ### Checks if the current values for the averaging controls are the same as when we last averaged
+        if self.ctrl.averageTimeRadio.isChecked():
+            method = 'time'
+            value = self.ctrl.averageTimeSpin.value()
+            if (method == self.lastAverageState.get('method', None)) and (value == self.lastAverageState.get('value', None)):
+                return False
+            else:
+                return True
+
+        elif self.ctrl.averageNumberRadio.isChecked():
+            method = 'number'
+            value = self.ctrl.averageNumberSpin.value()
+            if (method == self.lastAverageState.get('method', None)) and (value == self.lastAverageState.get('value', None)):
+                return False
+            else:
+                return True
+
+        else:
+            return True
+
+
+    def getNewAverages(self):
+
+        if self.ctrl.averageTimeRadio.isChecked():
+            method = 'time'
+            value = self.ctrl.averageTimeSpin.value()
+            self.averageByTime(value)
+        elif self.ctrl.averageNumberRadio.isChecked():
+            method = 'number'
+            value = self.ctrl.averageNumberSpin.value()
+            self.averageByNumber(value)
+        else:
+            raise Exception("Unable to average traces. Please make sure an averaging method is selected.")
+
+        self.lastAverageState = {'method': method, 'value': value}
+
+    def averageByTime(self, time):
+        t = 0
+        i = 0
+        n = int((self.traces['timestamp'].max() - self.expStart)/time) + ((self.traces['timestamp'].max() - self.expStart) % time > 0) ### weird solution for rounding up
+        self.resetAveragedTraces(n)
+        while t < len(self.traces):
+            traces = self.traces[(self.traces['timestamp'] > self.expStart+time*i)*(self.traces['timestamp'] < self.expStart+time*i+time)]
+            if len(traces) > 0:
+                x = traces[0]['data']['primary']
+                for t2 in traces[1:]:
+                    x += t2['data']['primary']
+                x /= float(len(traces))
+            else:
+                continue
+            print i, self.averagedTraces.shape
+            self.averagedTraces[i]['avgTimeStamp'] = traces['timestamp'].mean()
+            self.averagedTraces[i]['avgData'] = x
+            self.averagedTraces[i]['origTimes'] = list(traces['timestamp'])
+            t += len(traces)
+            i += 1
+
+    def averageByNumber(self, number):
+        t = 0
+        i = 0
+        n = int(len(traces)/number) + (len(traces) % number > 0) ### weird solution for rounding up
+        self.resetAveragedTraces(n)
+
+        while t < len(traces):
+            traces = self.traces[i:i+number]
+
+            x = traces[0]['data']['primary']
+            for t2 in traces[1:]:
+                x += t2['data']['primary']
+            x /= float(len(traces))
+
+            self.averagedTraces[i]['avgTimeStamp'] = traces['timestamp'].mean()
+            self.averagedTraces[i]['avgData'] = x
+            self.averagedTraces[i]['origTimes'] = list(traces['timestamp'])
+            t += len(traces)
+            i += 1
+
+
+
+
 
 
 
--- acq4/analysis/modules/STDPAnalyzer/STDPControlTemplate.py
+++ acq4/analysis/modules/STDPAnalyzer/STDPControlTemplate.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'acq4/analysis/modules/STDPAnalyzer/STDPControlTemplate.ui'
 #
-# Created: Thu Apr 30 08:56:35 2015
+# Created: Fri May  1 11:35:43 2015
 #      by: PyQt4 UI code generator 4.11.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,17 +26,66 @@ except AttributeError:
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(320, 387)
+        Form.resize(252, 139)
         self.gridLayout = QtGui.QGridLayout(Form)
+        self.gridLayout.setMargin(3)
+        self.gridLayout.setSpacing(3)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.groupBox = QtGui.QGroupBox(Form)
-        self.groupBox.setObjectName(_fromUtf8("groupBox"))
-        self.gridLayout.addWidget(self.groupBox, 0, 0, 1, 1)
+        self.traceDisplayGroup = QtGui.QGroupBox(Form)
+        self.traceDisplayGroup.setObjectName(_fromUtf8("traceDisplayGroup"))
+        self.gridLayout_3 = QtGui.QGridLayout(self.traceDisplayGroup)
+        self.gridLayout_3.setMargin(3)
+        self.gridLayout_3.setSpacing(3)
+        self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
+        self.averageCheck = QtGui.QCheckBox(self.traceDisplayGroup)
+        self.averageCheck.setObjectName(_fromUtf8("averageCheck"))
+        self.gridLayout_3.addWidget(self.averageCheck, 0, 0, 1, 2)
+        self.gridLayout_2 = QtGui.QGridLayout()
+        self.gridLayout_2.setHorizontalSpacing(1)
+        self.gridLayout_2.setVerticalSpacing(3)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.averageTimeRadio = QtGui.QRadioButton(self.traceDisplayGroup)
+        self.averageTimeRadio.setChecked(True)
+        self.averageTimeRadio.setObjectName(_fromUtf8("averageTimeRadio"))
+        self.gridLayout_2.addWidget(self.averageTimeRadio, 0, 0, 1, 1)
+        self.averageTimeSpin = SpinBox(self.traceDisplayGroup)
+        self.averageTimeSpin.setObjectName(_fromUtf8("averageTimeSpin"))
+        self.gridLayout_2.addWidget(self.averageTimeSpin, 0, 1, 1, 2)
+        self.averageNumberRadio = QtGui.QRadioButton(self.traceDisplayGroup)
+        self.averageNumberRadio.setObjectName(_fromUtf8("averageNumberRadio"))
+        self.gridLayout_2.addWidget(self.averageNumberRadio, 1, 0, 1, 2)
+        self.averageAnalysisCheck = QtGui.QCheckBox(self.traceDisplayGroup)
+        self.averageAnalysisCheck.setMinimumSize(QtCore.QSize(0, 20))
+        self.averageAnalysisCheck.setObjectName(_fromUtf8("averageAnalysisCheck"))
+        self.gridLayout_2.addWidget(self.averageAnalysisCheck, 2, 0, 1, 3)
+        self.averageNumberSpin = SpinBox(self.traceDisplayGroup)
+        self.averageNumberSpin.setDecimals(0)
+        self.averageNumberSpin.setMaximum(1000.0)
+        self.averageNumberSpin.setProperty("value", 5.0)
+        self.averageNumberSpin.setObjectName(_fromUtf8("averageNumberSpin"))
+        self.gridLayout_2.addWidget(self.averageNumberSpin, 1, 2, 1, 1)
+        self.displayTracesCheck = QtGui.QCheckBox(self.traceDisplayGroup)
+        self.displayTracesCheck.setMinimumSize(QtCore.QSize(0, 20))
+        self.displayTracesCheck.setObjectName(_fromUtf8("displayTracesCheck"))
+        self.gridLayout_2.addWidget(self.displayTracesCheck, 3, 0, 1, 3)
+        self.gridLayout_2.setColumnStretch(2, 4)
+        self.gridLayout_3.addLayout(self.gridLayout_2, 1, 1, 1, 1)
+        spacerItem = QtGui.QSpacerItem(10, 20, QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout_3.addItem(spacerItem, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.traceDisplayGroup, 0, 0, 1, 1)
+        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem1, 1, 0, 1, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(_translate("Form", "Form", None))
-        self.groupBox.setTitle(_translate("Form", "GroupBox", None))
+        self.traceDisplayGroup.setTitle(_translate("Form", "Trace Display:", None))
+        self.averageCheck.setText(_translate("Form", "Average traces based on:", None))
+        self.averageTimeRadio.setText(_translate("Form", "Time:", None))
+        self.averageNumberRadio.setText(_translate("Form", "Number:", None))
+        self.averageAnalysisCheck.setText(_translate("Form", "Use averaged traces for analysis", None))
+        self.displayTracesCheck.setText(_translate("Form", "Display original traces (slow)", None))
 
+from acq4.pyqtgraph.widgets.SpinBox import SpinBox
