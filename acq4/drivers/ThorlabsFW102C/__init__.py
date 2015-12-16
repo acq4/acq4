aa0f05f2e7bee575781a2bf42014e4ce7bc64367
maxandme <max.quiguer@gmail.com>
2015-12-16 13:05:48 +0100

    first commit of new filter-wheel branch

---
+++ acq4/drivers/ThorlabsFW102C/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from thorFW102cDriver import *
---
+++ acq4/drivers/ThorlabsFW102C/fw_test.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('..\\..\\..\\')
+
+import thorFW102cDriver
+
+pco = thorFW102cDriver.filterWheelDriverClass()
+#cam = pco.getCamera('pixelfly')
+#cam.close()
