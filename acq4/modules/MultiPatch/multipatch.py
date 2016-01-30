f4d4e1c65bb055e4582c37460f58d6be5e5df740
Luke Campagnola <lukec@alleninstitute.org>
2016-01-29 17:32:17 -0800

    More multipatch functionality

--- acq4/modules/MultiPatch/multipatch.py
+++ acq4/modules/MultiPatch/multipatch.py
@@ -26,7 +26,7 @@ class MultiPatchWindow(QtGui.QWidget):
         self.matrix = QtGui.QWidget()
         self.matrixLayout = QtGui.QGridLayout()
         self.matrix.setLayout(self.matrixLayout)
-        self.layout.addWidget(self.matrix, 0, 0, 1, 2)
+        self.layout.addWidget(self.matrix, 0, 0)
 
         man = getManager()
         pipNames = man.listInterfaces('pipette')
@@ -49,25 +49,104 @@ class MultiPatchWindow(QtGui.QWidget):
             for b in btns:
                 b.pipette = pip
 
+        self.movementWidget = QtGui.QWidget()
+        self.layout.addWidget(self.movementWidget)
+
+        self.movementLayout = QtGui.QGridLayout()
+        self.movementWidget.setLayout(self.movementLayout)
+
         self.moveInBtn = QtGui.QPushButton("Move in")
         self.moveOutBtn = QtGui.QPushButton("Move out")
-        self.layout.addWidget(self.moveInBtn, 1, 0)
-        self.layout.addWidget(self.moveOutBtn, 1, 1)
+        self.moveAboveTargetBtn = QtGui.QPushButton("Above target")
+        self.moveApproachBtn = QtGui.QPushButton("Approach")
+        self.moveToTargetBtn = QtGui.QPushButton("To target")
+        self.moveHomeBtn = QtGui.QPushButton("Home")
+        self.moveSearchBtn = QtGui.QPushButton("Search")
+        self.moveIdleBtn = QtGui.QPushButton("Idle")
+
+        self.movementLayout.addWidget(self.moveInBtn, 0, 0)
+        self.movementLayout.addWidget(self.moveOutBtn, 0, 1)
+        self.movementLayout.addWidget(self.moveAboveTargetBtn, 0, 2)
+        self.movementLayout.addWidget(self.moveApproachBtn, 0, 3)
+        self.movementLayout.addWidget(self.moveToTargetBtn, 0, 4)
+        self.movementLayout.addWidget(self.moveHomeBtn, 0, 5)
+        self.movementLayout.addWidget(self.moveSearchBtn, 0, 6)
+        self.movementLayout.addWidget(self.moveIdleBtn, 0, 7)
+
+        self.stepSizeSpin = pg.SpinBox(value=10e-6, suffix='m', siPrefix=True, limits=[5e-6, None], step=5e-6)
+        self.stepSizeLabel = QtGui.QLabel('Step size')
+        self.fastBtn = QtGui.QPushButton('Fast')
+        self.fastBtn.setCheckable(True)
+        self.slowBtn = QtGui.QPushButton('Slow')
+        self.slowBtn.setCheckable(True)
+
+        self.movementLayout.addWidget(self.stepSizeLabel, 1, 0)
+        self.movementLayout.addWidget(self.stepSizeSpin, 1, 1)
+        self.movementLayout.addWidget(self.slowBtn, 1, 2)
+        self.movementLayout.addWidget(self.fastBtn, 1, 3)
+
 
         self.moveInBtn.clicked.connect(self.moveIn)
         self.moveOutBtn.clicked.connect(self.moveOut)
+        self.moveAboveTargetBtn.clicked.connect(self.moveAboveTarget)
+        self.moveApproachBtn.clicked.connect(self.moveApproach)
+        self.moveToTargetBtn.clicked.connect(self.moveToTarget)
+        self.moveHomeBtn.clicked.connect(self.moveHome)
+        self.moveIdleBtn.clicked.connect(self.moveIdle)
+        self.moveSearchBtn.clicked.connect(self.moveSearch)
+
+        self.fastBtn.clicked.connect(lambda: self.slowBtn.setChecked(False))
+        self.slowBtn.clicked.connect(lambda: self.fastBtn.setChecked(False))
 
     def moveIn(self):
         for pip in self.selectedPipettes():
-            pip.advanceTowardTarget(10e-6)
+            pip.advanceTowardTarget(self.stepSizeSpin.value())
 
     def moveOut(self):
         for pip in self.selectedPipettes():
-            pip.retract(10e-6)
+            pip.retract(self.stepSizeSpin.value())
+
+    def moveAboveTarget(self):
+        speed = self.selectedSpeed(default='fast')
+        for pip in self.selectedPipettes():
+            pip.goAboveTarget(speed)
+
+    def moveApproach(self):
+        speed = self.selectedSpeed(default='slow')
+        for pip in self.selectedPipettes():
+            pip.goApproach(speed)
+
+    def moveToTarget(self):
+        speed = self.selectedSpeed(default='slow')
+        for pip in self.selectedPipettes():
+            pip.goTarget(speed)
+
+    def moveHome(self):
+        speed = self.selectedSpeed(default='fast')
+        for pip in self.selectedPipettes():
+            pip.goHome(speed)
+
+    def moveSearch(self):
+        speed = self.selectedSpeed(default='fast')
+        for pip in self.selectedPipettes():
+            pip.goSearch(speed)
+
+    def moveIdle(self):
+        speed = self.selectedSpeed(default='fast')
+        for pip in self.selectedPipettes():
+            pip.goIdle(speed)
 
     def selectedPipettes(self):
         return [col[0].pipette for col in self.columns if col[0].isChecked()]
 
+    def selectedSpeed(self, default):
+        if self.fastBtn.isChecked():
+            self.fastBtn.setChecked(False)
+            return 'fast'
+        if self.slowBtn.isChecked():
+            self.slowBtn.setChecked(False)
+            return 'slow'
+        return default
 
 
 
