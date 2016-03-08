3ebc5afec4f68663fdd9be6f84fc8a106d369b0e
Luke Campagnola <lukec@alleninstitute.org>
2016-03-08 09:25:46 -0800

    Patch module gets configurable sample rate and uses lower default sample rate

--- acq4/modules/Patch/Patch.py
+++ acq4/modules/Patch/Patch.py
@@ -7,7 +7,7 @@ from PyQt4 import QtGui
 class Patch(Module):
     def __init__(self, manager, name, config):
         Module.__init__(self, manager, name, config)
-        self.ui = PatchWindow(manager, config['clampDev'])
+        self.ui = PatchWindow(manager, config)
         self.ui.sigWindowClosed.connect(self.quit)
         mp = os.path.dirname(__file__)
         self.ui.setWindowIcon(QtGui.QIcon(os.path.join(mp, 'icon.png')))
