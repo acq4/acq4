3bb7fa7ea7c984a1a8203d91f365845c3b1e8427
mgraupe <michael.graupner@parisdescartes.fr>
2016-05-10 11:43:10 +0200

    renamed photodetector device to PMT

--- acq4/devices/PMT/PMT.py
+++ acq4/devices/PMT/PMT.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 from acq4.devices.Device import *
 from acq4.devices.OptomechDevice import OptomechDevice
+from acq4.devices.DAQGeneric import DAQGeneric
 
-class PhotoDetector(DAQGeneric, OptomechDevice):
+class PMT(DAQGeneric, OptomechDevice):
     def __init__(self, dm, config, name):
-        print 'PMT configure'
         omConf = {}
         for k in ['parentDevice', 'transform']:
             if k in config:
                 omConf[k] = config.pop(k)
         OptomechDevice.__init__(self, dm, config, name)
         DAQGeneric.__init__(self, dm, config, name)
+        print omConf
 
     def getFilterDevice(self):
         # return parent filter device or None
+        pass
         
         
