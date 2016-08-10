fb6274d9152c1d338070fe5aa1208858587b99ca
Stephanie Seeman <stephanies@alleninstitute.org>
2016-08-10 16:05:15 -0700

    Add motion card v3 support to scientifica driver

--- acq4/drivers/Scientifica/scientifica.py
+++ acq4/drivers/Scientifica/scientifica.py
@@ -44,6 +44,18 @@ class Scientifica(SerialDevice):
 
         # search for device with this description
         dev2 = Scientifica(name='SliceScope')
+
+
+    Notes
+    -----
+
+    The `getParam()` and `setParam()` methods provide access to several speed and acceleration values
+    used by the motion card. These values are interpreted differently between version 2 and version 3
+    motion cards. To avoid possible device damage, it is required to explicitly instantiate this class
+    using the argument `ctrl_version=3` when connecting to version 3 devices. 
+
+    Version 3 device documentation is available at
+    http://elecsoft.eu/motion2/commandset/Motion%20Card%202%20Command%20Documentation.html 
     """
     openDevices = {}
     availableDevices = None
@@ -72,13 +84,13 @@ class Scientifica(SerialDevice):
                 name = cls.openDevices[com].getDescription()
                 devs[name] = com
             else:
-                s = Scientifica(port=com)
+                s = Scientifica(port=com, ctrl_version=None)
                 devs[s.getDescription()] = com
                 s.close()
 
         cls.availableDevices = devs
 
-    def __init__(self, port=None, name=None, baudrate=None):
+    def __init__(self, port=None, name=None, baudrate=None, ctrl_version=2):
         self.lock = RLock()
 
         if name is not None:
@@ -107,12 +119,20 @@ class Scientifica(SerialDevice):
         else:
             raise ValueError('invalid baudrate %s' % baudrate)
 
+        # Attempt connection
         connected = False
         for baudrate in baudrates:
             try:
                 SerialDevice.__init__(self, port=self.port, baudrate=baudrate)
-                self.write('type\r')
-                self.readUntil('\r', timeout=1)
+                try:
+                    sci = self.send('scientifica', timeout=0.2)
+                except RuntimeError:
+                    # try again because prior communication at a different baud rate may have garbled serial communication.
+                    sci = self.send('scientifica', timeout=1.0)
+
+                if sci != 'Y519':
+                    # Device responded, not scientifica.
+                    raise ValueError("Received unexpected response from device at %s. (Is this a scientifica device?)" % port)
                 connected = True
                 break
             except TimeoutError:
@@ -122,6 +142,12 @@ class Scientifica(SerialDevice):
             raise RuntimeError("No response received from Scientifica device at %s. (tried baud rates: %s)" % (port, ', '.join(map(str, baudrates))))
 
         Scientifica.openDevices[self.port] = self
+        self._version = float(self.send('ver'))
+        if ctrl_version is not None and ((self._version >= 3) != (ctrl_version >= 3)):
+            name = self.getDescription()
+            raise RuntimeError("Scientifica device %s uses controller version %s, but version %s was requested. Warning: speed and acceleration"
+                               " parameter values are NOT compatible between controller versions." % (name, self._version, ctrl_version))
+
         self._readAxisScale()
 
     def close(self):
@@ -129,10 +155,10 @@ class Scientifica(SerialDevice):
         SerialDevice.close(self)
         del Scientifica.openDevices[port]
 
-    def send(self, msg):
+    def send(self, msg, timeout=5):
         with self.lock:
             self.write(msg + '\r')
-            result = self.readUntil('\r')[:-1]
+            result = self.readUntil('\r', timeout=timeout)[:-1]
             if result.startswith('E,'):
                 errno = int(result.strip()[2:])
                 exc = RuntimeError("Received error %d from Scientifica controller (request: %r)" % (errno, msg))
@@ -150,7 +176,11 @@ class Scientifica(SerialDevice):
         types = {
             '1': 'linear', '2': 'ums', '3': 'mmtp', '4': 'slicemaster', '5': 'patchstar',
             '6': 'mmsp', '7': 'mmsp_z', '1.05': 'patchstar', '1.08': 'microstar', '1.09': 'ums', '1.10': 'imtp',
-            '1.11': 'slice_scope', '1.12': 'condenser', '1.13': 'mmbp', '1.14': 'ivm_manipulator'
+            '1.11': 'slice_scope', '1.12': 'condenser', '1.13': 'mmbp', '1.14': 'ivm_manipulator',
+            '3.01': 'linear', '3.02': 'ums', '3.03': 'mmtp', '3.04': 'slicemaster', '3.05': 'patchstar',
+            '3.06': 'mmsp', '3.07': 'mmsp_z', '3.08': 'microstar', '3.09': 'ums', '3.10': 'imtp',
+            '3.11': 'slicescope', '3.12': 'condenser', '3.13': 'mmbp', '3.14': 'ivm_manipulator', '3.15': 'custom',
+            '3.16': 'extended_patchstar', '3.17': 'ivm_mini',
         }
         typ = self.send('type')
         return types.get(typ, typ)
@@ -232,6 +262,16 @@ class Scientifica(SerialDevice):
             * approachMode: Boolean indicating whether the manipulator is in approach mode. This can be
               set programmatically or by toggling the approach switch on the input device (but to
               prevent user confusion, setting this value programmatically is discouraged).
+
+        Notes
+        -----
+
+        For version 2 devices, all speed and acceleration values are expressed relative to the device's
+        user step parameter. Speed values are expressed as `actual_speed (um/sec) * 2 * userScale[axis]`,
+        and acceleration values are expressed as `actual_accel (um^2/sec) * userScale[axis] / 250`. 
+
+        For version 3 devices, these values are expressed in units of um/sec and um^2/sec, with no scaling
+        applied.
         """
         cmd, _, typ = self._param_commands[name]
         if typ is bool:
@@ -275,7 +315,10 @@ class Scientifica(SerialDevice):
         and generally should not be changed. They may also be negative to reverse the stage direction
         under both manual and programmatic control.
         """
-        cmd = ['UUX', 'UUY', 'UUZ'][axis]
+        if self._version < 3:
+            cmd = ['UUX', 'UUY', 'UUZ'][axis]
+        else:
+            cmd = ['USTEP X', 'USTEP Y', 'USTEP Z'][axis]
         return float(self.send(cmd))
 
     def setAxisScale(self, axis, scale):
@@ -287,14 +330,20 @@ class Scientifica(SerialDevice):
         and generally should not be changed. They may also be negative to reverse the stage direction
         under both manual and programmatic control.
 
-        Typical values:
+        Typical values for version 2.x devices:
 
         * PatchStar: 6.4, 6.4, 6.4
         * Microstar: 6.4, 6.4, 6.4
         * SliceScope: 4.03, 4.03, 6.4  (sometimes 5.12, 5.12, 6.4)
         * Condenser: 4.03, 4.03, 6.4
+
+        For version 3.x devices, use ``10240 / x``, where x is the equivalent axis scale for a version 2 device.
+        (e.g. use 1600 instead of 6.4)
         """
-        cmd = ['UUX', 'UUY', 'UUZ'][axis]
+        if self._version < 3:
+            cmd = ['UUX', 'UUY', 'UUZ'][axis]
+        else:
+            cmd = ['USTEP X', 'USTEP Y', 'USTEP Z'][axis]
         self.send('%s %f' % (cmd, float(scale)))
         self._readAxisScale()
 
@@ -308,7 +357,10 @@ class Scientifica(SerialDevice):
         Note: If other axes have different scale factors, then their max speds will be different as
         well.
         """
-        return self.getParam('maxSpeed') / abs(2. * self._axis_scale[0])
+        if self._version < 3:
+            return self.getParam('maxSpeed') / abs(2. * self._axis_scale[0])
+        else:
+            return self.getParam('maxSpeed')
 
     def setSpeed(self, speed):
         """Set the maximum speed of the stage in um/sec.
@@ -317,7 +369,10 @@ class Scientifica(SerialDevice):
         speed value. If other axes have different scale factors, then their maximum
         speed will also be different.
         """
-        self.setParam('maxSpeed', abs(speed * 2 * self._axis_scale[0]))
+        if self._version < 3:
+            self.setParam('maxSpeed', speed * 2 * abs(self._axis_scale[0]))
+        else:
+            self.setParam('maxSpeed', speed)
 
     def moveTo(self, pos, speed=None):
         """Set the position of the manipulator.
@@ -325,7 +380,7 @@ class Scientifica(SerialDevice):
         *pos* must be a list of 3 items, each is either an integer representing the desired position
         of the manipulator (in 1/10 micrometers), or None which indicates the axis should not be moved.
 
-        If *speed* is given, then the maximum speed of the manipulator is set before initiating the move.
+        If *speed* is given, then the maximum speed of the manipulator is set (in um/sec) before initiating the move.
 
         This method returns immediately. Use isMoving() to determine whether the move has completed.
         If the manipulator is already moving, then this method will time out and the command will be ignored.
