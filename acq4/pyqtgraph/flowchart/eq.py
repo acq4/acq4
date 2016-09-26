cd92792ee2c778f8d4aa4a876ff444760f978bed
Luke Campagnola <luke.campagnola@gmail.com>
2016-09-26 14:53:25 -0700

    Speed up eq.py array comparison

--- acq4/pyqtgraph/flowchart/eq.py
+++ acq4/pyqtgraph/flowchart/eq.py
@@ -6,15 +6,26 @@ def eq(a, b):
     """The great missing equivalence function: Guaranteed evaluation to a single bool value.
 
     Array arguments are only considered equivalent to objects that have the same type and shape, and where
-    the elementwise comparison returns true for all elements.
+    the elementwise comparison returns true for all elements. If both arguments are arrays, then
+    they must have the same shape and dtype to be considered equivalent.
     """
     if a is b:
         return True
 
     # Avoid comparing large arrays against scalars; this is expensive and we know it should return False.
-    if (isinstance(a, (ndarray, MetaArray)) or isinstance(b, (ndarray, MetaArray))) and type(a) != type(b):
+    aIsArr = isinstance(a, (ndarray, MetaArray))
+    bIsArr = isinstance(b, (ndarray, MetaArray))
+    if (aIsArr or bIsArr) and type(a) != type(b):
         return False
 
+    # If both inputs are arrays, we can speeed up comparison if shapes / dtypes don't match
+    # NOTE: arrays of dissimilar type should be considered unequal even if they are numerically
+    # equal because they may behave differently when computed on.
+    if aIsArr and bIsArr and (a.shape != b.shape or a.dtype != b.dtype):
+        return False
+
+    # Test for equivalence. 
+    # If the test raises a recognized exception, then return Falase
     try:
         e = a==b
     except ValueError:
@@ -25,6 +36,7 @@ def eq(a, b):
         print("a:", str(type(a)), str(a))
         print("b:", str(type(b)), str(b))
         raise
+    
     t = type(e)
     if t is bool:
         return e
@@ -42,3 +54,4 @@ def eq(a, b):
             return e.all()
     else:
         raise Exception("== operator returned type %s" % str(type(e)))
+            
