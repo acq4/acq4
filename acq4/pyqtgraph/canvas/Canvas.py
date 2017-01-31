37ebe500e22d55c7b38a0906de4cfdd343059b4f
Luke Campagnola <lukec@alleninstitute.org>
2017-01-30 16:01:59 -0800

    Memory management--Canvas does garbage collect upon removing item, flowchart operator nodes get option to change output type

--- acq4/pyqtgraph/canvas/Canvas.py
+++ acq4/pyqtgraph/canvas/Canvas.py
@@ -17,6 +17,7 @@ else:
 import numpy as np
 from .. import debug
 import weakref
+import gc
 from .CanvasManager import CanvasManager
 from .CanvasItem import CanvasItem, GroupCanvasItem
 
@@ -422,8 +423,8 @@ class Canvas(QtGui.QWidget):
                 self.removeItem(item._canvasItem)
             else:
                 self.view.removeItem(item)
-        
-        ## disconnect signals, remove from list, etc..
+                
+        gc.collect()
         
     def clear(self):
         while len(self.items) > 0:
@@ -471,14 +472,3 @@ class SelectBox(ROI):
             self.addScaleHandle([0, 0], center, lockAspect=True)
         self.addRotateHandle([0, 1], center)
         self.addRotateHandle([1, 0], center)
-
-
-
-
-
-
-
-
-
-
-    
