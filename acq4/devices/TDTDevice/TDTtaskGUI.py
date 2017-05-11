9cd275259737be59011eaaef9d55863826f5014b
Megan Kratz <meganbkratz@gmail.com>
2017-05-11 17:03:20 -0400

    Not functional! work towards getting sequencing working
    (cherry picked from commit 6ef3bf0eebc7fa1b698b4c2e91c743aaf5dc55c2)

--- acq4/devices/TDTDevice/TDTtaskGUI.py
+++ acq4/devices/TDTDevice/TDTtaskGUI.py
@@ -1,5 +1,10 @@
 from PyQt4 import QtCore, QtGui
 from acq4.devices.Device import TaskGui
+from acq4.pyqtgraph.parametertree.ParameterTree import ParameterTree
+from acq4.util.generator.StimParamSet import SeqParameter
+from acq4.util.generator.SeqParamSet import SeqEvalError
+import acq4.util.units as units
+import sys
 
 class TDTTaskGui(TaskGui):
     
@@ -7,20 +12,99 @@ class TDTTaskGui(TaskGui):
         TaskGui.__init__(self, dev, taskRunner)
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
-        self.attenSpin = QtGui.QSpinBox()
-        self.attenSpin.setMaximum(120)
-        self.attenSpin.setMinimum(0)
-        self.attenSpin.setValue(30)
-        self.layout.addWidget(self.attenSpin)
+        #self.attenSpin = QtGui.QSpinBox()
+        #self.attenSpin.setMaximum(120)
+        #self.attenSpin.setMinimum(0)
+        #self.attenSpin.setValue(30)
+        #self.layout.addWidget(self.attenSpin)
+        self.paramTree = ParameterTree()
+        self.layout.addWidget(self.paramTree)
+        self.attParam = SimpleSequenceParamSet(name='Attenuation', type='int', limits=[0,120], expanded=True, value=30, units='dB')
+        self.paramTree.addParameters(self.attParam)
+
+        #self.attParam.sigTreeStateChanged.connect(self.attParamsChanged)
 
     def generateTask(self, params=None):
-        return {'PA5.1': {'attenuation': self.attenSpin.value()}}
+        print "TDTTaskGui.generateTask:", params
+        return {'PA5.1': {'attenuation': self.attParam['default']}}
+        
+
+    
+    def listSequence(self):
+        """Return an OrderedDict of sequence parameter names and values {name: [val1, val2, val3]}"""
+        #print "TDT compile:", self.attParam.compile()
+        return {'attenuation': self.attParam.compile()[1]}
 
     def saveState(self):
         """Return a dictionary representing the current state of the widget."""
-        return {'attenuation':self.attenSpin.value()}
+        return self.attParam.saveState()
 
     def restoreState(self, state):
         """Restore the state of the widget from a dictionary previously generated using saveState"""
-        attenuation = state.get('attenuation', 30)
-        self.attenSpin.setValue(attenuation)
+        #attenuation = state.get('attenuation', 30)
+        #self.attenSpin.setValue(attenuation)
+        self.attParam.restoreState(state)
+
+class SimpleSequenceParamSet(SeqParameter):
+
+    def __init__(self, **args):
+        SeqParameter.__init__(self, **args)
+
+        self.evalLocals = units.allUnits.copy()
+        exec('from numpy import *', self.evalLocals)  ## import all of numpy into the eval namespace
+
+        ### add a field that displays the calculated sequence values, since we aren't using a plot to confirm output
+        self.addChild({'name': 'values', 'type': 'str', 'value': '', 'visible': False, 'readonly':True})
+        for k in ['range', 'list']:
+            self.visibleParams[k].append('values')
+
+        self.sigTreeStateChanged.connect(self.treeStateChanged)
+
+    def treeStateChanged(self):
+        SeqParameter.treeStateChanged(self)
+        self.compile
+        with self.treeChangeBlocker():
+            self.param('values').setOpts(value=str(seq))
+
+    def compile(self):
+        name = self.name()
+        #default = self.evalStr('default')
+        default = self.value()
+        mode = self['sequence']
+        if mode == 'off':
+            seq = []
+        elif mode == 'range':
+            start = self.evalStr('start')
+            stop = self.evalStr('stop')
+            nPts = self['steps']
+            if self['log spacing']:
+                seq = fn.logSpace(start, stop, nPts)
+            else:
+                seq = np.linspace(start, stop, nPts)
+        elif mode == 'list':
+            seq = list(self.evalStr('list'))
+        elif mode == 'eval':
+            seq = self.evalStr('expression')
+        else:
+            raise Exception('Unknown sequence mode %s' % mode)
+        
+        if self['randomize']:
+            np.random.shuffle(seq)
+        
+        ## sanity check
+        try:
+            len(seq)
+        except:
+            raise Exception("Parameter %s generated invalid sequence: %s" % (name, str(seq)))
+
+        self.param('values').setOpts(value=str(seq))
+
+        return default, seq
+
+    def evalStr(self, name):
+        try:
+            s = eval(self[name], self.evalLocals)
+        except:
+            #raise SeqEvalError(name, sys.exc_info()[1])
+            raise
+        return s
