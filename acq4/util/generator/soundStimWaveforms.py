393d3bda97b10516d697cf006df09def434d8239
ManisLab <ManisLab@Rig3>
2017-08-31 13:46:02 -0400

    Pauls changes -- seem to be mostly comments and changing spacing
    (cherry picked from commit a2247fb557fdb9593cdcf3da90e4a79eadbec015)

--- acq4/util/generator/soundStimWaveforms.py
+++ acq4/util/generator/soundStimWaveforms.py
@@ -80,8 +80,8 @@ def tonePip(freq= 1000.0, risfall=10e-3, start=0.0, stop=500.0e-3, base=0.0, **k
     # linramp=amplitude+sawWave(risfall*ms,amplitude,0,start*ms,(start+risfall)*ms, 0, **kwds)+pulse((start+risfall)*ms,(stop-risfall)*ms,amplitude, **kwds)-sawWave(risfall*ms,amplitude,0,(stop-risfall)*ms,stop*ms, **kwds)
     # cos2gat=(np.cos(linramp))**2
     # d=cos2gat
-    per=float(1/freq)
-    d=cos2gat(risfall,start, stop, **kwds)*waveforms.sineWave(per,1,0,start,stop,0, **kwds)
+    per = float(1./freq)
+    d = cos2gat(risfall, start, stop, **kwds) * waveforms.sineWave(per, 1, 0, start, stop, 0, **kwds)
     return d
 #def sawWave(period, amplitude=1.0, phase=0.0, start=0.0, stop=None, base=0.0, **kwds):    
 #np.cos(1.570796+sawWave(2.5e-3,1.570796,0,.250,.250+2.5e-3,0)+pulse(.250+2.5e-3,497.5e-3,1.570796)-sawWave(2.5e-3,1.570796,0,497.5e-3,500e-3))**2*sineWave(1/4000.0,1,0,.250,.500,0)
@@ -111,8 +111,8 @@ def soundstim(startfreq= 1000.0, npip= 11, tdur= 50, tipi= 400, octspace = 0.5, 
         dirconst = octspace
     else:
         dirconst = -1 * octspace
-    d=0
-    totalrep=npip*(tdur+tipi)
+    d = 0
+    totalrep = npip*(tdur+tipi)
     for repcount in np.arange(reps):
         freqs = startfreq * 2**(dirconst * np.arange(npip))
         
@@ -124,7 +124,8 @@ def soundstim(startfreq= 1000.0, npip= 11, tdur= 50, tipi= 400, octspace = 0.5, 
             print 'start', (icount)*(tdur+tipi)+repcount*totalrep+delay
             print 'stop',(tdur+tipi)*icount+tdur+repcount*totalrep+delay
             print 'freqs', freqs
-            d = d+tonePip(freqs[icount],2.5e-3,(icount)*(tdur+tipi)+repcount*totalrep+delay,(tdur+tipi)*icount+tdur+repcount*totalrep+delay,0, **kwds)
+            d = d + tonePip(float(freqs[icount]), 2.5e-3, icount*(tdur+tipi) + repcount*totalrep+delay,(
+                    tdur+tipi)*icount + tdur + repcount*totalrep + delay, 0, **kwds)
     return d
 
 def noisestim(risfall=10.0, npip=11, tdur= 50, tipi = 50, reps = 1, delay = 0, **kwds):
@@ -142,16 +143,16 @@ def noisestim(risfall=10.0, npip=11, tdur= 50, tipi = 50, reps = 1, delay = 0, *
     if not isNumOrNone(tipi):
         raise Exception("octspace argument must be a number between 0 and 1")
     
-    d=0
-    totalrep=npip*(tdur+tipi)
+    d = 0
+    totalrep = npip*(tdur+tipi)
     # for icount in np.arange(npip):
     #     d = d+noise(0,1,(icount)*(tdur+tipi),(tdur+tipi)*icount+tdur,**kwds)
     # return d
     for repcount in np.arange(reps):
         for icount in np.arange(npip):
-            start=(icount)*(tdur+tipi)+repcount*totalrep + delay
-            stop=(tdur+tipi)*icount+tdur+repcount*totalrep + delay
-            d = d+cos2gat(risfall,start,stop, **kwds)*waveforms.noise(0,1,start,stop,**kwds)
+            start = icount*(tdur+tipi) + repcount*totalrep + delay
+            stop = icount*(tdur+tipi) + tdur+repcount*totalrep + delay
+            d = d + cos2gat(risfall, start, stop, **kwds) * waveforms.noise(0, 1, start, stop, **kwds)
     return d
 #     amplitude = np.random_sample([nPts])
 #     d = tonePip(1000.0, 10.0, 0.0, 500.0, 0.0)
@@ -165,25 +166,25 @@ def sineAM(fC=16000.0, fM=10.0, risfall=2e-3, tdur=200e-3, tipi=150e-3, nstim=20
 
 ## Check all arguments
     if not isNum(nstim):
-        raise Exception("npip argument must be a number")
+        raise Exception("npip must be a number")
     if not isNumOrNone(tdur):
-        raise Exception("tdur argument must be a number")
+        raise Exception("tdur  must be a number")
     if not isNumOrNone(risfall):
-        raise Exception("risfall argument must be a number")
+        raise Exception("risfall must be a number")
     if not isNumOrNone(tipi):
-        raise Exception("octspace argument must be a number between 0 and 1")
+        raise Exception("tipi must be a number between 0 and 1")
     
     # def sineWave(period, amplitude=1.0, phase=0.0, start=0.0, stop=None, base=0.0, **kwds): 
     # def cos2gat(risfall=10.0, start=0.0, stop=500.0, **kwds):   
-    d=0
+    d = 0
     for icount in np.arange(nstim):
-        perM=float(1/fM)
-        perC=float(1/fC)
-        start=(icount)*(tdur+tipi)+delay
-        stop=(tdur+tipi)*icount+tdur+delay
-        sineC=waveforms.sineWave(perC,1,0,start,stop,0, **kwds)
-        sineM=1+0.7*waveforms.sineWave(perM,1,0,start,stop,0, **kwds)
-        d=d+cos2gat(risfall,start,stop, **kwds)*(sineC)*(sineM)
+        perM = float(1./fM)  # modulation period
+        perC = float(1./fC)  # carrier period
+        start = icount*(tdur+tipi)+delay
+        stop = (tdur+tipi)*icount+tdur+delay
+        sineC = waveforms.sineWave(perC, 1, 0, start, stop, 0, **kwds)
+        sineM = 1 + 0.7*waveforms.sineWave(perM, 1, 0, start, stop, 0, **kwds)
+        d = d + cos2gat(risfall, start, stop, **kwds)*sineC*sineM
     return d
 
 # def soundstimAM(fM=10, startfreq= 1000.0, npip= 11, tdur= 50, tipi= 400, octspace = 0.5, reps=1, direction= 'up', **kwds):
@@ -233,11 +234,11 @@ def narrowbandNoise(fC, bandwidth, start=0.0, stop=None, **kwds):
     warnings = kwds['warnings']
     #print 'sample rate', rate
     w = 0
-    nyq = float(rate/2)
+    nyq = float(rate/2.0)
     low = (fC-(bandwidth/2.0))/nyq
     high = (fC+(bandwidth/2.0))/nyq
     BW = [low,high]
-    b, a = signal.butter(4,BW,'band', False, output='ba')
+    b, a = signal.butter(4, BW, 'band', False, output='ba')
     BBN = waveforms.noise(0, 1, start, stop, **kwds)
     #print 'filter creation complete'
     if BBN.ndim == 1:
@@ -245,10 +246,9 @@ def narrowbandNoise(fC, bandwidth, start=0.0, stop=None, **kwds):
         w = signal.lfilter(b,a,BBN-sm)
         #print 'filtering complete'
         w = w + sm
-    b, a = signal.ellip(4,0.1, 120, BW, 'bandpass', False, output='ba')
+    b, a = signal.ellip(4, 0.1, 120, BW, 'bandpass', False, output='ba')
     sm = np.mean(w)
-    w = signal.lfilter(b,a,w-sm)
-    
+    w = signal.lfilter(b, a, w-sm)
     w = w + sm
     return w
 
@@ -269,12 +269,12 @@ def NBNStim(fC=16000.0, bandwidth=4000.0, risfall=2.5e-3, tdur=200e-3, tipi=150e
     
     # def sineWave(period, amplitude=1.0, phase=0.0, start=0.0, stop=None, base=0.0, **kwds): 
     # def cos2gat(risfall=10.0, start=0.0, stop=500.0, **kwds):   
-    d=0
+    d = 0
     for icount in np.arange(nstim):
-        start=(icount)*(tdur+tipi)+delay
-        stop=(tdur+tipi)*icount+tdur+delay
+        start = icount*(tdur+tipi)+delay
+        stop = icount*(tdur+tipi) + tdur + delay
         NBN = narrowbandNoise(fC, bandwidth, start, stop, **kwds)
-        d=d+cos2gat(risfall,start,stop, **kwds)*NBN
+        d = d + cos2gat(risfall, start, stop, **kwds)*NBN
     return d   
 
 # #Building FM sweeps
@@ -378,7 +378,7 @@ def NBNStim(fC=16000.0, bandwidth=4000.0, risfall=2.5e-3, tdur=200e-3, tipi=150e
 #     d = cos2gat(risfall, start, stop,**kwds)*d
 #     return d
 
-def FMsweepLinear(f0,f1,start,stop,base=0.0,**kwds):
+def FMsweepLinear(f0, f1, start, stop, base=0.0, **kwds):
     rate = kwds['rate']
     nPts = kwds['nPts']
     warnings = kwds['warnings']
@@ -401,18 +401,15 @@ def FMsweepLinear(f0,f1,start,stop,base=0.0,**kwds):
     else:
         stoptime = int(stop * rate)
     #d[start:stop] = amplitude * np.sin(phase * 2.0 * np.pi + np.arange(stop-start) * 2.0 * np.pi / (period * rate))
-    print 'start',start
-    print 'stop',stop
  
-    delt=np.arange((stoptime-starttime))
-    # print 'length of time:', len(delt)
-    samp = np.zeros(len(delt))
-    samp = signal.chirp(delt/rate,f0,stop,f1,method='linear', phi=0)
+    delt = np.arange((stoptime-starttime))/rate
+  #  samp = np.zeros(len(delt))
+    samp = signal.chirp(delt, f0, np.max(delt), f1, method='linear', phi=0)
     # print 'length of samp:',len(samp)
-    d[starttime:stoptime]=samp
+    d[starttime:stoptime] = samp
     return d 
 
-def FMsweepLogarithmic(f0,f1,start,stop,base=0.0,**kwds):
+def FMsweepLogarithmic(f0, f1, start, stop, base=0.0, **kwds):
     rate = kwds['rate']
     nPts = kwds['nPts']
     warnings = kwds['warnings']
@@ -436,45 +433,52 @@ def FMsweepLogarithmic(f0,f1,start,stop,base=0.0,**kwds):
         stoptime = int(stop * rate)
     #d[start:stop] = amplitude * np.sin(phase * 2.0 * np.pi + np.arange(stop-start) * 2.0 * np.pi / (period * rate))
 
-    delt=np.arange((stoptime-starttime))
+    delt = np.arange((stoptime-starttime))
     
     samp = np.zeros(len(delt))
-    samp = signal.chirp(delt/rate,f0,stop,f1,method='logarithmic', phi=0)
+    samp = signal.chirp(delt/rate, f0, stop, f1, method='logarithmic', phi=0)
    
-    d[starttime:stoptime]=samp
+    d[starttime:stoptime] = samp
     return d 
 
-def FMSweepReversal(method='linear',sweeprate=12, f0=5.0, f1= 11.0, start=0.0,stop=6.0,delay=1000,**kwds):
+def FMSweepReversal(method='linear',sweeprate=12., upfirst=True, f0=5000., f1= 11000., delay=0.5, duration=2.0, risfall=0.1, **kwds):
     rate = kwds['rate']
     nPts = kwds['nPts']
     warnings = kwds['warnings']
     d = 0
     SWP = 0
-    dir1=0
-    dir2=0
+    dir1 = 0
+    dir2 = 0
     ## Check all arguments 
     #sweeprate is in number of sweeps per second
-    timeswitch = float(stop/2)
+    #timeswitch = float(duration/2.0)
     #numsweeps=sweeprate
-    numsweeps = sweeprate*stop/2
-    sweeplen = 1.0/sweeprate
-    print 'sweeplen',sweeplen
-    print 'numsweeps', numsweeps
-    for counter in np.arange(numsweeps):
-        print 'counter',counter
+    numsweeps = int(sweeprate*duration/2.0)
+    sweeplen = 0.5/sweeprate
+    # print 'sweeplen',sweeplen
+    # print 'numsweeps', numsweeps
+    sweepstart = delay
+    if not upfirst:
+        t = f0
+        f0 = f1
+        f1 = t
+    for counter in range(numsweeps):
+        # print 'counter',counter
         if method == 'linear':
-            upstart=start+counter*sweeplen+delay
-            upstop=start+(counter+1)*sweeplen+delay
-            dir1 = FMsweepLinear(f0, f1,upstart, upstop, base=0.0, **kwds)
-           
-            dwnstart=timeswitch+counter*sweeplen+delay
-            dwnstop=timeswitch+(counter+1)*sweeplen+delay
-            dir2 = FMsweepLinear(f0, f1,dwnstart, dwnstop, base=0.0,**kwds)
-            SWP=SWP+dir1+dir2
+            upstart = sweepstart
+            upstop = upstart + sweeplen # start + (counter+1)*sweeplen + delay
+            dir1 = FMsweepLinear(f0, f1, upstart, upstop, base=0.0, **kwds)
+            
+            dwnstart = upstop
+            dwnstop = dwnstart+sweeplen
+            sweepstart = dwnstop
+            dir2 = FMsweepLinear(f1, f0, dwnstart, dwnstop, base=0.0, **kwds)
+            if counter == 0:
+                SWP = dir1 + dir2
+            else:
+                SWP = SWP + dir1 + dir2
     
-    d = cos2gat(.010,start,stop,**kwds)*SWP
-
-
+    d = cos2gat(risfall, delay, duration, **kwds) * SWP
 
     return d
 
