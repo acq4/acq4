c428d0d7a9e148166b2c4efb6a150af0ceb665ab
Luke Campagnola <luke.campagnola@gmail.com>
2017-11-11 13:58:19 -0500

    Add CCF atlas canvas item

--- acq4/util/Canvas/items/__init__.py
+++ acq4/util/Canvas/items/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, inspect
-from . import CellCanvasItem, ImageCanvasItem, MarkersCanvasItem, MultiPatchLogCanvasItem, ScanCanvasItem, simpleitems
+from . import (CellCanvasItem, ImageCanvasItem, MarkersCanvasItem, 
+               MultiPatchLogCanvasItem, ScanCanvasItem, simpleitems,
+               AtlasCanvasItem)
 from .itemtypes import registerItemType, getItemType, itemTypes
