02e1f078c7a78bbaae296328aa40da2078f370cd
manislab <manislab@rig3>
2017-11-14 14:35:42 -0500

    Cherrypick of 8cafb048, but removed changes related to camera from commit.

--- acq4/devices/TDTDevice/TDTtaskGUI.py
+++ acq4/devices/TDTDevice/TDTtaskGUI.py
@@ -10,8 +10,17 @@ class TDTTaskGui(TaskGui):
         self.attenSpin = QtGui.QSpinBox()
         self.attenSpin.setMaximum(120)
         self.attenSpin.setMinimum(0)
-        self.attenSpin.setValue(50)
+        self.attenSpin.setValue(30)
         self.layout.addWidget(self.attenSpin)
 
     def generateTask(self, params=None):
         return {'PA5.1': {'attenuation': self.attenSpin.value()}}
+
+    def saveState(self):
+        """Return a dictionary representing the current state of the widget."""
+        return {'attenuation':self.attenSpin.value()}
+
+    def restoreState(self, state):
+        """Restore the state of the widget from a dictionary previously generated using saveState"""
+        attenuation = state.get('attenuation', 30)
+        self.attenSpin.setValue(attenuation)
--- acq4/util/generator/waveforms.py
+++ acq4/util/generator/waveforms.py
@@ -380,7 +380,7 @@ def tonePip(freq= 1000.0, risfall=10.0, start=0.0, stop=500.0, base=0.0, **kwds)
 #np.cos(1.570796+sawWave(2.5e-3,1.570796,0,.250,.250+2.5e-3,0)+pulse(.250+2.5e-3,497.5e-3,1.570796)-sawWave(2.5e-3,1.570796,0,497.5e-3,500e-3))**2*sineWave(1/4000.0,1,0,.250,.500,0)
 
 # def soundstim(startfreq= 1000.0, npip= 11, tdur= 50, tipi= 200, direction= 'up', **kwds):  #tfr 09/28/2015
-def soundstim(startfreq= 1000.0, npip= 11, tdur= 50, tipi= 400, octspace = 0.5, reps=1, direction= 'up', **kwds):
+def soundstim(startfreq= 1000.0, npip= 11, tdur= 50, tipi= 400, octspace = 0.5, reps=1, direction= 'up', delay = 0, **kwds):
     rate = kwds['rate']
     nPts = kwds['nPts']
     warnings = kwds['warnings']
@@ -414,13 +414,13 @@ def soundstim(startfreq= 1000.0, npip= 11, tdur= 50, tipi= 400, octspace = 0.5, 
             #d = d+tonePip(freqs[icount],2.5,(icount)*250,250*icount+50,0, **kwds) #tropp 09/28/2015
             print 'icount', icount
             print 'repcount', repcount
-            print 'start', (icount)*(tdur+tipi)+repcount*totalrep
-            print 'stop',(tdur+tipi)*icount+tdur+repcount*totalrep
+            print 'start', (icount)*(tdur+tipi)+repcount*totalrep+delay
+            print 'stop',(tdur+tipi)*icount+tdur+repcount*totalrep+delay
             print 'freqs', freqs
-            d = d+tonePip(freqs[icount],2.5e-3,(icount)*(tdur+tipi)+repcount*totalrep,(tdur+tipi)*icount+tdur+repcount*totalrep,0, **kwds)
+            d = d+tonePip(freqs[icount],2.5e-3,(icount)*(tdur+tipi)+repcount*totalrep+delay,(tdur+tipi)*icount+tdur+repcount*totalrep+delay,0, **kwds)
     return d
 
-def noisestim(risfall=10.0, npip=11, tdur= 50, tipi = 50, reps = 1, **kwds):
+def noisestim(risfall=10.0, npip=11, tdur= 50, tipi = 50, reps = 1, delay = 0, **kwds):
     rate = kwds['rate']
     nPts = kwds['nPts']
     warnings = kwds['warnings']
@@ -442,8 +442,8 @@ def noisestim(risfall=10.0, npip=11, tdur= 50, tipi = 50, reps = 1, **kwds):
     # return d
     for repcount in np.arange(reps):
         for icount in np.arange(npip):
-            start=(icount)*(tdur+tipi)+repcount*totalrep
-            stop=(tdur+tipi)*icount+tdur+repcount*totalrep
+            start=(icount)*(tdur+tipi)+repcount*totalrep + delay
+            stop=(tdur+tipi)*icount+tdur+repcount*totalrep + delay
             d = d+cos2gat(risfall,start,stop, **kwds)*noise(0,1,start,stop,**kwds)
     return d
 #     amplitude = np.random_sample([nPts])
@@ -451,7 +451,7 @@ def noisestim(risfall=10.0, npip=11, tdur= 50, tipi = 50, reps = 1, **kwds):
 #     dd = amplitude*d
 #     return dd
 
-def sineAM(fC=16000.0, fM=10.0, risfall=2e-3, tdur=200e-3, tipi=150e-3, nstim=20, **kwds):
+def sineAM(fC=16000.0, fM=10.0, risfall=2e-3, tdur=200e-3, tipi=150e-3, nstim=20, delay = 0, **kwds):
     rate = kwds['rate']
     nPts = kwds['nPts']
     warnings = kwds['warnings']
@@ -472,8 +472,8 @@ def sineAM(fC=16000.0, fM=10.0, risfall=2e-3, tdur=200e-3, tipi=150e-3, nstim=20
     for icount in np.arange(nstim):
         perM=float(1/fM)
         perC=float(1/fC)
-        start=(icount)*(tdur+tipi)
-        stop=(tdur+tipi)*icount+tdur
+        start=(icount)*(tdur+tipi)+delay
+        stop=(tdur+tipi)*icount+tdur+delay
         sineC=sineWave(perC,1,0,start,stop,0, **kwds)
         sineM=1+0.7*sineWave(perM,1,0,start,stop,0, **kwds)
         d=d+cos2gat(risfall,start,stop, **kwds)*(sineC)*(sineM)
@@ -545,7 +545,7 @@ def narrowbandNoise(fC, bandwidth, start=0.0, stop=None, **kwds):
     w = w + sm
     return w
 
-def NBNStim(fC=16000.0, bandwidth=4000.0, risfall=2.5-3, tdur=200e-3, tipi=150e-3, nstim=20, **kwds): 
+def NBNStim(fC=16000.0, bandwidth=4000.0, risfall=2.5-3, tdur=200e-3, tipi=150e-3, nstim=20, delay=0,**kwds): 
     rate = kwds['rate']
     nPts = kwds['nPts']
     warnings = kwds['warnings']
@@ -564,9 +564,210 @@ def NBNStim(fC=16000.0, bandwidth=4000.0, risfall=2.5-3, tdur=200e-3, tipi=150e-
     # def cos2gat(risfall=10.0, start=0.0, stop=500.0, **kwds):   
     d=0
     for icount in np.arange(nstim):
-        start=(icount)*(tdur+tipi)
-        stop=(tdur+tipi)*icount+tdur
+        start=(icount)*(tdur+tipi)+delay
+        stop=(tdur+tipi)*icount+tdur+delay
         NBN = narrowbandNoise(fC, bandwidth, start, stop, **kwds)
         d=d+cos2gat(risfall,start,stop, **kwds)*NBN
     return d   
+
+# #Building FM sweeps
+# def FMfreqLinear(f0=5, f1=11, tcur=0, TotT=12, **kwds):
+#     rate = kwds['rate']
+#     nPts = kwds['nPts']
+#     warnings = kwds['warnings']
+
+#     ## Check all arguments
+#     if not isNum(f0):
+#         raise Exception("f0 argument must be a number")
+#     if not isNumOrNone(f1):
+#         raise Exception("f1 argument must be a number")
+#     if not isNumOrNone(tcur):
+#         raise Exception("tcur argument must be a number")
+#     if not isNumOrNone(TotT):
+#         raise Exception("TotT argument must be a number")
+
+#     fcur = f0(1-tcur/TotT)+f1*tcur/TotT
+
+#     return fcur
+
+# def FMfreqLog(f0=5, f1=11, tcur=0, TotT=12, **kwds):
+#     rate = kwds['rate']
+#     nPts = kwds['nPts']
+#     warnings = kwds['warnings']
+
+#     ## Check all arguments
+#     if not isNum(f0):
+#         raise Exception("f0 argument must be a number")
+#     if not isNumOrNone(f1):
+#         raise Exception("f1 argument must be a number")
+#     if not isNumOrNone(tcur):
+#         raise Exception("tcur argument must be a number")
+#     if not isNumOrNone(TotT):
+#         raise Exception("TotT argument must be a number")
+
+#     fcur = f0**(1-tcur/TotT)*tcur**f1/TotT
+
+#     return fcur
+
+# def FMmag(tcur=0, TotT=12, sigma=2.8, **kwds):
+#     rate = kwds['rate']
+#     nPts = kwds['nPts']
+#     warnings = kwds['warnings']
+
+#     ## Check all arguments
+#     if not isNum(sigma):
+#         raise Exception("sigma argument must be a number")
+#     if not isNumOrNone(tcur):
+#         raise Exception("tcur argument must be a number")
+#     if not isNumOrNone(TotT):
+#         raise Exception("TotT argument must be a number")
+
+#     Acur=np.exp(-1*(tcur-TotT)/(2*sigma**2))
+
+#     return Acur
+
+# def FMsweep(f0=5, f1=11, start=0, stop=12, risfall=10.0, **kwds):
+#     rate = kwds['rate']
+#     nPts = kwds['nPts']
+#     warnings = kwds['warnings']
+
+#     ## Check all arguments
+#     if not isNum(f0):
+#         raise Exception("f0 argument must be a number")
+#     if not isNumOrNone(f1):
+#         raise Exception("f1 argument must be a number")
+#     if not isNumOrNone(start):
+#         raise Exception("start argument must be a number")
+#     if not isNumOrNone(stop):
+#         raise Exception("stop argument must be a number")
+
+#     ## initialize array
+#     d = np.empty(nPts)
+#     d[:] = 0.0
+  
+#     ## Define start and end points
+#     if start is None:
+#         start = 0
+#     else:
+#         start = int(start * rate)
+#     if stop is None:
+#         stop = nPts-1
+#     else:
+#         stop = int(stop * rate)
+        
+#     if stop > nPts:
+#         warnings.append("WARNING: Function is longer than generated waveform\n")
+#         stop = nPts-1
+#     calcrng=3
+#     for nn in np.arange(calcrng):
+        
+#         FMamp = FMmag(5.0,12.0,2.8, **kwds)     
+#         freq = FMfreqLog(f0,f1,spoint,12,**kwds)
+#         period = 1/freq
+#         cycleTime = int(period * rate)
+#         if cycleTime < 10:
+#             warnings.append('Warning: Period is less than 10 samples\n')
+#         d[nn] = amplitude * np.sin(phase * 2.0 * np.pi + runvalue * 2.0 * np.pi / (period * rate))
+#     d = cos2gat(risfall, start, stop,**kwds)*d
+#     return d
+
+def FMsweepLinear(f0,f1,start,stop,base=0.0,**kwds):
+    rate = kwds['rate']
+    nPts = kwds['nPts']
+    warnings = kwds['warnings']
+    
+    ## Check all arguments 
+
+    
+    ## initialize array
+    
+    d = np.empty(nPts)
+    d[:] = base
+    
+    ## Define start and end points
+    if start is None:
+        starttime = 0
+    else:
+        starttime = int(start * rate)
+    if stop is None:
+        stoptime = nPts-1
+    else:
+        stoptime = int(stop * rate)
+    #d[start:stop] = amplitude * np.sin(phase * 2.0 * np.pi + np.arange(stop-start) * 2.0 * np.pi / (period * rate))
+    print 'start',start
+    print 'stop',stop
+ 
+    delt=np.arange((stoptime-starttime))
+    # print 'length of time:', len(delt)
+    samp = np.zeros(len(delt))
+    samp = signal.chirp(delt/rate,f0,stop,f1,method='linear', phi=0)
+    # print 'length of samp:',len(samp)
+    d[starttime:stoptime]=samp
+    return d 
+
+def FMsweepLogarithmic(f0,f1,start,stop,base=0.0,**kwds):
+    rate = kwds['rate']
+    nPts = kwds['nPts']
+    warnings = kwds['warnings']
+    
+    ## Check all arguments 
+
+    
+    ## initialize array
+    
+    d = np.empty(nPts)
+    d[:] = base
+    
+    ## Define start and end points
+    if start is None:
+        starttime = 0
+    else:
+        starttime = int(start * rate)
+    if stop is None:
+        stoptime = nPts-1
+    else:
+        stoptime = int(stop * rate)
+    #d[start:stop] = amplitude * np.sin(phase * 2.0 * np.pi + np.arange(stop-start) * 2.0 * np.pi / (period * rate))
+
+    delt=np.arange((stoptime-starttime))
+    
+    samp = np.zeros(len(delt))
+    samp = signal.chirp(delt/rate,f0,stop,f1,method='logarithmic', phi=0)
+   
+    d[starttime:stoptime]=samp
+    return d 
+
+def FMSweepReversal(method='linear',sweeprate=12, f0=5.0, f1= 11.0, start=0.0,stop=6.0,delay=1000,**kwds):
+    rate = kwds['rate']
+    nPts = kwds['nPts']
+    warnings = kwds['warnings']
+    d = 0
+    SWP = 0
+    dir1=0
+    dir2=0
+    ## Check all arguments 
+    #sweeprate is in number of sweeps per second
+    timeswitch = float(stop/2)
+    #numsweeps=sweeprate
+    numsweeps = sweeprate*stop/2
+    sweeplen = 1.0/sweeprate
+    print 'sweeplen',sweeplen
+    print 'numsweeps', numsweeps
+    for counter in np.arange(numsweeps):
+        print 'counter',counter
+        if method == 'linear':
+            upstart=start+counter*sweeplen+delay
+            upstop=start+(counter+1)*sweeplen+delay
+            dir1 = FMsweepLinear(f0, f1,upstart, upstop, base=0.0, **kwds)
+           
+            dwnstart=timeswitch+counter*sweeplen+delay
+            dwnstop=timeswitch+(counter+1)*sweeplen+delay
+            dir2 = FMsweepLinear(f0, f1,dwnstart, dwnstop, base=0.0,**kwds)
+            SWP=SWP+dir1+dir2
+    
+    d = cos2gat(.010,start,stop,**kwds)*SWP
+
+
+
+    return d
 _allFuncs = dict([(k, v) for k, v in globals().items() if callable(v)])
