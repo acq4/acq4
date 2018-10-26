d1efb731c5ec86ec934394d28a8e64fd27ca846a
Luke Campagnola <ausqualius@yahoo.com>
2018-10-26 00:35:58 -0700

    initial overhaul of stage coordinate system management (untested)

--- acq4/devices/Stage/Stage.py
+++ acq4/devices/Stage/Stage.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from acq4.util import Qt
+import numpy as np
 from acq4.devices.Device import *
 from acq4.devices.OptomechDevice import *
 from acq4.util.Mutex import Mutex
@@ -20,9 +21,8 @@ class Stage(Device, OptomechDevice):
     defined by the hardware.
     """
 
-    sigPositionChanged = Qt.Signal(object)
+    sigPositionChanged = Qt.Signal(object, object, object)  # self, new position, old position
     sigLimitsChanged = Qt.Signal(object)
-    sigSwitchChanged = Qt.Signal(object, object)  # self, {switch_name: value, ...}
 
     def __init__(self, dm, config, name):
         Device.__init__(self, dm, config, name)
@@ -36,20 +36,20 @@ class Stage(Device, OptomechDevice):
 
         self.config = config
         self.lock = Mutex(Qt.QMutex.Recursive)
-        self.pos = [0]*3
+
+        nAxes = len(self.axes())
+        self._lastPos = [0] * nAxes
+
+        # default implementation just uses this matrix to
+        # convert from device position to translation vector
+        self._axisTransform = None
+        self._inverseAxisTransform = None
+
         self._defaultSpeed = 'fast'
-        self.pitch = config.get('pitch', 27)
         self.setFastSpeed(config.get('fastSpeed', 1e-3))
         self.setSlowSpeed(config.get('slowSpeed', 10e-6))
 
-        # used to emit signal when position passes a threshold
-        self.switches = {}
-        self.switchThresholds = {}
-        for name, spec in config.get('switches', {}).items():
-            self.switches[name] = None
-            self.switchThresholds[name] = spec
-        
-        self._limits = [(None, None), (None, None), (None, None)]
+        self._limits = [(None, None)] * nAxes
 
         self._progressDialog = None
         self._progressTimer = Qt.QTimer()
@@ -60,15 +60,30 @@ class Stage(Device, OptomechDevice):
     def quit(self):
         self.stop()
 
+    def axes(self):
+        """Return a tuple of axis names implemented by this device, like ('x', 'y', 'z').
+
+        The axes described in the above data structure correspond to the mechanical
+        actuators on the device; they do not necessarily correspond to the axes in the 
+        global coordinate system or the local coordinate system of the device.
+
+        This method must be reimplemented by subclasses.
+        """
+        raise NotImplementedError("Must be implemented in subclass.")
+
     def capabilities(self):
         """Return a structure describing the capabilities of this device::
         
             {
-                'getPos': (x, y, z),   # whether each axis can be read from the device
-                'setPos': (x, y, z),   # whether each axis can be set on the device
-                'limits': (x, y, z),   # whether limits can be set for each axis
+                'getPos': (x, y, z),      # bool: whether each axis can be read from the device
+                'setPos': (x, y, z),      # bool: whether each axis can be set on the device
+                'limits': (x, y, z),      # bool: whether limits can be set for each axis
             }
             
+        The axes described in the above data structure correspond to the mechanical
+        actuators on the device; they do not necessarily correspond to the axes in the 
+        global coordinate system or the local coordinate system of the device.
+
         Subclasses must reimplement this method.
         """
         # todo: add other capability flags like resolution, speed, closed-loop, etc?
@@ -87,6 +102,9 @@ class Stage(Device, OptomechDevice):
         self.slowSpeed = v
 
     def _interpretSpeed(self, speed):
+        """Return a speed in m/s where the argument *speed* can be any of
+        'fast', 'slow', or a float m/s.
+        """
         if speed == 'fast':
             speed = self.fastSpeed
         elif speed == 'slow':
@@ -94,16 +112,73 @@ class Stage(Device, OptomechDevice):
         return speed
 
     def stageTransform(self):
-        """Return the transform that maps from the local coordinate system to
-        the scaled position reported by the stage hardware.
+        """Return the transform that implements the translation/rotation generated
+        by the current hardware state. 
         """
-        return Qt.QMatrix4x4(self._stageTransform)
+        return pg.SRTTransform3D(self._stageTransform)
 
-    def mapToStage(self, obj):
-        return self._mapTransform(obj, self._stageTransform)
+    def inverseStageTransform(self):
+        if self._inverseStageTransform is None:
+            inv, invertible = self.stageTransform().inverted()
+            if not invertible:
+                raise Exception("Transform is not invertible.")
+            self._inverseStageTransform = inv
+        return pg.SRTTransform3D(self._inverseStageTransform)
 
-    def mapFromStage(self, obj):
-        return self._mapTransform(obj, self._invStageTransform)
+    def _makeStageTransform(self, pos):
+        """Return a stage transform (as should be returned by stageTransform)
+        and an optional inverse, given a position reported by the device.
+
+        If the inverse transform is None, then it will be automatically generated
+        on demand by calling transform.inverted().
+
+        Subclasses may override this method; the default uses _axisTransform to 
+        map from the device position to a 3D translation matrix. This covers only cases
+        where the stage axes perform linear translations. For rotation or nonlinear
+        movement, this method must be reimplemented.
+        """
+        tr = pg.SRTTransform3D()
+        offset = self.axisTransform().map(pos)
+        tr.translate(offset)
+
+        inv = pg.SRTTransform3D()
+        inv.translate(-offset)
+
+        return tr, inv
+
+    def _solveStageTransform(self, pos_change):
+        """Given a desired change of local origin, return the device position required.
+
+        The default implementation simply inverts _axisTransform to generate this solution;
+        devices with more complex kinematics need to reimplement this method.
+        """ 
+        tr = self.stageTransform().translation() + pos_change
+        pos = self.inverseAxisTransform().map(tr)
+        return pos
+
+    def axisTransform(self):
+        if self._axisTransform is None:
+            self._axisTransform = pg.Transform3D()
+            self._inverseAxisTransform = pg.Transform3D()
+            scale = self.config.get('scale', None)
+            if scale is not None:
+                self._axisTransform.scale(*scale)
+                self._inverseAxisTransform.scale(*[1.0 / x for x in scale])
+        return pg.QtGui.QMatrix4x4(self._axisTransform)
+
+    def inverseAxisTransform(self):
+        if self._inverseAxisTransform is None:
+            inv, invertible = self.axisTransform().inverted()[1]
+            if not invertible:
+                raise Exception("Transform is not invertible.")
+            self._inverseAxisTransform = inv
+        return pg.QtGui.QMatrix4x4(self._inverseAxisTransform)
+
+    # def mapToStage(self, obj):
+    #     return self._mapTransform(obj, self._stageTransform)
+
+    # def mapFromStage(self, obj):
+    #     return self._mapTransform(obj, self._invStageTransform)
 
     def posChanged(self, pos):
         """Handle device position changes by updating the device transform and
@@ -112,62 +187,12 @@ class Stage(Device, OptomechDevice):
         Subclasses must call this method when the device position has changed.
         """
         with self.lock:
-            rel = [0] * len(self.pos)
-            rel[:len(pos)] = [pos[i] - self.pos[i] for i in range(len(pos))]
-            self.pos[:len(pos)] = pos
-        
-            self._stageTransform = pg.SRTTransform3D()
-            self._stageTransform.translate(*self.pos)
-            self._invStageTransform = pg.SRTTransform3D()
-            self._invStageTransform.translate(*[-x for x in self.pos])
+            lastPos = self._lastPos
+            self._lastPos = pos
+            self._stageTransform, self._inverseStageTransform = self._makeStageTransform(pos)
             self._updateTransform()
-        self.sigPositionChanged.emit({'rel': rel, 'abs': self.pos[:]})
 
-        self.checkSwitchChange(self.pos)
-
-    def checkSwitchChange(self, pos):
-        # position has changed. If user requested switch notifications, then we
-        # need to see if any thresholds were passed here and emit a signal.
-        with self.lock:
-            pos = self.pos[:]
-
-        changes = {}
-        for name, value in self.switches.items():
-            thresh = self.switchThresholds[name]
-            on = []
-            for ax, levels in enumerate(thresh):
-                # check each axis
-                if levels is None:
-                    continue
-                start, end = levels
-                if end > start:
-                    if pos[ax] > end:
-                        on.append(True)
-                    elif pos[ax] < start:
-                        on.append(False)
-                else:
-                    if pos[ax] < end:
-                        on.append(True)
-                    elif pos[ax] > start:
-                        on.append(False)
-
-            if len(on) == 0:
-                # no axes are in unambiguous on/off position
-                continue
-
-            if all(on) and value != 1:
-                self.switches[name] = 1
-                changes[name] = 1
-            elif not any(on) and value != 0:
-                self.switches[name] = 0
-                changes[name] = 0
-
-        if len(changes) > 0:
-            self.sigSwitchChanged.emit(self, changes)
-
-    def getSwitch(self, name):
-        with self.lock:
-            return self.switches[name]
+        self.positionChanged.emit(self, pos, lastPos)
 
     def baseTransform(self):
         """Return the base transform for this Stage.
@@ -196,7 +221,7 @@ class Stage(Device, OptomechDevice):
         """
         if not refresh:
             with self.lock:
-                return self.pos[:]
+                return self._lastPos[:]
         else:
             return self._getPosition()
 
@@ -224,16 +249,17 @@ class Stage(Device, OptomechDevice):
 
         See targetPosition().
         """
-        prof = pg.debug.Profiler()
-        tp = self.targetPosition()
-        prof('1')
-        lp = self.mapFromStage(tp)
-        prof('2')
-        return self.mapToGlobal(lp)
+        # imagine what the global transform will look like after we reach the target..
+        target = self.targetPosition()
+        tr = self.baseTransform() * self._makeStageTransform(target)
+        pd = self.parentDevice()
+        if pd is not None:
+            tr = pd.globalTransform() * tr
+        return tr.map([0, 0, 0])
 
     def getState(self):
         with self.lock:
-            return (self.pos[:],)
+            return (self._lastPos[:],)
 
     def deviceInterface(self, win):
         return StageInterface(self, win)
@@ -302,7 +328,7 @@ class Stage(Device, OptomechDevice):
         """Move the stage to a position expressed in the global coordinate frame.
         """
         localPos = self.mapFromGlobal(pos)
-        stagePos = self.mapToStage(localPos)
+        stagePos = self._solveStageTransform(localPos)
         return self.moveTo(stagePos, speed, linear=linear)
 
     def _toAbsolutePosition(self, abs, rel):
@@ -373,11 +399,6 @@ class Stage(Device, OptomechDevice):
         """Return a list the (min, max) position limits for each axis.
         """
         return self._limits[:]
-
-    def calibrate(self, camera):
-        cal = StageCalibration(self)
-        cal.calibrate(camera)
-        return cal
 
 
 class MoveFuture(object):
@@ -473,20 +494,12 @@ class StageInterface(Qt.QWidget):
                 if cap['limits'][axis]:
                     minCheck = Qt.QCheckBox('Min:')
                     minCheck.tag = (axis, 0)
-                    # minBtn = Qt.QPushButton('set')
-                    # minBtn.tag = (axis, 0)
-                    # minBtn.setMaximumWidth(30)
                     maxCheck = Qt.QCheckBox('Max:')
                     maxCheck.tag = (axis, 1)
-                    # maxBtn = Qt.QPushButton('set')
-                    # maxBtn.tag = (axis, 1)
-                    # maxBtn.setMaximumWidth(30)
                     self.limitChecks[axis] = (minCheck, maxCheck)
                     widgets.extend([minCheck, maxCheck])
                     for check in (minCheck, maxCheck):
                         check.clicked.connect(self.limitCheckClicked)
-                    # for btn in (minBtn, maxBtn):
-                    #     btn.sigClicked.connect(self.limitBtnClicked)
 
                 for i,w in enumerate(widgets):
                     self.layout.addWidget(w, self.nextRow, i)
