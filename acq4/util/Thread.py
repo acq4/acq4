5fd62a10f919502066300a1850f19ae3119b7504
Luke Campagnola <ausqualius@yahoo.com>
2018-12-21 14:13:39 -0800

    support for printing thread names

--- acq4/util/Thread.py
+++ acq4/util/Thread.py
@@ -10,13 +10,23 @@ class Thread(Qt.QThread):
     the new thread.
     """
     def __init__(self, *args, **kwds):
+        name = kwds.pop('name', None)
+        self._threadname = name
         Qt.QThread.__init__(self, *args, **kwds)
+        if not hasattr(Qt.QThread, '_names'):
+            Qt.QThread._names = {}
 
+        if name is not None:
+            self.setObjectName(name)
+        
         # sneaky trick: force all subclasses to use our run wrapper
         self.__subclass_run = self.run
         self.run = self.__run_wrapper
 
     def __run_wrapper(self):
+        id = int(Qt.QThread.currentThreadId())
+        Qt.QThread._names[id] = self._threadname
+
         # for every new thread, re-enable faulthandler to ensure the new
         # thread is properly handled
         enableFaulthandler()
