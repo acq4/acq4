ae95086656a3d72a2614efe9df45694942c93d49
Luke Campagnola <ausqualius@yahoo.com>
2019-02-08 14:08:07 -0800

    Reworking patchpipette ui

--- acq4/modules/MultiPatch/multipatch.py
+++ acq4/modules/MultiPatch/multipatch.py
@@ -9,9 +9,10 @@ from acq4.modules.Module import Module
 from acq4 import getManager
 from acq4.devices.PatchPipette import PatchPipette
 import acq4.pyqtgraph as pg
+from .pipetteControl import PipetteControl
+
 
 Ui_MultiPatch = Qt.importTemplate('.multipatchTemplate')
-Ui_PipetteControl = Qt.importTemplate('.pipetteTemplate')
 
 
 class MultiPatch(Module):
@@ -23,133 +24,6 @@ class MultiPatch(Module):
         
         self.win = MultiPatchWindow(self)
         self.win.show()
-
-
-class PipetteControl(Qt.QWidget):
-
-    sigMoveStarted = Qt.Signal(object)
-    sigMoveFinished = Qt.Signal(object)
-    sigSelectChanged = Qt.Signal(object, object)
-    sigLockChanged = Qt.Signal(object, object)
-
-    def __init__(self, pipette, parent=None):
-        Qt.QWidget.__init__(self, parent)
-        self.pip = pipette
-        self.moving = False
-        self.pip.pipetteDevice.sigGlobalTransformChanged.connect(self.positionChanged)
-        if isinstance(pipette, PatchPipette):
-            self.pip.sigStateChanged.connect(self.stateChanged)
-            self.pip.sigActiveChanged.connect(self.pipActiveChanged)
-            self.pip.sigTestPulseFinished.connect(self.updatePlots)
-        self.moveTimer = Qt.QTimer()
-        self.moveTimer.timeout.connect(self.positionChangeFinished)
-
-        self.ui = Ui_PipetteControl()
-        self.ui.setupUi(self)
-
-        for state in pipette.listStates():
-            self.ui.stateCombo.addItem(state)
-        self.ui.stateCombo.activated.connect(self.stateComboChanged)
-
-        n = re.sub(r'[^\d]+', '', pipette.name())
-        self.ui.activeBtn.setText(n)
-
-        for ch in self.children():
-            ch.pipette = pipette
-            ch.pipCtrl = self
-
-        self.ui.activeBtn.clicked.connect(self.activeClicked)
-        self.ui.selectBtn.clicked.connect(self.selectClicked)
-        self.ui.lockBtn.clicked.connect(self.lockClicked)
-        self.ui.tipBtn.clicked.connect(self.focusTipBtnClicked)
-        self.ui.targetBtn.clicked.connect(self.focusTargetBtnClicked)
-
-        self.gv = pg.GraphicsLayoutWidget()
-        self.leftPlot = self.gv.addPlot()
-        self.leftPlot.enableAutoRange(True, True)
-        self.rightPlot = self.gv.addPlot()
-        self.rightPlot.setLogMode(y=True, x=False)
-        self.rightPlot.setYRange(6, 10)
-        self.rightPlot.setLabels(left=('Rss', u'Ω'))
-        self.ui.plotLayout.addWidget(self.gv)
-
-        self.tpLabel = Qt.QGraphicsTextItem()
-        self.tpLabel.setParentItem(self.leftPlot.vb)
-        self.tpLabel.setDefaultTextColor(pg.mkColor('w'))
-
-        self.stateChanged(pipette)
-        self.pipActiveChanged()
-
-    def active(self):
-        return self.ui.activeBtn.isChecked()
-
-    def activeClicked(self, b):
-        self.pip.setActive(b)
-
-    def pipActiveChanged(self):
-        self.ui.activeBtn.setChecked(self.pip.active)
-
-    def selected(self):
-        return self.ui.selectBtn.isChecked()
-
-    def selectClicked(self):
-        self.sigSelectChanged.emit(self, self.selected())
-
-    def setSelected(self, sel):
-        self.ui.selectBtn.setChecked(sel)
-
-    def locked(self):
-        return self.ui.lockBtn.isChecked()
-    
-    def lockClicked(self):
-        self.sigLockChanged.emit(self, self.locked())
-
-    def setLocked(self, lock):
-        self.ui.lockBtn.setChecked(lock)
-
-    def updatePlots(self):
-        """Update the pipette data plots."""
-        tp = self.pip.lastTestPulse()
-        data = tp.data
-        pri = data['Channel': 'primary']
-        units = pri._info[-1]['ClampState']['primaryUnits'] 
-        self.leftPlot.plot(pri.xvals('Time'), pri.asarray(), clear=True)
-        self.leftPlot.setLabels(left=('', units))
-        tph = self.pip.testPulseHistory()
-        self.rightPlot.plot(tph['time'] - tph['time'][0], tph['steadyStateResistance'], clear=True)
-
-        tpa = tp.analysis()
-        self.tpLabel.setPlainText(pg.siFormat(tpa['steadyStateResistance'], suffix=u'Ω'))
-
-    def stateChanged(self, pipette):
-        """Pipette's state changed, reflect that in the UI"""
-        state = pipette.getState()
-        index = self.ui.stateCombo.findText(state)
-        self.ui.stateCombo.setCurrentIndex(index)
-
-    def stateComboChanged(self, stateIndex):
-        if isinstance(self.pip, PatchPipette):
-            state = str(self.ui.stateCombo.itemText(stateIndex))
-            self.pip.setState(state)
-
-    def positionChanged(self):
-        self.moveTimer.start(500)
-        if self.moving is False:
-            self.moving = True
-            self.sigMoveStarted.emit(self)
-
-    def positionChangeFinished(self):
-        self.moveTimer.stop()
-        self.moving = False
-        self.sigMoveFinished.emit(self)
-
-    def focusTipBtnClicked(self, state):
-        speed = self.selectedSpeed(default='slow')
-        self.focusTip(speed)
-
-    def focusTargetBtnClicked(self, state):
-        speed = self.selectedSpeed(default='slow')
-        self.focusTarget(speed)
 
 
 class MultiPatchWindow(Qt.QWidget):
