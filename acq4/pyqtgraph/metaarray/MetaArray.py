b66746403e95e9c69363fb196b1ccd4270e2d841
Luke Campagnola <ausqualius@yahoo.com>
2019-03-15 02:41:16 -0700

    metaarray deprecation warning

--- acq4/pyqtgraph/metaarray/MetaArray.py
+++ acq4/pyqtgraph/metaarray/MetaArray.py
@@ -21,7 +21,14 @@ from ..python2_3 import basestring
 ## This can be overridden by setting USE_HDF5 = False
 USE_HDF5 = True
 try:
-    import h5py.highlevel
+    import h5py
+
+    # Older h5py versions tucked Group and Dataset deeper inside the library:
+    if not hasattr(h5py, 'Group'):
+        import h5py.highlevel
+        h5py.Group = h5py.highlevel.Group
+        h5py.Dataset = h5py.highlevel.Dataset
+        
     HAVE_HDF5 = True
 except:
     USE_HDF5 = False
@@ -999,9 +1006,9 @@ class MetaArray(object):
             data[k] = val
         for k in root:
             obj = root[k]
-            if isinstance(obj, h5py.highlevel.Group):
+            if isinstance(obj, h5py.Group):
                 val = MetaArray.readHDF5Meta(obj)
-            elif isinstance(obj, h5py.highlevel.Dataset):
+            elif isinstance(obj, h5py.Dataset):
                 if mmap:
                     val = MetaArray.mapHDF5Array(obj)
                 else:
