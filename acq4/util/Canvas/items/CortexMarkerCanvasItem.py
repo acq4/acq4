cd1a3e30b5994410d4b3c0639b255a30366768e8
Megan Kratz <meganbkratz@gmail.com>
2019-07-21 12:07:42 -0600

    start of new CortexMarkerROI with layer boundaries

--- acq4/util/Canvas/items/CortexMarkerCanvasItem.py
+++ acq4/util/Canvas/items/CortexMarkerCanvasItem.py
@@ -14,11 +14,15 @@ class CortexMarkerCanvasItem(CanvasItem):
             if vr is None:
                 points = ((0, 0), (1, 1))
             else:
-                p1 = vr.center()
-                p2 = p1 + 0.2 * (vr.topRight()-p1)
-                points = ((p1.x(), p1.y()), (p2.x(), p2.y()))
+                #p1 = vr.center()
+                #p2 = p1 + 0.2 * (vr.topRight()-p1)
+                #points = ((p1.x(), p1.y()), (p2.x(), p2.y()))
+                p = vr.center()
+                d = vr.topRight()-vr.center()
+                points = ((p.x(), p.y()+0.6*d.y()), (p.x(), p.y()-0.6*d.y()))
+                #print('points:', points)
 
-        item = CortexMarkerROI(points, movable=False)
+        item = CortexMarkerROI(points, movable=True)
         CanvasItem.__init__(self, item, **kwds)
 
 
@@ -50,48 +54,112 @@ class CortexMarkerCanvasItem(CanvasItem):
 
 registerItemType(CortexMarkerCanvasItem)
 
-class CortexMarkerROI(pg.graphicsItems.ROI.LineSegmentROI):
-    def __init__(self, *args, **kwds):
-        pg.graphicsItems.ROI.LineSegmentROI.__init__(self, *args, **kwds)
 
-    def paint(self, p, *args):
-        pg.graphicsItems.ROI.LineSegmentROI.paint(self, p, *args)
-        h1 = self.handles[0]['item'].pos()
-        h2 = self.handles[1]['item'].pos()
-        p1 = p.transform().map(h1)
-        p2 = p.transform().map(h2)
+class CortexMarkerROI(pg.graphicsItems.ROI.ROI):
 
-        vec = pg.Point(self.mapToParent(h2)) - pg.Point(self.mapToParent(h1))
-        length = vec.length()
-        angle = -vec.angle(pg.Point(0,-1)) ## changed to match how Alice measured angle in optoanalysis/new_test_ui.py
-        self.angle = angle
+    def __init__(self, points=None, layers=None, **kwargs):
 
-        pvec = p2 - p1
-        pvecT = pg.Point(pvec.y(), -pvec.x())
-        pos = 0.5 * (p1 + p2) + pvecT * 40 / pvecT.length()
+        #size = (pg.Point(points[1])-pg.Point(points[0])).length()
+        #print('size:', size)
+        size = (1,1)
+        points = [(0,0), (1,1)]
+        pg.graphicsItems.ROI.ROI.__init__(self, points[0], size=size, **kwargs)
+        
 
-        p.resetTransform()
+        if layers is None:
+            layers = ['L1', 'L2/3', 'L4', 'L5', 'L6']
 
-        txt = pg.functions.siFormat(length, suffix='m') + '\n%0.1f deg' % angle
+        #points = [(0,0)]
+        #for i in range(len(layers)):
+        #    points.append((0,(i+1)*.01))
+        self.addScaleHandle([1,0.5], [0.5, 0.5])
+        self.addScaleRotateHandle([0.5, 1], [0.5, 0], name='pia')
+        for i in range(len(layers)-1):
+            self.addFreeHandle((0.5, float(i+1)/len(layers)))
+            #print("addFreeHandle:", (0.5, float(i+1)/len(layers)))
+        self.addScaleRotateHandle([0.5, 0], [0.5, 1], name='wm')
+
+        #self.handles[0]['item'].movePoint(pg.Point(points[0]))
+        #self.handles[-1]['item'].movePoint(pg.Point(points[1]))
+        #self.scale(0.001)
+        #self.stateChanged(finish=True)
+
+    def paint(self, p, opt, widget):
+        # Note: don't use self.boundingRect here, because subclasses may need to redefine it.
+        r = QtCore.QRectF(0, 0, self.state['size'][0], self.state['size'][1]).normalized()
+        
+        p.setRenderHint(QtGui.QPainter.Antialiasing)
+        p.setPen(self.currentPen)
+        p.translate(r.left(), r.top())
+        p.scale(r.width(), r.height())
+        p.drawRect(0, 0, 1, 1)
+
+
+        for i, h in enumerate(self.handles):
+            if h['type'] == 'f':
+                p.setPen(pg.mkPen(pg.intColor(i), width=2))
+                #y = h['item'].pos()
+                y2 = h['pos'].y()
+                #print('y:', y, '   y2:', y2)
+                p.drawLine(QtCore.QPointF(0,y2), QtCore.QPointF(1,y2))
+
+    #def addSegment(self, pos=(0,0), scaleHandle=False, connectTo=None):
+
+
+
+
+
+
+
+
+
+
+
+
+# class CortexMarkerROI(pg.graphicsItems.ROI.LineSegmentROI):
+#     def __init__(self, *args, **kwds):
+#         pg.graphicsItems.ROI.LineSegmentROI.__init__(self, *args, **kwds)
+
+#     def paint(self, p, *args):
+#         pg.graphicsItems.ROI.LineSegmentROI.paint(self, p, *args)
+#         h1 = self.handles[0]['item'].pos()
+#         h2 = self.handles[1]['item'].pos()
+#         p1 = p.transform().map(h1)
+#         p2 = p.transform().map(h2)
+
+#         vec = pg.Point(self.mapToParent(h2)) - pg.Point(self.mapToParent(h1))
+#         length = vec.length()
+#         angle = -vec.angle(pg.Point(0,-1)) ## changed to match how Alice measured angle in optoanalysis/new_test_ui.py
+#         self.angle = angle
+
+#         pvec = p2 - p1
+#         pvecT = pg.Point(pvec.y(), -pvec.x())
+#         pos = 0.5 * (p1 + p2) + pvecT * 40 / pvecT.length()
+
+#         p.resetTransform()
+
+#         txt = pg.functions.siFormat(length, suffix='m') + '\n%0.1f deg' % angle
        
-        r = QtCore.QRectF(pos.x()-20, pos.y()-20, 80, 30)
-        p.drawText(r, QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter, txt)
-        r1 = QtCore.QRectF(p1.x(), p1.y()-10, 20,10)
-        p.drawText(r1, "Pia")
-        r2 = QtCore.QRectF(p2.x(), p2.y()+10, 40,25)
-        p.drawText(r2, "White Matter")
+#         r = QtCore.QRectF(pos.x()-20, pos.y()-20, 80, 30)
+#         p.drawText(r, QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter, txt)
+#         r1 = QtCore.QRectF(p1.x(), p1.y()-10, 20,10)
+#         p.drawText(r1, "Pia")
+#         r2 = QtCore.QRectF(p2.x(), p2.y()+10, 40,25)
+#         p.drawText(r2, "White Matter")
 
-    def boundingRect(self):
-        r = pg.graphicsItems.ROI.LineSegmentROI.boundingRect(self)
-        #return r
+#     def boundingRect(self):
+#         r = pg.graphicsItems.ROI.LineSegmentROI.boundingRect(self)
+#         #return r
 
-        ### try to adjust for the text boxes so we don't get rendering artifacts 
-        pxw = self.pixelLength(pg.Point([1, 0]))
-        pxh = self.pixelLength(pg.Point([0, 1]))
-        if pxw is None or pxh is None:
-            return r
+#         ### try to adjust for the text boxes so we don't get rendering artifacts 
+#         pxw = self.pixelLength(pg.Point([1, 0]))
+#         pxh = self.pixelLength(pg.Point([0, 1]))
+#         if pxw is None or pxh is None:
+#             return r
 
-        xBuf = 80*pxw
-        yBuf = 80*pxh
-        return r.adjusted(-xBuf, -yBuf, xBuf, yBuf)
+#         xBuf = 80*pxw
+#         yBuf = 80*pxh
+#         return r.adjusted(-xBuf, -yBuf, xBuf, yBuf)
+
+
 
