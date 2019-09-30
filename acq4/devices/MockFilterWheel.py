443571c367e5369f05e1d6394b0e327eb2d35b0d
Luke Campagnola <luke.campagnola@gmail.com>
2019-09-29 20:26:02 -0700

    add mock filterwheel device

---
+++ acq4/devices/MockFilterWheel.py
@@ -0,0 +1,27 @@
+from FilterWheel.filterwheel import FilterWheel, FilterWheelFuture
+
+
+class MockFilterWheel(FilterWheel):
+    def __init__(self, dm, config, name):
+        self._position = 0
+        FilterWheel.__init__(self, dm, config, name)
+
+    def _setPosition(self, pos):
+        assert pos in self._filters, "Unknown filter wheel position %r" % pos
+        self._position = pos
+        return FilterWheelFuture(self, pos)
+
+    def _getPosition(self):
+        return self._position
+
+    def _isMoving(self):
+        return False
+
+    def _stop(self):
+        pass
+
+    def setSpeed(self, speed):
+        pass
+    
+    def getSpeed(self):
+        pass
