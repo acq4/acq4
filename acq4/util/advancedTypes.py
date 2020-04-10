dd5a682020127b3d4e997207133747afb9238f5e
Martin <outofculture@gmail.com>
2020-04-09 20:42:39 -0700

    remove unused classes, turn duplicates into import

--- acq4/util/advancedTypes.py
+++ acq4/util/advancedTypes.py
@@ -6,326 +6,20 @@ Distributed under MIT/X11 license. See license.txt for more infomation.
 
 Includes:
   - OrderedDict - Dictionary which preserves the order of its elements
-  - BiDict, ReverseDict - Bi-directional dictionaries
-  - ThreadsafeDict, ThreadsafeList - Self-mutexed data structures
+  - CaselessDict - Case-insensitive dict
 """
 from __future__ import print_function
 import copy, collections
 from acq4.util.debug import *
+from acq4.pyqtgraph.pgcollections import CaselessDict
 
-from collections import OrderedDict
-## Deprecated; this class is now present in Python 2.7 as collections.OrderedDict
-#class OrderedDict(dict):
-    #"""extends dict so that elements are iterated in the order that they were added.
-    #Since this class can not be instantiated with regular dict notation, it instead uses
-    #a list of tuples: 
-      #od = OrderedDict([(key1, value1), (key2, value2), ...])
-    #items set using __setattr__ are added to the end of the key list.
-    #"""
-    
-    #def __init__(self, data=None):
-        #self.order = []
-        #if data is not None:
-            #for i in data:
-                #self[i[0]] = i[1]
-    
-    #def __setitem__(self, k, v):
-        #if not self.has_key(k):
-            #self.order.append(k)
-        #dict.__setitem__(self, k, v)
-    
-    #def __delitem__(self, k):
-        #self.order.remove(k)
-        #dict.__delitem__(self, k)
-
-    #def keys(self):
-        #return self.order[:]
-    
-    #def items(self):
-        #it = []
-        #for k in self.keys():
-            #it.append((k, self[k]))
-        #return it
-    
-    #def values(self):
-        #return [self[k] for k in self.order]
-    
-    #def remove(self, key):
-        #del self[key]
-        ##self.order.remove(key)
-    
-    #def __iter__(self):
-        #for k in self.order:
-            #yield k
-            
-    #def update(self, data):
-        #"""Works like dict.update, but accepts list-of-tuples as well as dict."""
-        #if isinstance(data, dict):
-            #for k, v in data.iteritems():
-                #self[k] = v
-        #else:
-            #for k,v in data:
-                #self[k] = v
-
-    #def copy(self):
-        #return OrderedDict(self.items())
-        
-    #def itervalues(self):
-        #for k in self.order:
-            #yield self[k]
-            
-    #def iteritems(self):
-        #for k in self.order:
-            #yield (k, self[k])
-            
-    #def __deepcopy__(self, memo):
-        #return OrderedDict([(k, copy.deepcopy(v, memo)) for k, v in self.iteritems()])
-        
-        
-
-class ReverseDict(dict):
-    """extends dict so that reverse lookups are possible by requesting the key as a list of length 1:
-       d = BiDict({'x': 1, 'y': 2})
-       d['x']
-         1
-       d[[2]]
-         'y'
-    """
-    def __init__(self, data=None):
-        if data is None:
-            data = {}
-        self.reverse = {}
-        for k in data:
-            self.reverse[data[k]] = k
-        dict.__init__(self, data)
-        
-    def __getitem__(self, item):
-        if type(item) is list:
-            return self.reverse[item[0]]
-        else:
-            return dict.__getitem__(self, item)
-
-    def __setitem__(self, item, value):
-        self.reverse[value] = item
-        dict.__setitem__(self, item, value)
-
-    def __deepcopy__(self, memo):
-        raise Exception("deepcopy not implemented")
-        
-        
-class BiDict(dict):
-    """extends dict so that reverse lookups are possible by adding each reverse combination to the dict.
-    This only works if all values and keys are unique."""
-
-    def __init__(self, data=None):
-        if data is None:
-            data = {}
-        dict.__init__(self)
-        for k in data:
-            self[data[k]] = k
-
-    def __setitem__(self, item, value):
-        dict.__setitem__(self, item, value)
-        dict.__setitem__(self, value, item)
-    
-    def __deepcopy__(self, memo):
-        raise Exception("deepcopy not implemented")
-
-class ThreadsafeDict(dict):
-    """Extends dict so that getitem, setitem, and contains are all thread-safe.
-    Also adds lock/unlock functions for extended exclusive operations
-    Converts all sub-dicts and lists to threadsafe as well.
-    """
-    
-    def __init__(self, *args, **kwargs):
-        self.mutex = threading.RLock()
-        dict.__init__(self, *args, **kwargs)
-        for k in self:
-            if type(self[k]) is dict:
-                self[k] = ThreadsafeDict(self[k])
-
-    def __getitem__(self, attr):
-        self.lock()
-        try:
-            val = dict.__getitem__(self, attr)
-        finally:
-            self.unlock()
-        return val
-
-    def __setitem__(self, attr, val):
-        if type(val) is dict:
-            val = ThreadsafeDict(val)
-        self.lock()
-        try:
-            dict.__setitem__(self, attr, val)
-        finally:
-            self.unlock()
-        
-    def __contains__(self, attr):
-        self.lock()
-        try:
-            val = dict.__contains__(self, attr)
-        finally:
-            self.unlock()
-        return val
-
-    def __len__(self):
-        self.lock()
-        try:
-            val = dict.__len__(self)
-        finally:
-            self.unlock()
-        return val
-
-    def clear(self):
-        self.lock()
-        try:
-            dict.clear(self)
-        finally:
-            self.unlock()
-
-    def lock(self):
-        self.mutex.acquire()
-        
-    def unlock(self):
-        self.mutex.release()
-
-    def __deepcopy__(self, memo):
-        raise Exception("deepcopy not implemented")
-        
-class ThreadsafeList(list):
-    """Extends list so that getitem, setitem, and contains are all thread-safe.
-    Also adds lock/unlock functions for extended exclusive operations
-    Converts all sub-lists and dicts to threadsafe as well.
-    """
-    
-    def __init__(self, *args, **kwargs):
-        self.mutex = threading.RLock()
-        list.__init__(self, *args, **kwargs)
-        for k in self:
-            self[k] = mkThreadsafe(self[k])
-
-    def __getitem__(self, attr):
-        self.lock()
-        try:
-            val = list.__getitem__(self, attr)
-        finally:
-            self.unlock()
-        return val
-
-    def __setitem__(self, attr, val):
-        val = makeThreadsafe(val)
-        self.lock()
-        try:
-            list.__setitem__(self, attr, val)
-        finally:
-            self.unlock()
-        
-    def __contains__(self, attr):
-        self.lock()
-        try:
-            val = list.__contains__(self, attr)
-        finally:
-            self.unlock()
-        return val
-
-    def __len__(self):
-        self.lock()
-        try:
-            val = list.__len__(self)
-        finally:
-            self.unlock()
-        return val
-    
-    def lock(self):
-        self.mutex.acquire()
-        
-    def unlock(self):
-        self.mutex.release()
-
-    def __deepcopy__(self, memo):
-        raise Exception("deepcopy not implemented")
-        
-        
-def makeThreadsafe(obj):
-    if type(obj) is dict:
-        return ThreadsafeDict(obj)
-    elif type(obj) is list:
-        return ThreadsafeList(obj)
-    elif type(obj) in [str, int, float, bool, tuple]:
-        return obj
-    else:
-        raise Exception("Not sure how to make object of type %s thread-safe" % str(type(obj)))
-        
-        
-class Locker:
-    def __init__(self, lock):
-        self.lock = lock
-        self.lock.acquire()
-
-    def __del__(self):
-        try:
-            self.lock.release()
-        except:
-            pass
+## Template methods
+def wrapMethod(methodName):
+    return lambda self, *a, **k: getattr(self._data_, methodName)(*a, **k)
 
 
-class CaselessDict(OrderedDict):
-    """Case-insensitive dict. Values can be set and retrieved using keys of any case.
-    Note that when iterating, the original case is returned for each key."""
-
-    def __init__(self, *args):
-        OrderedDict.__init__(self, {})  ## requirement for the empty {} here seems to be a python bug?
-        self.keyMap = OrderedDict([(k.lower(), k) for k in OrderedDict.keys(self)])
-        if len(args) == 0:
-            return
-        elif len(args) == 1 and isinstance(args[0], dict):
-            for k in args[0]:
-                self[k] = args[0][k]
-        else:
-            raise Exception("CaselessDict may only be instantiated with a single dict.")
-        
-    #def keys(self):
-        #return self.keyMap.values()
-    
-    def __setitem__(self, key, val):
-        kl = key.lower()
-        if kl in self.keyMap:
-            OrderedDict.__setitem__(self, self.keyMap[kl], val)
-        else:
-            OrderedDict.__setitem__(self, key, val)
-            self.keyMap[kl] = key
-
-    def __getitem__(self, key):
-        kl = key.lower()
-        if kl not in self.keyMap:
-            raise KeyError(key)
-        return OrderedDict.__getitem__(self, self.keyMap[kl])
-
-    def __contains__(self, key):
-        return key.lower() in self.keyMap
-
-    def update(self, d):
-        for k, v in d.items():
-            self[k] = v
-
-    def copy(self):
-        return CaselessDict(OrderedDict.copy(self))
-
-    def __delitem__(self, key):
-        kl = key.lower()
-        if kl not in self.keyMap:
-            raise KeyError(key)
-        OrderedDict.__delitem__(self, self.keyMap[kl])
-        del self.keyMap[kl]
-
-    def __deepcopy__(self, memo):
-        raise Exception("deepcopy not implemented")
-
-    def clear(self):
-        OrderedDict.clear(self)
-        self.keyMap.clear()
-
+def protectMethod(methodName):
+    return lambda self, *a, **k: protect(getattr(self._data_, methodName)(*a, **k))
 
 
 class ProtectedDict(dict):
@@ -348,15 +42,7 @@ class ProtectedDict(dict):
 
     ## List of methods to disable
     disableMethods = ['__delitem__', '__setitem__', 'clear', 'pop', 'popitem', 'setdefault', 'update']
-    
-    
-    ## Template methods 
-    def wrapMethod(methodName):
-        return lambda self, *a, **k: getattr(self._data_, methodName)(*a, **k)
-        
-    def protectMethod(methodName):
-        return lambda self, *a, **k: protect(getattr(self._data_, methodName)(*a, **k))
-    
+
     def error(self, *args, **kargs):
         raise Exception("Can not modify read-only list.")
 
@@ -416,16 +102,9 @@ class ProtectedList(collections.Sequence):
     protectMethods = ['__getitem__', '__getslice__', '__mul__', '__reversed__', '__rmul__']
 
     ## List of methods to disable
-    disableMethods = ['__delitem__', '__delslice__', '__iadd__', '__imul__', '__setitem__', '__setslice__', 'append', 'extend', 'insert', 'pop', 'remove', 'reverse', 'sort']
-    
-    
-    ## Template methods 
-    def wrapMethod(methodName):
-        return lambda self, *a, **k: getattr(self._data_, methodName)(*a, **k)
-        
-    def protectMethod(methodName):
-        return lambda self, *a, **k: protect(getattr(self._data_, methodName)(*a, **k))
-    
+    disableMethods = ['__delitem__', '__delslice__', '__iadd__', '__imul__', '__setitem__', '__setslice__', 'append',
+                      'extend', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
     def error(self, *args, **kargs):
         raise Exception("Can not modify read-only list.")
 
@@ -494,16 +173,7 @@ class ProtectedTuple(collections.Sequence):
 
     ## List of methods which wrap from _data_ but return protected results
     protectMethods = ['__getitem__', '__getslice__', '__iter__', '__add__', '__mul__', '__reversed__', '__rmul__']
-    
-    
-    ## Template methods 
-    def wrapMethod(methodName):
-        return lambda self, *a, **k: getattr(self._data_, methodName)(*a, **k)
-        
-    def protectMethod(methodName):
-        return lambda self, *a, **k: protect(getattr(self._data_, methodName)(*a, **k))
-    
-    
+
     ## Directly (and explicitly) wrap some methods from _data_
     ## Many of these methods can not be intercepted using __getattribute__, so they
     ## must be implemented explicitly
