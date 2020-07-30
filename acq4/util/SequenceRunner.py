fef978718afa9fe242222d1ab3dce3ebb20d28c4
Martin <outofculture@gmail.com>
2020-07-30 10:28:04 -0700

    better spacing

--- acq4/util/SequenceRunner.py
+++ acq4/util/SequenceRunner.py
@@ -201,19 +201,17 @@ class SequenceRunner:
 
 
 if __name__ == '__main__':
-    #!/usr/bin/python -i
-    # -*- coding: utf-8 -*-
-    # from SequenceRunner import *
-
     print("========== runSequence test: simplest way to invoke sequence ============")
     def fn(x, y):
         print(x, "*", y, "=", x * y)
         return x * y
     print(runSequence(fn, {'x': [1, 3, 5, 7], 'y': [2, 4, 6, 8]}, ['y', 'x'], passArgs=True))
 
+
     print("\n========== seq.start(fn) test: Sequence using reusable SR object ============")
     seq = SequenceRunner({'x': [1, 3, 5, 7], 'y': [2, 4, 6, 8]}, ['y', 'x'], passArgs=True)
     print(seq.start(fn))
+
 
     print("\n========== seq.start() test: Sequence using subclassed SR object ============")
     class SR(SequenceRunner):
@@ -222,11 +220,13 @@ if __name__ == '__main__':
     s = SR({'x': [1, 3, 5, 7], 'y': [2, 4, 6, 8], 'z': 0.5}, ['y', 'x'], passArgs=True)
     print(s.start())
 
+
     print("\n========== seq.start() 3D parameter space test ============")
     s.setParams({'x': [1, 3, 5, 7], 'y': [2, 4, 6, 8], 'z': [0.5, 0.6, 0.7]})
     s.setOrder(['x', 'z', 'y'])
     a = s.start()
     print(a)
+
 
     print("\n========== break test: kernel function may skip parts of the parameter space ============")
     s = SR({'x': [1, 3, 5, 7, 9, 11, 13], 'y': [2, 4, 6, 8, 10, 12, 14]}, ['x', 'y'], passArgs=True)
@@ -239,6 +239,7 @@ if __name__ == '__main__':
         return prod
     print(s.start(fn, returnMask=True))
 
+
     print("\n========== line end test: functions run at specific edges of the parameter space ============")
     s = SR({'x': [1, 3, 5, 7], 'y': [2, 4, 6, 8]}, ['x', 'y'], passArgs=True)
     def fn(x, y):
@@ -248,12 +249,14 @@ if __name__ == '__main__':
     s.setEndFuncs([None, fn2])
     s.start(fn)
 
+
     print("\n========== nested index test: specific parts of each parameter are flagged for iteration ============")
     def fn(x, y):
         print("x:", x, "   y:", y)
         return 0
     runSequence(fn, {'x': [1, 3, [5, 6, 7], 8], 'y': {'a': 1, 'b': [1, 2, [3, 'x', 5], 6]}}, ['y["b"][2]', 'x[2]'],
                 passArgs=True)
+
 
     print("\n========== ndarray return test: kernel function returns an array, return is 2D array ============")
     def fn(tVals, yVals, nPts):
