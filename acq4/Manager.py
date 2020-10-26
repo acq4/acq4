2db4e17fcd0cd3b93895665563a2cc3505df961c
Martin <outofculture@gmail.com>
2020-10-26 12:37:50 -0700

    specify scope of exit behavior in help str

--- acq4/Manager.py
+++ acq4/Manager.py
@@ -111,7 +111,7 @@ class Manager(Qt.QObject):
                     print(str(err))
                     print("""
     Valid options are:
-        -x --exit-on-error Whether to exit immidiately on the first exception
+        -x --exit-on-error Whether to exit immidiately on the first exception during initial Manager setup
         -c --config=       Configuration file to load
         -a --config-name=  Named configuration to load
         -m --module=       Module name to load
