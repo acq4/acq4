733cb028b6051500ac2c78ac06f1520519539438
Martin Chase <outofculture@gmail.com>
2021-06-21 22:46:14 -0700

    connect to clicked, hey

--- acq4/modules/MultiPatch/pipetteControl.py
+++ acq4/modules/MultiPatch/pipetteControl.py
@@ -34,8 +34,8 @@ class PipetteControl(Qt.QWidget):
         self.ui.setupUi(self)
         self.ui.holdingSpin.setOpts(bounds=[None, None], decimals=0, suffix='V', siPrefix=True, step=5e-3, format='{scaledValue:.3g} {siPrefix:s}{suffix:s}')
         self.ui.pressureSpin.setOpts(bounds=[None, None], decimals=0, suffix='Pa', siPrefix=True, step=1e3, format='{scaledValue:.3g} {siPrefix:s}{suffix:s}')
-        self.ui.autoOffsetBtn.connect(self.autoOffsetRequested)
-        self.ui.autoPipCapBtn.connect(self.autoPipCapRequested)
+        self.ui.autoOffsetBtn.clicked.connect(self.autoOffsetRequested)
+        self.ui.autoPipCapBtn.clicked.connect(self.autoPipCapRequested)
 
         self.displayWidgets = [
             self.ui.stateText,
