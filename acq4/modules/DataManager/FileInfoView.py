c77636a76d68ffa1bc7dbd41edc522e523b909b8
Luke Campagnola <lukec@alleninstitute.org>
2021-11-09 11:12:21 -0800

    Make datamanager file info view extensible with new custom widgets

--- acq4/modules/DataManager/FileInfoView.py
+++ acq4/modules/DataManager/FileInfoView.py
@@ -26,6 +26,109 @@ class FocusEventCatcher(Qt.QObject):
         return False
 
 
+class MetadataField(object):
+    class SignalEmitter(Qt.QObject):
+        valueChanged = Qt.Signal(object)  # self
+
+    configOrder = ['type']
+        
+    def __init__(self, widget, name, value, config):
+        self.widget = widget
+        self.name = name
+        self.emitter = MetadataField.SignalEmitter()
+        self.valueChanged = self.emitter.valueChanged  # maybe not necessary
+
+        if value is None:
+            value = self.configDict(config).get('default', None)
+        
+        if value is not None:
+            self.setValue(value)
+
+        self.focusEventCatcher = FocusEventCatcher()
+        self.focusEventCatcher.sigLostFocus.connect(self.focusLost)
+        self.widget.installEventFilter(self.focusEventCatcher)
+        
+    def configDict(self, config):
+        return {name:config[i] for i,name in enumerate(self.configOrder) if i < len(config)}
+        
+    def getValue(self):
+        raise NotImplementedError()
+        
+    def setValue(self, value):
+        raise NotImplementedError()
+
+    def focusLost(self, obj):
+        self.valueChanged.emit(self)
+            
+        
+class TextMetadata(MetadataField):
+    configOrder = ['type', 'numLines', 'default']
+    def __init__(self, name, value, config):
+        widget = Qt.QTextEdit()
+        widget.setTabChangesFocus(True)
+        MetadataField.__init__(self, widget, name, value, config)
+
+    def getValue(self):
+        return str(self.widget.toPlainText())
+        
+    def setValue(self, value):
+        self.widget.setText(value)
+
+
+class StringMetadata(MetadataField):
+    configOrder = ['type', 'default']
+    def __init__(self, name, value, config):
+        widget = Qt.QLineEdit()
+        MetadataField.__init__(self, widget, name, value, config)
+
+    def getValue(self):
+        return str(self.widget.text())
+        
+    def setValue(self, value):
+        self.widget.setText(value)
+
+
+class ListMetadata(MetadataField):
+    configOrder = ['type', 'values', 'default']
+    def __init__(self, name, value, config):
+        widget = Qt.QComboBox()
+        widget.addItems([''] + self.configDict(config)['values'])
+        widget.setEditable(True)
+        MetadataField.__init__(self, widget, name, value, config)
+
+    def getValue(self):
+        return str(self.widget.currentText())
+        
+    def setValue(self, value):
+        self.widget.lineEdit().setText(value)
+
+
+class BoolMetadata(MetadataField):
+    configOrder = ['type', 'default']
+    def __init__(self, name, value, config):
+        widget = Qt.QCheckBox()
+        MetadataField.__init__(self, widget, name, value, config)
+
+    def getValue(self):
+        return self.widget.isChecked()
+        
+    def setValue(self, value):
+        self.widget.setChecked(value)
+
+
+fieldTypes = {
+    'text': TextMetadata,
+    'string': StringMetadata,
+    'list': ListMetadata,
+    'bool': BoolMetadata,
+}
+
+
+def registerFieldType(name, metadata_class):
+    global fieldTypes
+    fieldTypes[name] = metadata_class
+
+
 class FileInfoView(Qt.QWidget):
     def __init__(self, parent):
         Qt.QWidget.__init__(self, parent)
@@ -34,9 +137,8 @@ class FileInfoView(Qt.QWidget):
         self.ui.setupUi(self)
         self.current = None
         self.widgets = {}
+        self.metadataFields = {}
         self.ui.fileInfoLayout = self.ui.formLayout_2
-        self.focusEventCatcher = FocusEventCatcher()
-        self.focusEventCatcher.sigLostFocus.connect(self.focusLost)
         
     def setCurrentFile(self, file):
         #print "=============== set current file ============"
@@ -57,16 +159,6 @@ class FileInfoView(Qt.QWidget):
         fields = self.manager.suggestedDirFields(file)
         
         ## Generate fields, populate if data exists
-        #print "Add %d rows.." % len(fields)
-
-        # each field has a number of options that can be set by providing
-        # either a dict or a tuple (where the order of arguments determine their meaning).
-        fieldTypeArgs = {
-            'text': ['numLines', 'default'],
-            'string': ['default'],
-            'list': ['values', 'default'],
-            'bool': ['default'],
-        }
 
         for fieldName, fieldOpts in fields.items():
             if fieldName in infoKeys:
@@ -76,38 +168,11 @@ class FileInfoView(Qt.QWidget):
             if not isinstance(fieldOpts, (dict, tuple)):
                 fieldOpts = (fieldOpts,)
             
-            if isinstance(fieldOpts, tuple):
-                fieldTyp = fieldOpts[0]
-                # convert to dict based on order of args
-                fieldOpts = {fieldTypeArgs[fieldTyp][i-1]:fieldOpts[i] for i in range(1, len(fieldOpts))}
-                fieldOpts['type'] = fieldTyp
-
-            fieldTyp = fieldOpts['type']
-
-            # now use args to create a widget for this field                
-            value = info.get(fieldName, fieldOpts.get('default', None))
-            if fieldTyp == 'text':
-                w = Qt.QTextEdit()
-                w.setTabChangesFocus(True)
-                if value is not None:
-                    w.setText(value)
-            elif fieldTyp == 'string':
-                w = Qt.QLineEdit()
-                if value is not None:
-                    w.setText(value)
-            elif fieldTyp == 'list':
-                w = Qt.QComboBox()
-                w.addItems([''] + fieldOpts['values'])
-                w.setEditable(True)
-                if value is not None:
-                    w.lineEdit().setText(value)
-            elif fieldTyp == 'bool':
-                w = Qt.QCheckBox()
-                if value is not None:
-                    w.setChecked(value)
-            else:
-                raise Exception("Don't understand info type '%s' (parameter %s)" % (fieldTyp, fieldName))
-            self.addRow(fieldName, w)
+            fieldTyp = fieldOpts[0]
+            value = info.get(fieldName, None)
+            metadata_class = fieldTypes[fieldTyp]
+            w = metadata_class(name=fieldName, value=value, config=fieldOpts)
+            self.addRow(fieldName, metadataField=w)
         
         ## Add fields for any other keys that happen to be present
         #print "Add %d rows.." % len(infoKeys)
@@ -124,42 +189,32 @@ class FileInfoView(Qt.QWidget):
                         dt = ""
                     s = time.strftime("%Y.%m.%d   %H:%M:%S", time.localtime(float(s))) + dt
                     
-                    
                 w = Qt.QLabel(s)
             if type(f) is tuple:
                 f = '.'.join(f)
             f = str(f).replace('__', '')
-            self.addRow(f, w)
-            
-    def addRow(self, name, widget):
-        
-        """Add a row to the layout with a label/widget pair."""
-        #print "addRow"
-        self.widgets[widget] = name
-        self.ui.fileInfoLayout.addRow(name, widget)
-        widget.installEventFilter(self.focusEventCatcher)
-            
-    def focusLost(self, obj):
-        field = self.widgets[obj]
-        #print "focus lost", obj, field
-        if isinstance(obj, Qt.QLineEdit):
-            val = str(obj.text())
-        elif isinstance(obj, Qt.QTextEdit):
-            val = str(obj.toPlainText())
-        elif isinstance(obj, Qt.QComboBox):
-            val = str(obj.currentText())
-        elif isinstance(obj, Qt.QCheckBox):
-            val = obj.isChecked()
-        else:
-            return
-        #print "Update", field, val
+            self.addRow(f, widget=w)
+
+    def fieldChanged(self, field):
         info = self.current.info()
-        if field not in info or val != info[field]:
-            self.current.setInfo({field: val})
+        val = field.getValue()
+        if field.name not in info or val != info[field.name]:
+            self.current.setInfo({field.name: val})
+            
+    def addRow(self, name, metadataField=None, widget=None):
+        """Add a row to the layout with a label/widget pair."""
+        if metadataField is not None:
+            widget = metadataField.widget
+            metadataField.valueChanged.connect(self.fieldChanged)
+            self.metadataFields[name] = metadataField
+            
+        self.widgets[name] = widget
+        self.ui.fileInfoLayout.addRow(name, widget)
             
     def clear(self):
-        #print "clear"
         ## remove all rows from layout
         while self.ui.fileInfoLayout.count() > 0:
             w = self.ui.fileInfoLayout.takeAt(0).widget()
             w.setParent(None)
+        self.widgets = {}
+        self.metadataFields = {}
