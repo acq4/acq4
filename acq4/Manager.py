42e72e1e2b8fcf8343a2cb2fbd8402e487be0495
Martin Chase <outofculture@gmail.com>
2022-06-25 12:07:31 -0700

    make logExc seem used

--- acq4/Manager.py
+++ acq4/Manager.py
@@ -33,7 +33,9 @@ from .Interfaces import InterfaceDirectory
 from .devices.Device import Device, DeviceTask
 from .util import DataManager, ptime, Qt
 from .util.HelpfulException import HelpfulException
-from .util.debug import logExc, logMsg, createLogWindow  # logExc needed by debug
+from .util.debug import logExc, logMsg, createLogWindow
+
+_ = logExc  # prevent cleanup of logExc; needed by debug
 
 
 def __reload__(old):
