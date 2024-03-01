27d7cc25bd8df2b49868a488babbb748dcb727fa
Martin Chase <outofculture@gmail.com>
2024-02-29 19:25:49 -0800

    simplfy, remove repetition

--- acq4/modules/DataManager/DataManagerModule.py
+++ acq4/modules/DataManager/DataManagerModule.py
@@ -128,22 +128,21 @@ class DataManager(Module):
         self.manager.setCurrentDir(handle)
 
     def currentDirChanged(self, name=None, change=None, args=()):
+        newDir = None
         if change in [None, 'moved', 'renamed', 'parent']:
             try:
                 newDir = self.manager.getCurrentDir()
                 dirName = newDir.name(relativeTo=self.baseDir)
             except Exception:
-                newDir = None
                 dirName = ""
             self.ui.currentDirText.setText(dirName)
             self.ui.fileTreeWidget.setCurrentDir(newDir)
-            has_images = newDir is not None and newDir.hasMatchingChildren(lambda f: f.shortName().endswith('.tif'))
-            self.ui.loadPinnedImagesBtn.setEnabled(has_images)
         elif change == 'children':
             with contextlib.suppress(HelpfulException):
                 newDir = self.manager.getCurrentDir()
-                has_images = newDir is not None and newDir.hasMatchingChildren(lambda f: f.shortName().endswith('.tif'))
-                self.ui.loadPinnedImagesBtn.setEnabled(has_images)
+
+        has_images = newDir is not None and newDir.hasMatchingChildren(lambda f: f.shortName().endswith('.tif'))
+        self.ui.loadPinnedImagesBtn.setEnabled(has_images)
 
     def showFileDialog(self):
         bd = self.manager.getBaseDir()
