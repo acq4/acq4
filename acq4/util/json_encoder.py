80ed542efbf9b0141f44c3e4d74fac728360cf06
Martin Chase <outofculture@gmail.com>
2024-03-20 11:58:33 -0700

    encode numpy primatives in json

---
+++ acq4/util/json_encoder.py
@@ -0,0 +1,14 @@
+import json
+
+import numpy as np
+
+
+class ACQ4JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
