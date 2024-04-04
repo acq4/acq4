e02f146bac836571875b25e86d78e4e425b2927e
Martin Chase <outofculture@gmail.com>
2024-04-04 14:49:14 -0700

    clear up why this is printing

--- acq4/devices/MockOdorDelivery.py
+++ acq4/devices/MockOdorDelivery.py
@@ -3,4 +3,4 @@ from acq4.devices.OdorDelivery import OdorDelivery
 
 class MockOdorDelivery(OdorDelivery):
     def setChannelValue(self, channel: int, value: int):
-        print(f"setting {channel} to {value}")
+        print(f"MockOdorDelivery[{self.name}] setting {channel} to {value}")
