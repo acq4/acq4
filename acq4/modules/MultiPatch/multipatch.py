6ace6954cd6f02768c96113b117539535219d057
Martin Chase <outofculture@gmail.com>
2024-10-01 09:50:24 -0700

    no more unreadable stacks

--- acq4/modules/MultiPatch/multipatch.py
+++ acq4/modules/MultiPatch/multipatch.py
@@ -588,7 +588,7 @@ class MultiPatchWindow(Qt.QWidget):
             for dev in self.pips:
                 dev_gr = group.create_group(dev.name())
                 dev_gr.attrs['device'] = dev.name()
-                self._testPulseStacks[dev.name()] = H5BackedTestPulseStack(dev_gr, readable=False)
+                self._testPulseStacks[dev.name()] = H5BackedTestPulseStack(dev_gr)
         for pip in self.selectedPipettes():
             pip.emitFullTestPulseData(rec)
 
