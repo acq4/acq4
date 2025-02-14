f208fbd42733dc0c3d54498fc66ce81abfff331c
Martin Chase <outofculture@gmail.com>
2025-02-14 10:32:45 -0800

    users will sometimes drive the hardware to where the motion planner would consider things impossible

--- acq4/util/geometry.py
+++ acq4/util/geometry.py
@@ -393,8 +393,9 @@ class GeometryMotionPlanner:
             obst = list(self.geometries.keys())[i]
             if visualizer is not None:
                 visualizer.addObstacleVolumeOutline(obst_volume, to_global_from_obst)
-            if obst_volume.contains_point(to_global_from_obst.inverse.map(start)):
-                raise ValueError(f"Start point {start} is inside obstacle {obst.name}")
+            # users will sometimes drive the hardware to where the motion planner would consider things impossible
+            # if obst_volume.contains_point(to_global_from_obst.inverse.map(start)):
+            #     raise ValueError(f"Start point {start} is inside obstacle {obst.name}")
             if obst_volume.contains_point(to_global_from_obst.inverse.map(stop)):
                 raise ValueError(f"Destination point {stop} is inside obstacle {obst.name}")
 
