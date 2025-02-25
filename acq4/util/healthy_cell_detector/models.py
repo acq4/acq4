52727e4c4fcdf6ce859450d5bb3f0168662deee7
Martin Chase <outofculture@gmail.com>
2025-02-25 11:47:33 -0800

    "skip connections" to invert the maxpool

--- acq4/util/healthy_cell_detector/models.py
+++ acq4/util/healthy_cell_detector/models.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -5,58 +6,105 @@ class NeuronAutoencoder(nn.Module):
     def __init__(self, latent_dim=64):
         super().__init__()
 
-        # Encoder
-        # incoming data 1x5x63x63
-        self.encoder = nn.Sequential(
-            nn.Conv3d(1, 16, kernel_size=3, padding=1),  # 16x5x63x63
-            nn.BatchNorm3d(16),
-            nn.ReLU(),
-            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2)),  # 16x5x31x31
+        # Encoder blocks broken down to access intermediate outputs
+        # Block 1: 1x5x63x63 -> 16x5x31x31
+        self.enc_block1 = nn.Sequential(nn.Conv3d(1, 16, kernel_size=3, padding=1), nn.BatchNorm3d(16), nn.ReLU(),)
+        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
-            nn.Conv3d(16, 32, kernel_size=3, padding=1),
-            nn.BatchNorm3d(32),
-            nn.ReLU(),
-            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2)),  # 32x5x15x15
+        # Block 2: 16x5x31x31 -> 32x5x15x15
+        self.enc_block2 = nn.Sequential(nn.Conv3d(16, 32, kernel_size=3, padding=1), nn.BatchNorm3d(32), nn.ReLU(),)
+        self.pool2 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
-            nn.Conv3d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm3d(64),
-            nn.ReLU(),
-            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2)),  # 64x5x7x7
+        # Block 3: 32x5x15x15 -> 64x5x7x7
+        self.enc_block3 = nn.Sequential(nn.Conv3d(32, 64, kernel_size=3, padding=1), nn.BatchNorm3d(64), nn.ReLU(),)
+        self.pool3 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
-            nn.Conv3d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm3d(128),
-            nn.ReLU(),
-            nn.MaxPool3d(kernel_size=2, stride=2),  # 128x2x3x3
+        # Block 4: 64x5x7x7 -> 128x2x3x3
+        self.enc_block4 = nn.Sequential(nn.Conv3d(64, 128, kernel_size=3, padding=1), nn.BatchNorm3d(128), nn.ReLU(),)
+        self.pool4 = nn.MaxPool3d(kernel_size=2, stride=2)
 
-            nn.Conv3d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm3d(256),
-            nn.ReLU(),
-            nn.MaxPool3d(kernel_size=2, stride=2),  # 256x1x1x1
+        # Block 5: 128x2x3x3 -> 256x1x1x1
+        self.enc_block5 = nn.Sequential(nn.Conv3d(128, 256, kernel_size=3, padding=1), nn.BatchNorm3d(256), nn.ReLU(),)
+        self.pool5 = nn.MaxPool3d(kernel_size=2, stride=2)
 
-            nn.Flatten(),
-            nn.Linear(256, latent_dim),
-        )
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 256),
-            nn.Unflatten(1, (256, 1, 1, 1)),
+        # Latent space
+        self.flatten = nn.Flatten()
+        self.fc_encoder = nn.Linear(256, latent_dim)
+        self.fc_decoder = nn.Linear(latent_dim, 256)
+        self.unflatten = nn.Unflatten(1, (256, 1, 1, 1))
+
+        # Decoder blocks with skip connections
+        # Block 1: 256x1x1x1 -> 128x2x3x3
+        self.dec_block1 = nn.Sequential(
             nn.ConvTranspose3d(256, 128, kernel_size=2, stride=2, output_padding=(0, 1, 1)),
             nn.BatchNorm3d(128),
             nn.ReLU(),
-            nn.ConvTranspose3d(128, 64, kernel_size=2, stride=2, output_padding=1),
-            nn.BatchNorm3d(64),
-            nn.ReLU(),
-            nn.ConvTranspose3d(64, 32, kernel_size=(1, 2, 2), stride=(1, 2, 2), output_padding=(0, 1, 1)),
+        )
+
+        # Block 2: (128+128)x2x3x3 -> 64x5x7x7
+        self.dec_block2 = nn.Sequential(
+            nn.ConvTranspose3d(128 + 128 + 128, 64, kernel_size=2, stride=2, output_padding=1), nn.BatchNorm3d(64), nn.ReLU(),
+        )
+
+        # Block 3: (64+64)x5x7x7 -> 32x5x15x15
+        self.dec_block3 = nn.Sequential(
+            nn.ConvTranspose3d(64 + 64 + 64, 32, kernel_size=(1, 2, 2), stride=(1, 2, 2), output_padding=(0, 1, 1)),
             nn.BatchNorm3d(32),
             nn.ReLU(),
-            nn.ConvTranspose3d(32, 16, kernel_size=(1, 2, 2), stride=(1, 2, 2), output_padding=(0, 1, 1)),
+        )
+
+        # Block 4: (32+32)x5x15x15 -> 16x5x31x31
+        self.dec_block4 = nn.Sequential(
+            nn.ConvTranspose3d(32 + 32 + 32, 16, kernel_size=(1, 2, 2), stride=(1, 2, 2), output_padding=(0, 1, 1)),
             nn.BatchNorm3d(16),
             nn.ReLU(),
-            nn.ConvTranspose3d(16, 1, kernel_size=(1, 2, 2), stride=(1, 2, 2), output_padding=(0, 1, 1)),
+        )
+
+        # Block 5: (16+16)x5x31x31 -> 1x5x63x63
+        self.dec_block5 = nn.Sequential(
+            nn.ConvTranspose3d(16 + 16 + 16, 1, kernel_size=(1, 2, 2), stride=(1, 2, 2), output_padding=(0, 1, 1)),
             nn.Sigmoid(),
         )
 
     def forward(self, x):
-        latent = self.encoder(x)
-        reconstructed = self.decoder(latent)
+        # Encoder forward pass with saved intermediate outputs
+        x1 = self.enc_block1(x)
+        p1 = self.pool1(x1)
+
+        x2 = self.enc_block2(p1)
+        p2 = self.pool2(x2)
+
+        x3 = self.enc_block3(p2)
+        p3 = self.pool3(x3)
+
+        x4 = self.enc_block4(p3)
+        p4 = self.pool4(x4)
+
+        x5 = self.enc_block5(p4)
+        p5 = self.pool5(x5)
+
+        # Latent space
+        flattened = self.flatten(p5)
+        latent = self.fc_encoder(flattened)
+
+        # Begin decoder
+        dec_latent = self.fc_decoder(latent)
+        unflattened = self.unflatten(dec_latent)
+
+        # Decoder forward pass with skip connections
+        d1 = self.dec_block1(unflattened)
+
+        # Concatenate skip connections along channel dimension
+        d1_skip = torch.cat([d1, x5], dim=1)
+        d2 = self.dec_block2(d1_skip)
+
+        d2_skip = torch.cat([d2, x4], dim=1)
+        d3 = self.dec_block3(d2_skip)
+
+        d3_skip = torch.cat([d3, x3], dim=1)
+        d4 = self.dec_block4(d3_skip)
+
+        d4_skip = torch.cat([d4, x2], dim=1)
+        reconstructed = self.dec_block5(d4_skip)
+
         return reconstructed, latent
