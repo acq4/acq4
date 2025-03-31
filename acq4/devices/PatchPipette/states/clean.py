319f1a561b03924cf4aed307b7ff1be4d5f30d54
Martin Chase <outofculture@gmail.com>
2025-03-31 15:27:47 -0700

    move home between and after each cleaning step

--- acq4/devices/PatchPipette/states/clean.py
+++ acq4/devices/PatchPipette/states/clean.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from acq4.util.debug import printExc
-from acq4.util.future import future_wrap
+from acq4.util.future import future_wrap, Future
 from pyqtgraph import units
 from ._base import PatchPipetteState
 
@@ -44,7 +44,6 @@ class CleanState(PatchPipetteState):
     }
 
     def __init__(self, *args, **kwds):
-        self.currentFuture = None
         self.sonication = None
         super().__init__(*args, **kwds)
 
@@ -97,11 +96,9 @@ class CleanState(PatchPipetteState):
                 (wellPos, 'fast', False, f"into the {stage}ing well"),
             ]
 
-            self.currentFuture = pip._movePath(path)
-
             # todo: if needed, we can check TP for capacitance changes here
             # and stop moving as soon as the fluid is detected
-            self.waitFor(self.currentFuture, timeout=None)
+            self.waitFor(pip._movePath(path), timeout=None)
 
             if dev.sonicatorDevice is not None:
                 self.sonication = dev.sonicatorDevice.doProtocol(config['sonicationProtocol'])
@@ -113,24 +110,16 @@ class CleanState(PatchPipetteState):
             if self.sonication is not None and not self.sonication.isDone():
                 self.waitFor(self.sonication)
 
-            self.resetPosition()
+            self.waitFor(self.resetPosition())
 
         dev.pipetteRecord()['cleanCount'] += 1
         dev.setTipClean(True)
         self.dev.pipetteDevice.moveTo('home', 'fast')
-        self.currentFuture = None
         dev.newPatchAttempt()
         return 'out'
 
-    def resetPosition(self, _future=None):
-        if self.currentFuture is not None:
-            # play in reverse
-            fut = self.currentFuture
-            self.currentFuture = None
-            if _future is not None:
-                _future.waitFor(fut.undo(), timeout=None)
-            else:
-                self.waitFor(fut.undo(), timeout=None)
+    def resetPosition(self) -> Future:
+        return self.dev.pipetteDevice.moveTo('home', 'fast')
 
     @future_wrap
     def cleanup(self, _future):
@@ -144,4 +133,4 @@ class CleanState(PatchPipetteState):
         except Exception:
             printExc("Error resetting pressure after clean")
 
-        self.resetPosition(_future)
+        _future.waitFor(self.resetPosition())
