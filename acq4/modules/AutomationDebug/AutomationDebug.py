94ac7083c21b51473a9c8dd81f5a5a8f04c053f7
Martin Chase <outofculture@gmail.com>
2025-04-08 18:10:26 -0700

    asarray once

--- acq4/modules/AutomationDebug/AutomationDebug.py
+++ acq4/modules/AutomationDebug/AutomationDebug.py
@@ -249,14 +249,14 @@ class AutomationDebugWindow(Qt.QWidget):
         if self.ui.mockCheckBox.isChecked() and self.ui.mockFilePath.text():
             # Load the MetaArray file
             mock_file_path = self.ui.mockFilePath.text()
-            data = MetaArray(file=mock_file_path)
+            data = MetaArray(file=mock_file_path).asarray()
             base_position = np.array(self.cameraDevice.mapToGlobal((0, 0, 0)))
             # positions = data._info[0]["globalPosition"]
             # z_scale = abs(positions[0][2] - positions[1][2])
             z_scale = 1e-6  # ugh, some stacks lack position info
             z_stack = [
                 Frame(
-                    data[i].asarray(),
+                    data[i],
                     info={
                         "transform": {
                             "pos": base_position + (0, 0, i * z_scale),
@@ -269,8 +269,8 @@ class AutomationDebugWindow(Qt.QWidget):
         else:
             # Normal acquisition path
             depth = self.cameraDevice.getFocusDepth()
-            start = depth - 40 * µm
-            stop = depth + 40 * µm
+            start = depth - 10 * µm
+            stop = depth + 10 * µm
             # Acquire real z-stack
             z_stack = _future.waitFor(acquire_z_stack(self.cameraDevice, start, stop, 1 * µm)).getResult()
             self.cameraDevice.setFocusDepth(depth).raiseErrors("error restoring focus")  # no need to wait
