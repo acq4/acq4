8e530c34d1ab6df01fd33edb10e4b6e160611254
Martin Chase <outofculture@gmail.com>
2025-05-16 18:02:06 -0700

    we can keep tracking until cell detect is done

--- acq4/modules/AutomationDebug/AutomationDebug.py
+++ acq4/modules/AutomationDebug/AutomationDebug.py
@@ -997,20 +997,15 @@ class AutomationDebugWindow(Qt.QWidget):
     def _autopatchCellDetect(self, cell, _future):
         try:
             ppip = self.patchPipetteDevice
-            has_stopped = False
             ppip.setState("cell detect")
             while True:
                 if (state := ppip.getState().stateName) != "cell detect":
                     _future.setState(f"Autopatch: patch cell: {state}")
                     cell.enableTracking(False)
                     pg.disconnect(cell.sigPositionChanged, self._updatePipetteTarget)
-                if np.linalg.norm(np.array(cell.position) - ppip.pipetteDevice.globalPosition()) > 10e-6:
-                    if state == "cell detect" and not has_stopped and not cell.isTracking:
-                        break  # something bad happened
-                elif not has_stopped:
-                    cell.enableTracking(False)
                     _future.waitFor(self.cameraDevice.moveCenterToGlobal(cell.position, "fast"))
-                    has_stopped = True
+                if state == "cell detect" and not cell.isTracking:
+                    break  # something bad happened
                 if state in ("whole cell", "bath", "broken", "fouled"):
                     break
                 _future.sleep(0.1)
