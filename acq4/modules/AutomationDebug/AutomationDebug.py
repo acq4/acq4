48d34cf7b7a02b44b4847f96a0dc313429bb5295
Martin Chase <outofculture@gmail.com>
2025-05-17 06:05:05 -0700

    style

--- acq4/modules/AutomationDebug/AutomationDebug.py
+++ acq4/modules/AutomationDebug/AutomationDebug.py
@@ -696,7 +696,7 @@ class AutomationDebugWindow(Qt.QWidget):
             if state != "whole cell":
                 logMsg(f"Autopatch: Cell detect finished: {state}. Next!")
                 continue
-            logMsg(f"Autopatch: got whole cell, start task runner")
+            logMsg("Autopatch: got whole cell, start task runner")
             _future.setState("Autopatch: running task runner")
             self._autopatchRunTaskRunner(_future)
             _future.setState("Autopatch: resealing")
