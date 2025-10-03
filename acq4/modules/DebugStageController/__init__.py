bfb12cc213885e8c1b268affe143b800f99ce2bb
Martin Chase <outofculture@gmail.com>
2025-10-03 16:15:24 -0700

    just go away please

--- acq4/modules/DebugStageController/__init__.py
+++
@@ -1,6 +0,0 @@
-# Module initialization for DebugStageController 
-# Exports the main DebugStageController class and factory function
-
-from .DebugStageController import DebugStageController, createWindow
-
-__all__ = ['DebugStageController', 'createWindow']
