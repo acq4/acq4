475914a8ceed7a7f7cc634c09467f50a8099efd2
Martin Chase <outofculture@gmail.com>
2025-10-10 13:46:16 -0700

    keep the holding potential if seal gets far enough

--- acq4/devices/PatchPipette/states/seal.py
+++ acq4/devices/PatchPipette/states/seal.py
@@ -182,7 +182,7 @@ class SealState(PatchPipetteState):
     holdingThreshold : float
         Seal resistance (ohms) above which the holding potential will switch
         from its initial value to the value specified in the *holdingPotential*
-        parameter.
+        parameter. Default 100MΩ
     holdingPotential : float
         Holding potential (volts) to apply to the pipette after the seal resistance
         becomes greater than *holdingThreshold*.
@@ -338,6 +338,8 @@ class SealState(PatchPipetteState):
                 if self._analysis.failure() or dt > config['autoSealTimeout']:
                     self._patchrec['sealSuccessful'] = False
                     self._taskDone(interrupted=True, error=f"Seal failed after {dt:f} seconds")
+                    if holdingSet:
+                        self.nextStateConfig['initialVCHolding'] = config['holdingPotential']
                     return config['fallbackState']
 
                 self.updatePressure()
