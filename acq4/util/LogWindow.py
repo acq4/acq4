0d8ff6ef45553a5acef22f1b5262c37ced974b74
Luke Campagnola <lukec@alleninstitute.org>
2026-04-30 18:31:25 -0700

    fix: raise log window properly

--- acq4/util/LogWindow.py
+++ acq4/util/LogWindow.py
@@ -139,9 +139,7 @@ class ErrorDialog(Qt.QDialog):
 
     def logClicked(self):
         self.accept()
-        log = get_log_window()
-        log.show()
-        log.raise_()
+        get_log_window().raise_window()
         self.messages = []
 
     def nextMessage(self):
@@ -264,7 +262,10 @@ class DocumentedLogViewer(LogViewer):
 
     def raise_window(self):
         """Bring the log window to the front."""
-        self.show()
+        if self.isMinimized():
+            self.showNormal()
+        else:
+            self.show()
         self.raise_()
         self.activateWindow()
 
